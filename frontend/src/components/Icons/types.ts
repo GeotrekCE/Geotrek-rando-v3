13eceb4990a0d3825d7c46ef7a4ab530b3d17adc
Loïc Chau <loicch@bam.tech>
2021-01-04 17:38:14 +0100

    chore(Icons): Create generic Icons props
    Our icons are generated by SVGR and are therefore components

---
+++ frontend/src/components/Icons/types.ts
@@ -0,0 +1,8 @@
+export interface GenericIconProps {
+  /** Default value is "currentColor", which allows you
+   * to color your icon using the CSS `color` property. It is the recommended way. */
+  color?: string;
+  opacity?: number;
+  size?: number;
+  className?: string;
+}
