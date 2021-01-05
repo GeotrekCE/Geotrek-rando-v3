62bda1b5bdbe7c5a69098422a27852a4c7dccd34
Loïc Chau <loicch@bam.tech>
2021-01-05 10:15:52 +0100

    feature(ActivitySearchFilter): Add walking activities to ActivitySearchFilter

--- frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.style.ts
+++ frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.style.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { borderRadius, colorPalette, getSpacing } from 'stylesheet';
 
 export const ActivitySearchFilterContainer = styled.div`
+  display: flex;
+
   padding: 0 ${getSpacing(3)};
   background-color: ${colorPalette.white};
   box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.15);
--- frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.tsx
+++ frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
+import { Walking } from '../Icons/Walking';
+
+import { ActivityButton } from './ActivityButton';
 import { ActivitySearchFilterContainer } from './ActivitySearchFilter.style';
 
 export const ActivitySearchFilter: React.FC = () => {
-  return <ActivitySearchFilterContainer>Randonnée</ActivitySearchFilterContainer>;
+  return (
+    <ActivitySearchFilterContainer>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+      <ActivityButton icon={Walking}>
+        <FormattedMessage id="home.walking" />
+      </ActivityButton>
+    </ActivitySearchFilterContainer>
+  );
 };
