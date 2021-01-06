bd7c81c6d6c93a4678f5cf1b76118b8d101d725a
Delphine Bugner <delphineb@bam.tech>
2021-01-06 11:42:09 +0100

    :recycle: (header) move style to main component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { FunctionComponent } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+import { oldGetSpacing, typography, zIndex } from 'stylesheet';
 
 import { routes } from 'services/routes';
-
-import { HeaderContainer, Logo, Title } from './Header.style';
 
 export const Header: FunctionComponent = () => {
   return (
@@ -15,3 +15,32 @@ export const Header: FunctionComponent = () => {
     </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled.header`
+  ${typography.h1}
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: ${oldGetSpacing(13)};
+  padding: ${oldGetSpacing(10)} ${oldGetSpacing(4)};
+
+  /* Replace background-color later */
+  background-color: grey;
+  position: sticky;
+  top: 0;
+  z-index: ${zIndex.header};
+`;
+
+HeaderContainer.displayName = 'HeaderContainer';
+
+const Title = styled.h1`
+  ${typography.h1}
+`;
+Title.displayName = 'HeaderTitle';
+
+const Logo = styled.img`
+  height: ${oldGetSpacing(9)};
+`;
+
+Logo.displayName = 'Logo';
