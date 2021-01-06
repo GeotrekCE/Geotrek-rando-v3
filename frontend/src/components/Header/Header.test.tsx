25e98d00c6e4063a01e672a702aaddb24a761574
Delphine Bugner <delphineb@bam.tech>
2021-01-06 15:05:54 +0100

    :camera_flash: (header) Header snapshot (current version : mobile)

---
+++ frontend/src/components/Header/Header.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '../../services/testing/reactTestingLibraryWrapper';
+import { Header } from '.';
+
+describe('Header', () => {
+  it('EEU I see header in mobile version', () => {
+    const component = render(<Header logoPath="./logo.png" />);
+    expect(component).toMatchSnapshot();
+  });
+});
