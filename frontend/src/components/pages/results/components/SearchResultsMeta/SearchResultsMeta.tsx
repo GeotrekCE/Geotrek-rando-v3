9c0ea3dd60264482ca8380377ee143c8255fa1da
Loïc Chau <loicch@bam.tech>
2021-01-06 18:34:55 +0100

    feature(SearchResultsMeta): Add illustration and adapt to desktop

--- frontend/src/components/pages/results/components/SearchResultsMeta/SearchResultsMeta.tsx
+++ frontend/src/components/pages/results/components/SearchResultsMeta/SearchResultsMeta.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
-import { colorPalette, getSpacing, typography } from 'stylesheet';
+import styled, { css } from 'styled-components';
+import { colorPalette, desktopOnly, getSpacing, typography } from 'stylesheet';
 
 export const SearchResultsMeta: React.FC = () => {
   return (
-    <div className="flex-initial flex-col">
-      <ResultsNumber>82 résultats trouvés</ResultsNumber>
-      <RankingInfo>Classement par ordre de pertinence</RankingInfo>
+    <div className="flex">
+      <div>
+        <Illustration className="hidden desktop:block" src="images/little-forest.png" />
+      </div>
+
+      <div className="desktop: ml-6">
+        <ResultsNumber>82 résultats trouvés</ResultsNumber>
+        <RankingInfo className="desktop:hidden">Classement par ordre de pertinence</RankingInfo>
+      </div>
     </div>
   );
 };
 
+const Illustration = styled.img`
+  height: ${getSpacing(16)};
+  width: ${getSpacing(16)};
+`;
+
 const ResultsNumber = styled.div`
   ${typography.h1};
   color: ${colorPalette.darkPurple};
+
+  ${desktopOnly(css`
+    ${typography.h2}
+  `)}
 `;
 
 const RankingInfo = styled.div`
