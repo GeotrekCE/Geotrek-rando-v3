6d14bf93b5962e1eb4a56bdc9af4b5d901b381d9
Loïc Chau <loicch@bam.tech>
2021-01-06 11:44:10 +0100

    feat(ResultsPage): Put a ResultCard on Results page

--- frontend/src/components/pages/results/Results.tsx
+++ frontend/src/components/pages/results/Results.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { Layout } from 'components/Layout/Layout';
 import { FilterBar } from './components/FilterBar';
+import { ResultCard } from './components/ResultCard';
 
 export const ResultsUI: React.FC = () => {
   return (
     <Layout>
       <FilterBar />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
     </Layout>
   );
 };
---
+++ frontend/src/components/pages/results/components/ResultCard/index.tsx
@@ -0,0 +1,1 @@
+export { ResultCard } from './ResultCard';
