3d7ef2b6f64eb49c9e731be866358441ecba656b
Delphine Bugner <delphineb@bam.tech>
2021-01-07 11:21:13 +0100

    :sparkles: (BurgerMenu) personnalize icons

--- frontend/src/components/BurgerMenu/BurgerMenu.tsx
+++ frontend/src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,13 +1,17 @@
 import { slide as Slide } from 'react-burger-menu';
+import { BurgerMenu as BmIcon } from '../Icons/BurgerMenu';
+import { Cross } from '../Icons/Cross';
 
 export const BurgerMenu = () => {
   return (
     <Slide
       right
-      burgerButtonClassName="fixed w-5 h-3 top-4 right-3"
+      customBurgerIcon={<BmIcon color="white" />}
+      customCrossIcon={<Cross size={14} className="mt-3" />}
+      burgerButtonClassName="fixed w-6 h-6 top-2.5 right-2.5"
       burgerBarClassName="bg-white"
       menuClassName="bg-white"
-      crossButtonClassName="left-3"
+      crossButtonClassName="left-5"
       crossClassName="bg-greyDarkColored"
     />
   );
