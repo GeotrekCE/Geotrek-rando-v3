70dd6a259f285502c7b9a26cc541e977e38a21b0
Delphine Bugner <delphineb@bam.tech>
2021-01-07 16:57:47 +0100

    :fire: (test) remove redundant header test

--- frontend/src/components/Header/__tests__/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '../../../services/testing/reactTestingLibraryWrapper';
-import { Header } from '..';
-
-describe('Header', () => {
-  it('EEU I see header in mobile version', () => {
-    const component = render(<Header logoPath="./logo.png" />);
-    expect(component).toMatchSnapshot();
-  });
-});
