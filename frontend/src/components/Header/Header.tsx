7656d7333932b0057e1ad737ceb18e5b5616bf44
Loïc Chau <loicch@bam.tech>
2021-01-08 14:00:02 +0100

    refacto(Header): Use theme zIndex

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ export const Header: React.FC<Props> = ({ logoPath }) => {
   return (
     <>
       <BurgerMenu subSections={subSections} sections={sections} title="Menu" />
-      <div className="h-11 bg-primary1 flex flex-row items-center sticky top-0 z-10">
+      <div className="h-11 bg-primary1 flex flex-row items-center sticky top-0 z-header">
         <Link href={routes.HOME}>
           <img className="h-9 mx-3" alt="logo" src={logoPath} />
         </Link>
