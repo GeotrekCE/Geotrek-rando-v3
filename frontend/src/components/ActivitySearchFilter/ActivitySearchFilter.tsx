add232e82ca8c7d70eb2f4b4c44b09121b894840
Louis Kraemer <louisk@bam.tech>
2021-01-08 15:18:22 +0100

    :sparkles: (home) display activities from API

--- frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.tsx
+++ frontend/src/components/ActivitySearchFilter/ActivitySearchFilter.tsx
@@ -2,23 +2,29 @@ import { Activity } from 'components/ActivitySearchFilterMobile/ActivitySearchFi
 import { Link } from 'components/Link';
 import React from 'react';
 import { routes } from 'services/routes';
-import { Walking } from '../Icons/Walking';
+
 import { ActivityButton } from './ActivityButton';
+import { useActivitySearchFilter } from './useActivitySearchFilter';
+
 interface Props {
   className?: string;
   activities: Array<Activity>;
 }
 
-export const ActivitySearchFilter: React.FC<Props> = ({ className, activities }) => {
+export const ActivitySearchFilter: React.FC<Props> = ({ className }) => {
+  const { activities } = useActivitySearchFilter();
+
   return (
+    // <ActivitySearchFilterContainer className={className}>
     <div className={`py-7 bg-white shadow-lg rounded-2xl overflow-scroll ${className ?? ''}`}>
-      {activities.map(activity => (
-        <Link href={routes.SEARCH} key={activity.value}>
-          <ActivityButton icon={Walking} key={activity.value}>
-            {activity.label}
+      {activities?.map(activity => (
+        <Link href={routes.SEARCH} key={activity.id}>
+          <ActivityButton iconUrl={activity.pictogram} key={activity.id}>
+            <span>{activity.name}</span>
           </ActivityButton>
         </Link>
       ))}
+      {/* </ActivitySearchFilterContainer> */}
     </div>
   );
 };
