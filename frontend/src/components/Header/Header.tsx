0f9ed5aaf5173da3c92bb70f04e67367ebd49370
Loïc Chau <loicch@bam.tech>
2021-01-08 16:32:29 +0100

    feature(Header): Use correct header height on desktop

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { FormattedMessage } from 'react-intl';
+import styled, { css } from 'styled-components';
 import { Link } from 'components/Link';
 import { routes } from 'services/routes';
+import { desktopOnly, sizes } from 'stylesheet';
 import BurgerMenu from '../BurgerMenu';
 
 interface Props {
@@ -24,14 +26,20 @@ export const Header: React.FC<Props> = ({ logoPath }) => {
   return (
     <>
       <BurgerMenu subSections={subSections} sections={sections} title="Menu" />
-      <div className="h-11 bg-primary1 flex flex-row items-center sticky top-0 z-header">
+      <Container className="h-11 bg-primary1 flex flex-row items-center sticky top-0 z-header">
         <Link href={routes.HOME}>
           <img className="h-9 mx-3" alt="logo" src={logoPath} />
         </Link>
         <h1 className="flex-1 text-white">
           <FormattedMessage id={'home.title'} />
         </h1>
-      </div>
+      </Container>
     </>
   );
 };
+
+const Container = styled.div`
+  ${desktopOnly(css`
+    height: ${sizes.desktopHeader}px;
+  `)}
+`;
