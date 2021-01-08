f890ce1db315fc09d460ba7a383ae1adaf97fc2d
Delphine Bugner <delphineb@bam.tech>
2021-01-08 18:55:56 +0100

    :lipstick: (activityFilter) finishing mobile component styling

--- frontend/src/components/ActivitySearchFilterMobile/ActivitySearchFilterMobile.tsx
+++ frontend/src/components/ActivitySearchFilterMobile/ActivitySearchFilterMobile.tsx
@@ -31,21 +31,22 @@ export const ActivitySearchFilterMobile: React.FC<ActivitySearchFilterMobileProp
         className="flex-1"
         options={[...activities, selectAll]}
         styles={selectStyles}
+        isSearchable={false}
         placeholder={<FormattedMessage id="home.selectPlaceholder" />}
         onChange={activity => updateSelectedActivity(activity?.value ?? null)}
       />
       {/* TODO update route with active filter using selected activity */}
       {selectedActivity !== null ? (
-        <Link href={routes.SEARCH}>{validateButtonStatic}</Link>
+        <Link href={routes.SEARCH}>{validateButton}</Link>
       ) : (
-        validateButtonStatic
+        validateButton
       )}
     </div>
   );
 };
 
-const validateButtonStatic = (
-  <div className="bg-primary1 hover:bg-primary3 text-white shadow-lg rounded-lg p-3.5 cursor-pointer">
+const validateButton = (
+  <div className="bg-primary1 hover:bg-primary1-light shadow-lg text-white rounded-lg p-3.5 cursor-pointer transition-all">
     <Arrow size={24} />
   </div>
 );
@@ -67,34 +68,38 @@ const selectStyles = {
     ...styles,
     padding: 'none',
   }),
+  singleValue: (styles: any) => ({
+    ...styles,
+    color: colorPalette.greyDarkColored,
+    paddingLeft: getSpacing(2),
+  }),
   menu: (styles: any) => ({
     ...styles,
+    padding: `${getSpacing(2)} 0`,
     margin: 0,
     border: 'none',
     boxShadow: shadow.large,
     borderRadius: getSpacing(2),
-    // borderTopLeftRadius: 0,
-    // borderTopRightRadius: 0,
   }),
-  option: (styles: any) => {
-    return {
-      ...styles,
-      backgroundColor: colorPalette.filter.background,
-      color: colorPalette.greyDarkColored,
-      ':hover': {
-        backgroundColor: colorPalette.primary2,
-      },
-    };
-  },
-  input: (styles: any) => ({
+  option: (styles: any) => ({
     ...styles,
+    backgroundColor: 'white',
+    paddingLeft: getSpacing(4),
+    color: colorPalette.greyDarkColored,
+    ':hover': {
+      backgroundColor: colorPalette.primary2,
+    },
+  }),
+  indicatorSeparator: (styles: any) => ({
+    color: 'transparent',
   }),
   placeholder: (styles: any) => ({
     ...styles,
     color: colorPalette.greyDarkColored,
-    padding: 'none',
+    paddingLeft: '8px',
     ':focus': {
       outline: 'none',
+      color: 'white',
     },
   }),
 };
