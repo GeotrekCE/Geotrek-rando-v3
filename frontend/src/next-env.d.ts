dac9f9e78604aac698f0b12aa19d798fc6ebf459
Delphine Bugner <delphineb@bam.tech>
2021-01-11 17:28:38 +0100

    :label: (react country flag) add typescript module export in next-env.d.ts

--- frontend/src/next-env.d.ts
+++ frontend/src/next-env.d.ts
@@ -6,3 +6,15 @@ declare namespace NodeJS {
     REACT_APP_API_BASE_URL: string;
   }
 }
+declare module 'react-country-flag' {
+  import * as React from 'react';
+  export interface ReactCountryFlagProps {
+    countryCode: string;
+    cdnSuffix?: string;
+    cdnUrl?: string;
+    svg?: boolean;
+    style?: React.CSSProperties;
+    className?: string;
+  }
+  export default class ReactCountryFlag extends React.Component<ReactCountryFlagProps> {}
+}
