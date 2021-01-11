371bfe05835018e673452c4a31485978c512afe3
Louis Kraemer <louisk@bam.tech>
2021-01-11 13:53:36 +0100

    :fire: (ActivitySearchFilter) remove mobile component

--- frontend/src/components/ActivitySearchFilterMobile/useActivitySearchFilterMobile.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export const useActivitySearchFilterMobile = () => {
-  const [selectedActivity, updateSelectedActivity] = useState<string | null>(null);
-  return {
-    selectedActivity,
-    updateSelectedActivity,
-  };
-};
