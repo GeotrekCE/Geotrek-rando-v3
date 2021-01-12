6d950286c62029f639dfeac4d556a1f885e8cff1
Louis Kraemer <louisk@bam.tech>
2021-01-12 15:43:23 +0100

    :sparkles: (utils) add function to get displayable filters

--- frontend/src/modules/filters/utils.ts
+++ frontend/src/modules/filters/utils.ts
@@ -1,5 +1,12 @@
 import { getDifficulties } from './connector';
-import { AvailableFilters, BaseFilters, TrekFilters } from './interface';
+import {
+  AvailableFilters,
+  BaseFilters,
+  DisplayableAvailableFilters,
+  DisplayableFilter,
+  Filter,
+  TrekFilters,
+} from './interface';
 
 export const getAvailableFilters = async (): Promise<AvailableFilters> => {
   const difficulties = await getDifficulties();
@@ -64,5 +71,33 @@ export const getAvailableFilters = async (): Promise<AvailableFilters> => {
       label: `search.filters.${TrekFilters.POSITIVE_ELEVATION}`,
       choices: {},
     },
+  };
+};
+
+const adaptAvailableFilters = (
+  filter: Filter,
+): { label: string; options: DisplayableFilter[] } => ({
+  label: filter.label,
+  options: Object.keys(filter.choices).map(difficultyId => ({
+    value: difficultyId,
+    label: filter.choices[difficultyId].label,
+  })),
+});
+
+export const getDisplayableAvailableFilters = async (): Promise<DisplayableAvailableFilters> => {
+  const availableFilters = await getAvailableFilters();
+  return {
+    [BaseFilters.ACTIVITIES]: adaptAvailableFilters(availableFilters[BaseFilters.ACTIVITIES]),
+    [BaseFilters.CITY]: adaptAvailableFilters(availableFilters[BaseFilters.CITY]),
+    [BaseFilters.DISTRICT]: adaptAvailableFilters(availableFilters[BaseFilters.DISTRICT]),
+    [BaseFilters.THEME]: adaptAvailableFilters(availableFilters[BaseFilters.THEME]),
+    [TrekFilters.DIFFICULTY]: adaptAvailableFilters(availableFilters[TrekFilters.DIFFICULTY]),
+    [TrekFilters.COURSE_TYPE]: adaptAvailableFilters(availableFilters[TrekFilters.COURSE_TYPE]),
+    [TrekFilters.ACCESSIBILITY]: adaptAvailableFilters(availableFilters[TrekFilters.ACCESSIBILITY]),
+    [TrekFilters.DURATION]: adaptAvailableFilters(availableFilters[TrekFilters.DURATION]),
+    [TrekFilters.LENGTH]: adaptAvailableFilters(availableFilters[TrekFilters.LENGTH]),
+    [TrekFilters.POSITIVE_ELEVATION]: adaptAvailableFilters(
+      availableFilters[TrekFilters.POSITIVE_ELEVATION],
+    ),
   };
 };
