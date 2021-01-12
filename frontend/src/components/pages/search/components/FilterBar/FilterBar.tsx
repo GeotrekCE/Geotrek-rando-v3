8ff863bb3a0bf40e53df919cfcceb731b153cfa6
Loïc Chau <loicch@bam.tech>
2021-01-12 15:09:06 +0100

    fix(FilterBar): Multi line layout

--- frontend/src/components/pages/search/components/FilterBar/FilterBar.tsx
+++ frontend/src/components/pages/search/components/FilterBar/FilterBar.tsx
@@ -8,6 +8,7 @@ import {
 import { useState } from 'react';
 import styled from 'styled-components';
 import { getSpacing, sizes } from 'stylesheet';
+import { flexGap } from 'services/cssHelpers';
 
 import { useHideOnScrollDown } from 'hooks/useHideOnScrollDown';
 import { Button } from 'components/Button';
@@ -29,53 +30,65 @@ export const FilterBar: React.FC<Props> = props => {
 
   const filterBarDisplayedState = useHideOnScrollDown(sizes.desktopHeader);
 
-  const filterBarContainerClassName = `w-full py-3 pl-5 pr-2 hidden desktop:block fixed shadow bg-white z-floatingButton ${
+  const filterBarContainerClassName = `w-full py-3 pl-6 pr-2 hidden desktop:block fixed shadow bg-white z-floatingButton ${
     filterBarExpansionState === 'COLLAPSED' ? 'h-filterBar' : ''
-  }`;
-  const additionalFiltersSectionClassName = `flex flex-wrap mt-4 ${
-    filterBarExpansionState === 'COLLAPSED' ? 'hidden' : ''
   }`;
 
   return (
     <Container className={filterBarContainerClassName} displayedState={filterBarDisplayedState}>
-      <div className="flex">
-        <Filter />
-        <Filter />
-        <Filter />
-        <SeeMoreButton
-          icon={Plus}
-          onClick={() => setFilterBarExpansionState('EXPANDED')}
-          filterBarState={filterBarExpansionState}
-        >
-          Voir plus
-        </SeeMoreButton>
+      <div className={`${filterBarExpansionState === 'EXPANDED' ? 'mb-4' : 'h-filterBar'}`}>
+        <FiltersLayout>
+          <Filter />
+          <Filter />
+          <Filter />
+          <SeeMoreButton
+            icon={Plus}
+            onClick={() => setFilterBarExpansionState('EXPANDED')}
+            filterBarState={filterBarExpansionState}
+          >
+            Voir plus
+          </SeeMoreButton>
+        </FiltersLayout>
       </div>
-      <div className={additionalFiltersSectionClassName}>
-        <Filter />
-        <Filter />
-        <Filter />
-        <Filter />
-        <Filter />
-        <Filter />
-        <CollapseFiltersButton collapseFilters={() => setFilterBarExpansionState('COLLAPSED')} />
-      </div>
+      <AdditionalFilters expansionState={filterBarExpansionState}>
+        <FiltersLayout>
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <Filter />
+          <CollapseFiltersButton collapseFilters={() => setFilterBarExpansionState('COLLAPSED')} />
+        </FiltersLayout>
+      </AdditionalFilters>
     </Container>
   );
 };
 
 const Filter = () => (
-  <div className="mx-1">
-    <SelectableDropdown
-      name="difficulties"
-      placeholder="Difficulté"
-      options={[
-        { value: 'veryEasy', label: 'Très facile' },
-        { value: 'easy', label: 'Facile' },
-        { value: 'medium', label: 'Moyen' },
-        { value: 'hard', label: 'Difficile' },
-      ]}
-    />
-  </div>
+  <SelectableDropdown
+    name="difficulties"
+    placeholder="Difficulté"
+    options={[
+      { value: 'veryEasy', label: 'Très facile' },
+      { value: 'easy', label: 'Facile' },
+      { value: 'medium', label: 'Moyen' },
+      { value: 'hard', label: 'Difficile' },
+    ]}
+  />
 );
 
 const Container = styled.div<{ displayedState: 'DISPLAYED' | 'HIDDEN' }>`
@@ -86,10 +99,17 @@ const Container = styled.div<{ displayedState: 'DISPLAYED' | 'HIDDEN' }>`
       : -sizes.desktopHeader - sizes.filterBar}px;
 `;
 
-const SeeMoreButton = styled(Button)<{ filterBarState: 'EXPANDED' | 'COLLAPSED' }>`
-  margin: 0 ${getSpacing(1)};
+const FiltersLayout = styled.div`
+  ${flexGap(getSpacing(2), getSpacing(4))}
+`;
 
+const SeeMoreButton = styled(Button)<{ filterBarState: 'EXPANDED' | 'COLLAPSED' }>`
   ${({ filterBarState }) => filterBarState === 'EXPANDED' && 'display: none'};
+`;
+
+const AdditionalFilters = styled.div<{ expansionState: 'EXPANDED' | 'COLLAPSED' }>`
+  ${({ expansionState }) => (expansionState === 'EXPANDED' ? 'display: flex' : 'display: none')};
+  flex-wrap: wrap;
 `;
 
 const CollapseFiltersButton = ({ collapseFilters }: { collapseFilters: () => void }) => (
