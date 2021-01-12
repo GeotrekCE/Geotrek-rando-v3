8cea6cdbd684b296360e460d68ade0c1a26269e2
Louis Kraemer <louisk@bam.tech>
2021-01-12 09:57:59 +0100

    :recycle: (activities/api) clean code

--- frontend/src/modules/activities/api.ts
+++ frontend/src/modules/activities/api.ts
@@ -2,23 +2,6 @@ import { GeotrekAPI } from 'services/api/client';
 import { APIQuery, APIResponseForList } from 'services/api/interface';
 import { RawListActivity } from './interface';
 
-export const fetchActivities = async (
-  query: APIQuery,
-): Promise<APIResponseForList<RawListActivity>> => {
-  const apiQuery: APIQuery = {
-    language: query.language,
-  };
-  if (query.fields !== undefined) {
-    apiQuery.fields = query.fields;
-  }
-  if (query.omit !== undefined) {
-    apiQuery.omit = query.omit;
-  }
-  if (query.page !== undefined) {
-    apiQuery.page = query.page;
-  }
-  if (query.page_size !== undefined) {
-    apiQuery.page_size = query.page_size;
-  }
-  return GeotrekAPI.url('/practice').query(apiQuery).get().json();
+export const fetchActivities = (query: APIQuery): Promise<APIResponseForList<RawListActivity>> => {
+  return GeotrekAPI.url('/practice').query(query).get().json();
 };
