fbd5ed538c6608cc323c072c1fa0d1c65cb1b4b4
Loïc Chau <loicch@bam.tech>
2021-01-12 14:19:00 +0100

    refacto(cssHelpers): Customize flexGap for x and y gap

--- frontend/src/services/cssHelpers.ts
+++ frontend/src/services/cssHelpers.ts
@@ -4,15 +4,16 @@ import { css, FlattenSimpleInterpolation } from 'styled-components';
  * Emulates flex-gap which has yet to be implemented on every browser
  * https://coryrylan.com/blog/css-gap-space-with-flexbox
  */
-export const flexGap = (gap: string): FlattenSimpleInterpolation => css`
-  --gap: ${gap};
+export const flexGap = (gapX: string, gapY?: string): FlattenSimpleInterpolation => css`
+  --gapX: ${gapX};
+  --gapY: ${gapY ?? gapX};
   display: inline-flex;
   flex-wrap: wrap;
-  margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
-  width: calc(100% + var(--gap));
+  margin: calc(-1 * var(--gapY)) 0 0 calc(-1 * var(--gapX));
+  width: calc(100% + var(--gapX));
 
   & > * {
-    margin: var(--gap) 0 0 var(--gap);
+    margin: var(--gapY) 0 0 var(--gapX);
   }
 `;
 
