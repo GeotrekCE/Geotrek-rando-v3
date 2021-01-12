c7ee532715cee1f84bfe001eb6648fce4aaff367
Louis Kraemer <louisk@bam.tech>
2021-01-12 14:49:49 +0100

    :sparkles: (filters) hide filters when no options are available

--- frontend/src/components/pages/search/components/FilterBar/FilterBar.tsx
+++ frontend/src/components/pages/search/components/FilterBar/FilterBar.tsx
@@ -16,15 +16,17 @@ interface Props {
 export const FilterBar: React.FC<Props> = props => {
   return (
     <div className="w-full py-3 pl-6 pr-2 hidden desktop:flex shadow">
-      <SelectableDropdown
-        name={TrekFilters.DIFFICULTY}
+      {props.availableFilters[TrekFilters.DIFFICULTY].options.length > 0 && (
+        <SelectableDropdown
+          name={TrekFilters.DIFFICULTY}
           placeholder={props.availableFilters[TrekFilters.DIFFICULTY].label}
           options={props.availableFilters[TrekFilters.DIFFICULTY].options}
-        setFilterValues={(values: DisplayableFilter[]) =>
-          props.setFilterValues(TrekFilters.DIFFICULTY, values)
-        }
-        selectedFilters={props.selectedFilters[TrekFilters.DIFFICULTY]}
-      />
+          setFilterValues={(values: DisplayableFilter[]) =>
+            props.setFilterValues(TrekFilters.DIFFICULTY, values)
+          }
+          selectedFilters={props.selectedFilters[TrekFilters.DIFFICULTY]}
+        />
+      )}
     </div>
   );
 };
