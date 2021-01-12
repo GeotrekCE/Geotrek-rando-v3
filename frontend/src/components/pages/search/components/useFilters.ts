6fc7ce87a0120e26258a13b0355e8e80f5d9b6e2
Louis Kraemer <louisk@bam.tech>
2021-01-12 10:00:09 +0100

    :sparkles: (filters) add ccustom hook to handle filters

---
+++ frontend/src/components/pages/search/components/useFilters.ts
@@ -0,0 +1,60 @@
+import {
+  BaseFilters,
+  DisplayableAvailableFilters,
+  DisplayableFilter,
+  RangeFilters,
+  SelectedFilters,
+  TrekFilters,
+} from 'modules/filters/interface';
+import { getAvailableFilters } from 'modules/filters/utils';
+import { useReducer } from 'react';
+import { useQuery } from 'react-query';
+import { filterReducer, setFilterValuesAction } from './filterReducer';
+
+export const useFilter = () => {
+  const { data: availableFilters } = useQuery('filters', getAvailableFilters);
+
+  const initialState: SelectedFilters = {
+    [BaseFilters.ACTIVITIES]: [],
+    [BaseFilters.CITY]: [],
+    [BaseFilters.DISTRICT]: [],
+    [BaseFilters.THEME]: [],
+    [TrekFilters.DIFFICULTY]: [],
+    [TrekFilters.COURSE_TYPE]: [],
+    [TrekFilters.ACCESSIBILITY]: [],
+    [RangeFilters.DURATION]: [],
+    [RangeFilters.LENGTH]: [],
+    [RangeFilters.POSITIVE_ELEVATION]: [],
+  };
+
+  const [selectedFilters, dispatch] = useReducer(filterReducer, initialState);
+
+  const setFilterValues = (
+    filter: BaseFilters | TrekFilters | RangeFilters,
+    values: DisplayableFilter[],
+  ) => dispatch(setFilterValuesAction(filter, values));
+
+  const displayableAvailableFilters: DisplayableAvailableFilters = {
+    [BaseFilters.ACTIVITIES]: [],
+    [BaseFilters.CITY]: [],
+    [BaseFilters.DISTRICT]: [],
+    [BaseFilters.THEME]: [],
+    [TrekFilters.DIFFICULTY]: availableFilters
+      ? Object.keys(availableFilters[TrekFilters.DIFFICULTY].choices).map(difficultyId => ({
+          value: difficultyId,
+          label: availableFilters[TrekFilters.DIFFICULTY].choices[difficultyId].label,
+        }))
+      : [],
+    [TrekFilters.COURSE_TYPE]: [],
+    [TrekFilters.ACCESSIBILITY]: [],
+    [RangeFilters.DURATION]: [],
+    [RangeFilters.LENGTH]: [],
+    [RangeFilters.POSITIVE_ELEVATION]: [],
+  };
+
+  return {
+    displayableAvailableFilters,
+    setFilterValues,
+    selectedFilters,
+  };
+};
