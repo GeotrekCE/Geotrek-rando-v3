baf33180a1b46de4fda330d8c6350b0d1d72fdd6
Loïc Chau <loicch@bam.tech>
2021-01-13 16:42:16 +0100

    chore(TrekResults): Specify results number

--- frontend/src/modules/results/connector.ts
+++ frontend/src/modules/results/connector.ts
@@ -4,9 +4,12 @@ import { adaptTrekResults } from './adapter';
 import { fetchTrekResults } from './api';
 import { TrekResults } from './interface';
 
+// TODO it should come from the config
+const resultsNumber = 5;
+
 export const getTrekResults = async (): Promise<TrekResults> => {
   const [rawTrekResults, difficulties, tags] = await Promise.all([
-    fetchTrekResults({ language: 'fr' }),
+    fetchTrekResults({ language: 'fr', page_size: resultsNumber }),
     getDifficulties(),
     getTags(),
   ]);
