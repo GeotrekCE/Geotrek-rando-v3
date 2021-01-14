2aaeb82047853ae5d7456ec9d9149d8720e03d39
Louis Kraemer <louisk@bam.tech>
2021-01-14 10:35:05 +0100

    :sparkles: (home) add config for home page

---
+++ frontend/src/modules/home/interface.ts
@@ -0,0 +1,31 @@
+interface ActivityBar {
+  shouldDisplay: boolean;
+}
+
+interface Header {
+  logo: string;
+  structureName: string;
+  menu: Menu;
+}
+
+interface MenuItem {
+  name: string;
+  url: string;
+}
+
+interface Menu {
+  primaryItemsNumber: number;
+  items: MenuItem[];
+  supportedLanguage: string[];
+}
+
+interface PictureAndText {
+  pictureUrl: string;
+  text: string;
+}
+
+export interface HomePageConfig {
+  header: Header;
+  pictureAndText: PictureAndText;
+  activityBar: ActivityBar;
+}
