190d92ec96969b1872ad1e9c26db70a1487a0fb1
Loïc Chau <loicch@bam.tech>
2021-01-14 10:29:59 +0100

    chore(ThemesModule): Add getThemes connector

--- frontend/src/modules/filters/theme/connector.ts
+++ frontend/src/modules/filters/theme/connector.ts
@@ -1,7 +1,13 @@
-import { adaptThemeFilter } from './adapter';
+import { Choices } from '../interface';
+import { adaptThemeFilter, adaptThemes } from './adapter';
 import { fetchThemes } from './api';
 
 export const getThemeFilter = async () => {
   const rawCities = await fetchThemes({ language: 'fr' });
   return adaptThemeFilter(rawCities.results);
+};
+
+export const getThemes = async (): Promise<Choices> => {
+  const rawThemes = await fetchThemes({ language: 'fr' });
+  return adaptThemes(rawThemes.results);
 };
