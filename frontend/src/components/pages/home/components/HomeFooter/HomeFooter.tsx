9c464492cbe4ce2152bd2e8891564999dd903791
Delphine Bugner <delphineb@bam.tech>
2021-01-14 14:00:13 +0100

    :bug: (home footer) fix SocialNetworks position

--- frontend/src/components/pages/home/components/HomeFooter/HomeFooter.tsx
+++ frontend/src/components/pages/home/components/HomeFooter/HomeFooter.tsx
@@ -5,18 +5,18 @@ export const HomeFooter: React.FC = () => {
   return (
     <div className="bg-black flex flex-col">
       <div className="mx-4 desktop:mx-40">
+        <SocialNetworks
+          socialNetworkList={[
+            { id: 'facebook', url: 'https://www.facebook.com' },
+            { id: 'twitter', url: 'https://www.twitter.com' },
+            { id: 'youtube', url: 'https://www.youtube.com' },
+          ]}
+        />
         <div
           className="
           flex flex-col desktop:block
           pt-3 pb-10 desktop:pt-10 desktop:pb-18"
         >
-          <SocialNetworks
-            socialNetworkList={[
-              { id: 'facebook', url: 'https://www.facebook.com' },
-              { id: 'twitter', url: 'https://www.twitter.com' },
-              { id: 'youtube', url: 'https://www.youtube.com' },
-            ]}
-          />
           <PortalContact
             name="Parc National des Écrins"
             addressLine1="Domaine de Charance"
