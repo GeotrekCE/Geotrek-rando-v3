0864512e0a3f6355926a5a58d806b3c9a4097a98
Delphine Bugner <delphineb@bam.tech>
2021-01-15 18:26:31 +0100

    :white_check_mark: (home) add ActivitySuggestion api integration test

--- frontend/src/components/pages/home/__tests__/Home.test.tsx
+++ frontend/src/components/pages/home/__tests__/Home.test.tsx
@@ -1,14 +1,31 @@
-import { render } from 'services/testing/reactTestingLibraryWrapper';
+import nock from 'nock';
+import { render, waitFor } from 'services/testing/reactTestingLibraryWrapper';
+import 'isomorphic-fetch';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Home } from '../';
+import { mockActivitySuggestionsResponse } from '../mocks';
 
-test('AAU, I can see the home page', () => {
+describe('Home page', () => {
   const queryClient = new QueryClient();
-  render(
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>,
-  );
+  it('AAU, I can see an activity suggestion', async () => {
+    process.env.REACT_APP_API_BASE_URL = 'https://geotrekdemo.ecrins-parcnational.fr/api/v2';
+
+    nock(process.env.REACT_APP_API_BASE_URL)
+      .get('/trek')
+      .query({
+        fields: 'name,thumbnail',
+        language: 'fr',
+        page_size: 5,
+      })
+      .reply(200, mockActivitySuggestionsResponse);
+
+    const home = render(
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>,
+    );
+    await home.findAllByText('Col de Font Froide');
+  });
 });
