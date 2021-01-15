035a95af6d77427fd023b08907cb2744b3b0e2f7
Delphine Bugner <delphineb@bam.tech>
2021-01-15 11:28:58 +0100

    :sparkles: (useHome) add ActivitySuggestions query in hook

--- frontend/src/components/pages/home/useHome.ts
+++ frontend/src/components/pages/home/useHome.ts
@@ -1,6 +1,14 @@
 import { getHomePageConfig } from 'modules/home/utils';
+import { ActivitySuggestion } from 'modules/activitySuggestions/interface';
+import { getActivitySuggestions } from 'modules/activitySuggestions/connector';
+import { useQuery } from 'react-query';
 
 export const useHome = () => {
+  const { data: activitySuggestions } = useQuery<ActivitySuggestion[], Error>(
+    'activitySuggestions',
+    getActivitySuggestions,
+  );
   const homePageConfig = getHomePageConfig();
-  return { config: homePageConfig };
+
+  return { config: homePageConfig, activitySuggestions };
 };
