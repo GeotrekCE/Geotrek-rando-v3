58fb727a260128452091434accc64cec1c4206c0
Loïc Chau <loicch@bam.tech>
2021-01-15 16:29:39 +0100

    refacto(ResultsModule): Query TrekResults according to filters parameters

--- frontend/src/modules/results/connector.ts
+++ frontend/src/modules/results/connector.ts
@@ -1,16 +1,23 @@
 import { getActivities } from 'modules/activities/connector';
 import { getDifficulties } from 'modules/filters/connector';
 import { getThemes } from 'modules/filters/theme/connector';
+import { QueryFilterState } from 'components/pages/search/utils';
+
 import { adaptTrekResults } from './adapter';
 import { fetchTrekResults } from './api';
 import { TrekResults } from './interface';
+import { formatFiltersToUrlParams } from './utils';
 
 // TODO it should come from the config
 const resultsNumber = 5;
 
-export const getTrekResults = async (): Promise<TrekResults> => {
+export const getTrekResults = async (filtersState: QueryFilterState[]): Promise<TrekResults> => {
   const [rawTrekResults, difficulties, themes, activities] = await Promise.all([
-    fetchTrekResults({ language: 'fr', page_size: resultsNumber }),
+    fetchTrekResults({
+      language: 'fr',
+      page_size: resultsNumber,
+      ...formatFiltersToUrlParams(filtersState),
+    }),
     getDifficulties(),
     getThemes(),
     getActivities(),
