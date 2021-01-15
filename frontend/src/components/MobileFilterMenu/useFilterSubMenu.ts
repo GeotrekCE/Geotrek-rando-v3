062e14f4c71d1c942e12980c49af38d9e36cecb4
Louis Kraemer <louisk@bam.tech>
2021-01-15 08:31:09 +0100

    :sparkles: (filter) add filter options to mobile

--- frontend/src/components/MobileFilterMenu/useFilterSubMenu.ts
+++ frontend/src/components/MobileFilterMenu/useFilterSubMenu.ts
@@ -1,11 +1,16 @@
+import { FilterState } from 'modules/filters/interface';
 import { useState } from 'react';
 
-export const useFilterSubMenu = () => {
+export const useFilterSubMenu = (filtersState: FilterState[]) => {
   const [currentFilterId, setCurrentFilterId] = useState<string | null>(null);
   const hideSubMenu = () => setCurrentFilterId(null);
-  const selectFilter = (filterid: string) => setCurrentFilterId(filterid);
+  const selectFilter = (filterId: string) => setCurrentFilterId(filterId);
 
   const subMenuState: 'DISPLAYED' | 'HIDDEN' = currentFilterId === null ? 'HIDDEN' : 'DISPLAYED';
 
-  return { currentFilterId, subMenuState, hideSubMenu, selectFilter };
+  const filteredFiltersState = filtersState.filter(({ id }) => id === currentFilterId);
+
+  const currentFilterState = filteredFiltersState.length > 0 ? filteredFiltersState[0] : null;
+
+  return { currentFilterId, subMenuState, hideSubMenu, selectFilter, currentFilterState };
 };
