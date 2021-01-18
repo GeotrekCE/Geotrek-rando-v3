01a98fcb404b32fb6e02c0fefd969c7591fb2ff3
Delphine Bugner <delphineb@bam.tech>
2021-01-18 15:26:19 +0100

    :bug: (ResultCard test) make tests independant

--- frontend/src/components/pages/search/components/ResultCard/__tests__/ResultCard.test.tsx
+++ frontend/src/components/pages/search/components/ResultCard/__tests__/ResultCard.test.tsx
@@ -6,25 +6,45 @@ describe('Results Card', () => {
   const id = 2;
   const title = 'Balade au pays des menhirs';
 
-  const component = render(
-    <ResultCard
-      id={id}
-      place="Saint-Etienne-du-Valdonnez"
-      title={title}
-      tags={['En famille', 'Ciel étoilé', 'Beau paysage']}
-      thumbnailUri=""
-      badgeIconUri=""
-      informations={{
-        duration: '2h',
-        distance: '5km',
-        elevation: '+360m',
-        difficulty: { label: '', pictogramUri: '' },
-        reservationSystem: 1,
-      }}
-    />,
-  );
-  it('AAU, I can see a ResultCard', () => expect(component).toMatchSnapshot());
+  it('AAU, I can see a ResultCard', () => {
+    const component = render(
+      <ResultCard
+        id={id}
+        place="Saint-Etienne-du-Valdonnez"
+        title={title}
+        tags={['En famille', 'Ciel étoilé', 'Beau paysage']}
+        thumbnailUri=""
+        badgeIconUri=""
+        informations={{
+          duration: '2h',
+          distance: '5km',
+          elevation: '+360m',
+          difficulty: { label: '', pictogramUri: '' },
+          reservationSystem: 1,
+        }}
+      />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+
   it('AAU, I will be redirected to the right details page', async () => {
+    const component = render(
+      <ResultCard
+        id={id}
+        place="Saint-Etienne-du-Valdonnez"
+        title={title}
+        tags={['En famille', 'Ciel étoilé', 'Beau paysage']}
+        thumbnailUri=""
+        badgeIconUri=""
+        informations={{
+          duration: '2h',
+          distance: '5km',
+          elevation: '+360m',
+          difficulty: { label: '', pictogramUri: '' },
+          reservationSystem: 1,
+        }}
+      />,
+    );
     const links = await component.findAllByTestId(`Link-ResultCard-${id}`);
     links.forEach(link =>
       expect(link).toHaveAttribute('href', `/details-${id}-${encodeURI(title)}`),
