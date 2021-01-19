450069bd6854ce8d931db9bde4ad4f9a13799585
Delphine Bugner <delphineb@bam.tech>
2021-01-19 16:18:33 +0100

    :white_check_mark: (Details) add mock and test on Details

---
+++ frontend/src/components/pages/details/Details.mocks.ts
@@ -0,0 +1,24 @@
+export const rawDetails = {
+  access:
+    "A La Fare en Champsaur (N 85), prendre la direction de Saint-Bonnet puis de La Motte (D23). Traverser La Motte et continuer jusqu'au hameau de Molines.",
+  advised_parking: 'Parking de Molines (avant le hameau en été)',
+  departure: 'Molines-en-Champsaur',
+  name: 'Col de Font Froide',
+  practice: 4,
+  public_transport:
+    'Entre Gap et La Motte-en-Champsaur, possibilit&eacute; de correspondance avec le car Gap-Grenoble.',
+  thumbnail: {
+    author: 'Dominique Vincent - PNE',
+    title: 'le-depart-du-hameau-de-molines',
+    legend: 'Le départ du hameau de Molines',
+    url:
+      'https://geotrekdemo.ecrins-parcnational.fr/media/paperclip/trekking_trek/2/le-depart-du-hameau-de-molines.JPG',
+  },
+};
+
+export const rawActivity = {
+  id: 4,
+  name: 'Pédestre',
+  order: null,
+  pictogram: 'https://geotrekdemo.ecrins-parcnational.fr/media/upload/practice-foot_GpBv9u1.svg',
+};
