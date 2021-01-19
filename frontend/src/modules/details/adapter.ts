40af0ea2574cf8b2eb1613ca285bce90f7dd1b52
Delphine Bugner <delphineb@bam.tech>
2021-01-19 17:43:10 +0100

    :sparkles: (details adapter) concatenate access and parking with

--- frontend/src/modules/details/adapter.ts
+++ frontend/src/modules/details/adapter.ts
@@ -7,11 +7,16 @@ export const adaptResults = ({
 }: {
   rawDetails: RawDetails;
   activity: Activity;
-}): Details => ({
-  title: rawDetails.name,
-  place: rawDetails.departure,
-  imgUrl: rawDetails.thumbnail.url,
-  practice: activity,
-  transport: rawDetails.public_transport,
-  access_parking: rawDetails.access + ' -- ' + rawDetails.advised_parking,
-});
+}): Details => {
+  return {
+    title: rawDetails.name,
+    place: rawDetails.departure,
+    imgUrl: rawDetails.thumbnail.url,
+    practice: activity,
+    transport: rawDetails.public_transport,
+    access_parking:
+      rawDetails.access.length > 0 && rawDetails.advised_parking.length > 0
+        ? `${rawDetails.access}\n${rawDetails.advised_parking}`
+        : `${rawDetails.access}${rawDetails.advised_parking}`,
+  };
+};
