50b32ca10eef4be283a75d14eaba8771cca8afde
Delphine Bugner <delphineb@bam.tech>
2021-01-20 15:57:42 +0100

    :sparkles: (Details utils) add italic font in parsed html

--- frontend/src/components/pages/details/utils.tsx
+++ frontend/src/components/pages/details/utils.tsx
@@ -1,12 +1,23 @@
+import styled from 'styled-components';
 import parse from 'html-react-parser';
 import { Details, DetailsString } from 'modules/details/interface';
 
 export const checkAndParse = (
   details: Details | undefined,
   field: keyof DetailsString,
-): [boolean, string | JSX.Element | JSX.Element[]] => {
+): [boolean, JSX.Element] => {
   const isValid =
     details !== undefined && details[field] !== undefined && details[field].length > 0;
-  const content = details !== undefined ? parse(details[field] ?? '') : '';
-  return [isValid, content];
+  const parsedHtmlContent = details !== undefined ? parse(details[field] ?? '') : '';
+  const styledHtmlContent = <HtmlText>{parsedHtmlContent}</HtmlText>;
+  return [isValid, styledHtmlContent];
 };
+
+const HtmlText = styled.div`
+  & > em {
+    font-style: italic;
+  }
+  & > p > em {
+    font-style: italic;
+  }
+`;
