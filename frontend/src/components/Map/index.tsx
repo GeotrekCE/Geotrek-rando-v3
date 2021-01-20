af40c430b49127ed59fbb608f2d6b7610372ba75
Louis Kraemer <louisk@bam.tech>
2021-01-20 12:38:50 +0100

    :bug: (map) remove loading text

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -7,8 +7,6 @@ export const MapDynamicComponent = (props: {
   segments?: TreksList | null;
 }) => {
   const Map = dynamic(() => import('./Map'), {
-    // eslint-disable-next-line react/display-name
-    loading: () => <p>Loading a map</p>,
     ssr: false,
   });
   return <Map {...props} />;
