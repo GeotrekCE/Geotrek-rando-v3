e730dad45e490731b81fc0ef03fd13569cc966c7
Louis Kraemer <louisk@bam.tech>
2021-01-20 12:00:57 +0100

    :white_check_mark: (config) mock the map component

--- frontend/jestAfterEnv.setup.tsx
+++ frontend/jestAfterEnv.setup.tsx
@@ -3,3 +3,7 @@ import '@testing-library/jest-dom';
 jest.mock('./src/services/envLoader.ts', () => ({
   getApiUrl: () => 'https://geotrekdemo.ecrins-parcnational.fr/api/v2',
 }));
+
+jest.mock('./src/components/Map', () => ({
+  MapDynamicComponent: () => null,
+}));
