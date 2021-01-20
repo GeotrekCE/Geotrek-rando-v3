4ac07585da2c6b94109751645d1b31afdb1fad01
Delphine Bugner <delphineb@bam.tech>
2021-01-20 14:08:35 +0100

    :sparkles: (useDetails) add function checkAndParse for string fields

--- frontend/src/components/pages/details/useDetails.tsx
+++ frontend/src/components/pages/details/useDetails.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
-import { Details } from 'modules/details/interface';
+import { Details, DetailsString } from 'modules/details/interface';
 import { getDetails } from 'modules/details/connector';
+import parse from 'html-react-parser';
 
 const isUrlString = (url: string | string[] | undefined): url is string =>
   url !== undefined && typeof url === 'string';
@@ -10,5 +11,14 @@ export const useDetails = (detailsUrl: string | string[] | undefined) => {
   const { data } = useQuery<Details, Error>('details', () => getDetails(id), {
     enabled: isUrlString(detailsUrl),
   });
-  return { details: data };
+  const checkAndParse = (
+    details: Details | undefined,
+    field: keyof DetailsString,
+  ): [boolean, string | JSX.Element | JSX.Element[]] => {
+    const isValid =
+      details !== undefined && details[field] !== undefined && details[field].length > 0;
+    const content = details !== undefined ? parse(details[field] ?? '') : '';
+    return [isValid, content];
+  };
+  return { details: data, checkAndParse };
 };
