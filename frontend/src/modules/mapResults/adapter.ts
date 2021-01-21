0abf7782423e52a86725c4953bce2bfce45343e0
Loïc Chau <loicch@bam.tech>
2021-01-21 16:15:13 +0100

    feature(mapResultsModule): Fetch all trek results locations

--- frontend/src/modules/mapResults/adapter.ts
+++ frontend/src/modules/mapResults/adapter.ts
@@ -1,8 +1,8 @@
 import { MapResults, RawMapResults } from './interface';
-import { formatLocation } from './utils';
+import { concatMapResults, formatLocation } from './utils';
 
-export const adaptMapResults = (rawMapResults: RawMapResults): MapResults =>
-  rawMapResults.results.map(rawMapResult => ({
+export const adaptMapResults = (rawMapResults: RawMapResults[]): MapResults =>
+  concatMapResults(rawMapResults).map(rawMapResult => ({
     id: rawMapResult.id,
     location: formatLocation(rawMapResult.parking_location),
   }));
