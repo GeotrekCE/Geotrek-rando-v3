a722f6bf6243f2502fd2e8e0cbd1c9c2df667925
Delphine Bugner <delphineb@bam.tech>
2021-01-21 14:44:47 +0100

    :bug: (modules/difficulty) missing slash in route

--- frontend/src/modules/filters/difficulties/api.ts
+++ frontend/src/modules/filters/difficulties/api.ts
@@ -6,4 +6,4 @@ export const fetchDifficulties = (query: APIQuery): Promise<APIResponseForList<R
   GeotrekAPI.url('/difficulty').query(query).get().json();
 
 export const fetchDifficulty = (query: APIQuery, id: number): Promise<RawDifficulty> =>
-  GeotrekAPI.url(`/difficulty/${id}`).query(query).get().json();
+  GeotrekAPI.url(`/difficulty/${id}/`).query(query).get().json();
