a84cd723aadcbb858f0abd52e5d845758c044adb
Louis Kraemer <louisk@bam.tech>
2021-01-21 16:45:48 +0100

    :sparkles: (Home) add description to head

--- frontend/src/components/PageHead.tsx
+++ frontend/src/components/PageHead.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head';
 
 interface Props {
   title?: string;
+  description?: string;
 }
 
-export const PageHead = ({ title }: Props) => (
+export const PageHead = ({ title, description }: Props) => (
   <Head>
     <title>{title ?? 'GeoTrek'}</title>
+    <meta name="description" content={description ?? 'Geotrek rando'} />
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
