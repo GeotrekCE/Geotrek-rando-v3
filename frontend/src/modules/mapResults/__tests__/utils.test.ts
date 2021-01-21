480daf2d893a471dcc83d3cabd6c60751363a1a4
Loïc Chau <loicch@bam.tech>
2021-01-21 15:19:28 +0100

    chore(mapResultsModule): Create utils to compute results pages urls

--- frontend/src/modules/mapResults/__tests__/utils.test.ts
+++ frontend/src/modules/mapResults/__tests__/utils.test.ts
@@ -1,4 +1,61 @@
-import { formatLocation } from '../utils';
+import { computePageCount, formatLocation, generateNextUrl, generatePagesUrls } from '../utils';
+
+describe('computeNumberOfPages', () => {
+  it('should correctly compute number of pages for an exact division', () => {
+    const pageSize = 5;
+    const resultsCount = 15;
+    const output = computePageCount(pageSize, resultsCount);
+    const expected = 3;
+
+    expect(output).toBe(expected);
+  });
+
+  it('should correctly compute number of pages for a partial division', () => {
+    const pageSize = 5;
+    const resultsCount = 12;
+    const output = computePageCount(pageSize, resultsCount);
+    const expected = 3;
+
+    expect(output).toBe(expected);
+  });
+});
+describe('generateAllPagesUrls', () => {
+  it('should generate a list of results pages', () => {
+    const input =
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=2&page_size=5';
+    const output = generatePagesUrls(input, 4);
+    const expected = [
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=1&page_size=5',
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=2&page_size=5',
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=3&page_size=5',
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=4&page_size=5',
+    ];
+
+    expect(output).toStrictEqual(expected);
+  });
+});
+
+describe('generateNextUrl', () => {
+  it('should replace the input url page with the given page parameter', () => {
+    const input =
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=2&page_size=5';
+    const output = generateNextUrl(input, 3);
+    const expected =
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=3&page_size=5';
+
+    expect(output).toBe(expected);
+  });
+
+  it('should replace the input url page with the given page parameter with multiple numbers', () => {
+    const input =
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=9481&page_size=5';
+    const output = generateNextUrl(input, 124);
+    const expected =
+      'https://geotrekdemo.ecrins-parcnational.fr/api/v2/trek/?fields=id%2Cparking_location&language=fr&page=124&page_size=5';
+
+    expect(output).toBe(expected);
+  });
+});
 
 describe('formatLocation', () => {
   it('should transform array of coordinates into an object with explicit properties', () => {
--- frontend/src/modules/mapResults/utils.ts
+++ frontend/src/modules/mapResults/utils.ts
@@ -5,3 +5,23 @@ export const formatLocation = (rawLocation: number[] | null): { x: number; y: nu
         x: rawLocation[0],
         y: rawLocation[1],
       };
+
+/** Replace the given url page number by the given page number */
+export const generateNextUrl = (url: string, pageNumber: number): string => {
+  return url.replace(/page=[0-9]*/, `page=${pageNumber}`);
+};
+
+/** Computes the number of pages there will be given the size of one page and total result count */
+export const computePageCount = (pageSize: number, resultsCount: number): number => {
+  return Math.floor(resultsCount / pageSize) + (resultsCount % pageSize === 0 ? 0 : 1);
+};
+
+/** Generate all results pages URLs given the number of pages and an url */
+export const generatePagesUrls = (url: string, pageCount: number): string[] => {
+  const urls = [];
+  for (let pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
+    urls.push(generateNextUrl(url, pageNumber));
+  }
+
+  return urls;
+};
