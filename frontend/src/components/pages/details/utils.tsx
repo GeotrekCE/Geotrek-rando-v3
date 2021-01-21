c1b0544e61cca1bf4db3893a333edd27767bd916
Delphine Bugner <delphineb@bam.tech>
2021-01-21 13:04:43 +0100

    :sparkles: (Details/utils) add checkInformation function

--- frontend/src/components/pages/details/utils.tsx
+++ frontend/src/components/pages/details/utils.tsx
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 import parse from 'html-react-parser';
-import { Details, DetailsString } from 'modules/details/interface';
+import { Details, DetailsHtml, DetailsInformation } from 'modules/details/interface';
+import { Difficulty } from 'modules/filters/difficulties/interface';
+
+export const checkInformation = (
+  details: Details | undefined,
+  informationField: keyof DetailsInformation,
+): [boolean, string | Difficulty] => {
+  const isValid =
+    details !== undefined &&
+    details.informations !== undefined &&
+    details.informations[informationField] !== null;
+  const defaultValue = informationField === 'difficulty' ? { label: '', pictogramUri: '' } : '';
+  const content = details?.informations[informationField] ?? defaultValue;
+  return [isValid, content];
+};
 
 export const checkAndParse = (
   details: Details | undefined,
-  field: keyof DetailsString,
+  field: keyof DetailsHtml,
 ): [boolean, JSX.Element] => {
   const isValid =
     details !== undefined && details[field] !== undefined && details[field].length > 0;
