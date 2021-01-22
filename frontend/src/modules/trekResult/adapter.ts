28a2db32b814cd8088c493647979055a690d3c30
Loïc Chau <loicch@bam.tech>
2021-01-22 17:32:06 +0100

    fix(module.trekResult): Add a fallback thumbnail url

--- frontend/src/modules/trekResult/adapter.ts
+++ frontend/src/modules/trekResult/adapter.ts
@@ -1,9 +1,11 @@
 import { RawTrekPopupResult, TrekPopupResult } from './interface';
 
+const fallbackImgUri = 'https://upload.wikimedia.org/wikipedia/fr/d/df/Logo_ecrins.png';
+
 export const adaptTrekPopupResults = (rawDetails: RawTrekPopupResult): TrekPopupResult => {
   return {
     title: rawDetails.name,
     place: rawDetails.departure,
-    imgUrl: rawDetails.thumbnail.url,
+    imgUrl: rawDetails.thumbnail.url || fallbackImgUri,
   };
 };
