4fa6b37a68a16362e302952d0e7f0c682d218e8a
Delphine Bugner <delphineb@bam.tech>
2021-01-22 10:04:43 +0100

    :sparkles: (RemoteIconInformation) use css fill property to add color

--- frontend/src/components/Information/RemoteIconInformation/index.tsx
+++ frontend/src/components/Information/RemoteIconInformation/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SVG from 'react-inlinesvg';
-import { fillSvgWithColor } from 'stylesheet';
 
 import { Information } from '../BaseInformation';
 
@@ -8,23 +7,13 @@ export interface Props {
   iconUri: string;
   children: React.ReactNode;
   className?: string;
-  color?: string;
 }
 
 /**
  * Wraps BaseInformation and gives it an icon coming from a remote source (url)
  */
-export const RemoteIconInformation: React.FC<Props> = ({ iconUri, color, ...otherProps }) => {
+export const RemoteIconInformation: React.FC<Props> = ({ iconUri, ...otherProps }) => {
   return (
-    <Information
-      icon={
-        <SVG
-          src={iconUri}
-          className="w-6 h-full text-primary1"
-          preProcessor={color !== undefined ? fillSvgWithColor(color) : undefined}
-        />
-      }
-      {...otherProps}
-    />
+    <Information icon={<SVG src={iconUri} className="w-6 h-full fill-current" />} {...otherProps} />
   );
 };
