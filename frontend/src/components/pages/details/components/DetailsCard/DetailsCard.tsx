36be6902e6ffe9867369b95c917fa200b4643ea8
Delphine Bugner <delphineb@bam.tech>
2021-01-27 14:55:42 +0100

    :sparkles: (DetailsCard) connect with real data & manage scroll

--- frontend/src/components/pages/details/components/DetailsCard/DetailsCard.tsx
+++ frontend/src/components/pages/details/components/DetailsCard/DetailsCard.tsx
@@ -9,8 +9,8 @@ import { useDetailsCard } from './useDetailsCard';
 export interface DetailsCardProps {
   name: string;
   description?: string;
-  thumbnailUri: string;
-  iconUri: string;
+  thumbnailUri?: string;
+  iconUri?: string;
 }
 
 export const DetailsCard: React.FC<DetailsCardProps> = ({
@@ -30,25 +30,40 @@ export const DetailsCard: React.FC<DetailsCardProps> = ({
     <div
       className="mx-1
       w-60 desktop:w-auto
+      flex-none
       border-greySoft border border-solid
       rounded-2xl overflow-hidden
       flex flex-col desktop:flex-row
+      desktop:mb-6
       hover:shadow-sm transition-all"
     >
       <div className="relative flex-none desktop:w-2/5">
         <img
           src={thumbnailUri}
-          className="object-cover object-center h-50 w-60 desktop:h-full desktop:w-full bg-primary2"
+          className="h-50 w-60 desktop:w-full desktop:h-full
+          object-cover object-center
+           bg-greySoft"
         />
-        <ActivityBadge className="absolute top-4 left-4 bg-primary1" iconUri={iconUri} />
+        {iconUri !== null && iconUri !== undefined && (
+          <img
+            className="absolute top-4 left-4 h-8 w-8 rounded-full shadow-sm object-cover object-center"
+            src={'https://geotrekdemo.ecrins-parcnational.fr/media/upload/poi-fauna.png'}
+          />
+        )}
       </div>
       <div
-        className="flex flex-col my-auto flex-auto
-        p-2 desktop:pl-6 desktop:pr-8 desktop:py-6"
+        className={`flex flex-col
+        p-2 desktop:pl-6 desktop:pr-8 ${
+          truncateState === 'TRUNCATE' ? 'desktop:py-15' : 'desktop:py-6'
+        }`}
       >
         <p className="text-Mobile-C1 desktop:text-H4 text-primary1 font-bold">{name}</p>
         {description !== undefined && (
-          <div className="mt-1 desktop:mt-4 flex flex-col desktop:flex-row desktop:items-end text-Mobile-C2 desktop:text-P1">
+          <div
+            className="mt-1 desktop:mt-4
+            flex flex-col desktop:flex-row desktop:items-end
+            text-Mobile-C2 desktop:text-P1"
+          >
             {descriptionStyled}
             <span
               className="text-primary1 underline cursor-pointer flex-shrink-0 desktop:ml-1"
