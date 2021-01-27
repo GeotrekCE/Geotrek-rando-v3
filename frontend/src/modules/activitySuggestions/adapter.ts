8eac89d93233c6040593f70107f9cc0c353a5b38
Louis Kraemer <louisk@bam.tech>
2021-01-27 14:57:30 +0100

    :bug: (adapter/activitySugestions)

--- frontend/src/modules/activitySuggestions/adapter.ts
+++ frontend/src/modules/activitySuggestions/adapter.ts
@@ -3,5 +3,5 @@ import { ActivitySuggestion, RawActivitySuggestion } from './interface';
 export const adaptResults = (rawActivities: RawActivitySuggestion[]): ActivitySuggestion[] =>
   rawActivities.map(rawActivity => ({
     title: rawActivity.name,
-    imgUrl: rawActivity.thumbnail.url,
+    imgUrl: rawActivity.thumbnail.url ?? null,
   }));
