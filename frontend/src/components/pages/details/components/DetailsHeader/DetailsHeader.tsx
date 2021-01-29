461408e2f06efdbfc05c148a317e5fa4f8729dfc
Delphine Bugner <delphineb@bam.tech>
2021-01-29 10:22:28 +0100

    :sparkles: (Details) add download link to the 2 buttons

--- frontend/src/components/pages/details/components/DetailsHeader/DetailsHeader.tsx
+++ frontend/src/components/pages/details/components/DetailsHeader/DetailsHeader.tsx
@@ -31,13 +31,16 @@ export const DetailsHeader: React.FC<DetailsHeaderProps> = ({ sections, download
 };
 
 const DownloadButton: React.FC<{ url: string }> = ({ url }) => (
-  <div
+  <a
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
     className="rounded-full py-2 px-4 my-2
-  border-primary1 border-solid border
-  cursor-pointer
-  text-primary1 font-bold
-  hover:border-primary1-light hover:shadow-sm hover:text-primary1-light transition-all"
+    border-primary1 border-solid border
+      cursor-pointer
+    text-primary1 font-bold
+    hover:border-primary1-light hover:shadow-sm hover:text-primary1-light transition-all"
   >
     <FormattedMessage id={'details.download'} />
-  </div>
+  </a>
 );
