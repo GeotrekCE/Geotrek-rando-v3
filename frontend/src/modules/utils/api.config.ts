cc502b1b0d8f805842ec476bce6be32c20b039f9
Loïc Chau <loicch@bam.tech>
2021-02-01 14:39:06 +0100

    chore(apiConfig): Create getter for config

---
+++ frontend/src/modules/utils/api.config.ts
@@ -0,0 +1,5 @@
+import { APICallsConfig } from 'modules/interface';
+
+import apiCallsConfig from '../../../config/apiCalls.json';
+
+export const getApiCallsConfig = (): APICallsConfig => apiCallsConfig;
