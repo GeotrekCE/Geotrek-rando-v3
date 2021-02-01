7f55704c475b734d38bb3775f502db2f4a6ffd5f
Louis Kraemer <louisk@bam.tech>
2021-02-01 18:25:43 +0100

    :sparkles: (header add url redirection to mobile filter menu)

--- frontend/src/components/Header/BurgerMenuSection/useBurgerMenuSection.tsx
+++ frontend/src/components/Header/BurgerMenuSection/useBurgerMenuSection.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { useIntl } from 'react-intl';
 
 export const useBurgerMenuSection = () => {
   const [openState, setOpenState] = useState<'OPENED' | 'CLOSED'>('CLOSED');
+
+  const intl = useIntl();
+
   return {
     openState,
     setOpenState,
+    intl,
   };
 };
