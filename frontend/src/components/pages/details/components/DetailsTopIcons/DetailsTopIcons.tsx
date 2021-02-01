f37819c7ec73f92c88c8375ed593b0eb16127a2a
Delphine Bugner <delphineb@bam.tech>
2021-02-01 14:21:16 +0100

    feat (detailsTopIcon) extract component from detail page

--- frontend/src/components/pages/details/components/DetailsTopIcons/DetailsTopIcons.tsx
+++ frontend/src/components/pages/details/components/DetailsTopIcons/DetailsTopIcons.tsx
@@ -1,21 +1,45 @@
+import { Download } from 'components/Icons/Download';
+import { Printer } from 'components/Icons/Printer';
+import { Activity } from 'modules/activities/interface';
+import React from 'react';
 import SVG from 'react-inlinesvg';
-import { colorPalette, fillSvgWithColor, sizes } from 'stylesheet';
+import { colorPalette, fillSvgWithColor } from 'stylesheet';
+import { DetailsButton } from '../DetailsButton';
+import { DetailsButtonDropdown } from '../DetailsButtonDropdown';
 
 interface DetailsTopIconsProps {
   className?: string;
-  practice?: Practice;
+  practice?: Activity;
+  pdfUri?: string;
+  gpxUri?: string;
+  kmlUri?: string;
 }
 
-export const DetailsTopIcons: React.FC<DetailsTopIconsProps> = ({ className }) => {
+export const DetailsTopIcons: React.FC<DetailsTopIconsProps> = ({
+  className,
+  practice,
+  pdfUri,
+  gpxUri,
+  kmlUri,
+}) => {
+  const dropdownButtonOptions = [];
+  if (gpxUri !== undefined) dropdownButtonOptions.push({ label: 'GPX', value: gpxUri });
+  if (kmlUri !== undefined) dropdownButtonOptions.push({ label: 'KML', value: kmlUri });
+
   return (
     <div className={`${className ?? ''} flex flex-col`}>
       <div className="flex justify-between items-center">
-        {practice?.pictogram !== undefined && <ActivityLogo src={details?.practice?.pictogram} />}
-        <div className="hidden desktop:flex">
-          {details?.pdfUri !== undefined && (
-            <DetailsButton url={details.pdfUri}>
+        {practice !== undefined && <ActivityLogo src={practice.pictogram} />}
+        <div className="hidden desktop:flex space-x-4">
+          {pdfUri !== undefined && (
+            <DetailsButton url={pdfUri}>
               <Printer size={30} />
             </DetailsButton>
+          )}
+          {dropdownButtonOptions.length > 0 && (
+            <DetailsButtonDropdown options={dropdownButtonOptions}>
+              <Download className="text-primary1 m-2" size={30} />
+            </DetailsButtonDropdown>
           )}
         </div>
       </div>
