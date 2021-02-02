cd20eed2ebab27b16d5aa77c5b35548b75f0ec3c
Delphine Bugner <delphineb@bam.tech>
2021-02-02 12:40:12 +0100

    refacto (modules/mapResults) move computePageCount & generatePageNumbers to utils

--- frontend/src/modules/mapResults/utils.ts
+++ frontend/src/modules/mapResults/utils.ts
@@ -8,18 +8,8 @@ export const formatLocation = (rawLocation: number[] | null): { x: number; y: nu
         y: rawLocation[1],
       };
 
-/** Computes the number of pages there will be given the size of one page and total result count */
-export const computePageCount = (pageSize: number, resultsCount: number): number => {
-  return Math.floor(resultsCount / pageSize) + (resultsCount % pageSize === 0 ? 0 : 1);
-};
-
 export const concatMapResults = (rawMapResults: RawMapResults[]): RawMapResult[] =>
   rawMapResults.reduce<RawMapResult[]>(
     (rawResults, currentResult) => [...rawResults, ...currentResult.results],
     [],
   );
-
-/** Generates an array with the numbers of the pages in it */
-export const generatePageNumbersArray = (pageSize: number, resultsCount: number): number[] => {
-  return Array.from(Array(computePageCount(pageSize, resultsCount)), (e, i) => i + 1);
-};
