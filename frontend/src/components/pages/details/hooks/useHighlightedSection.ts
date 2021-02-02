16821073dc183df79343321cf95f058847ad76f2
Loïc Chau <loicch@bam.tech>
2021-02-02 19:20:42 +0100

    fix: always see a header highlighted when there is a section on screen

--- frontend/src/components/pages/details/hooks/useHighlightedSection.ts
+++ frontend/src/components/pages/details/hooks/useHighlightedSection.ts
@@ -14,16 +14,27 @@ export const useHighlightedSection = ({
     const handleScroll = () => {
       const scrollPosition = window.scrollY + headerHeight;
 
-      const sectionOnScreen = (Object.keys(
-        sectionsPositions,
-      ) as (keyof typeof sectionsPositions)[]).find(sectionId => {
-        const positions = sectionsPositions[sectionId];
-        if (positions) {
-          const { top, bottom } = positions;
-          if (top === undefined || bottom === undefined) return false;
-          return scrollPosition < bottom && scrollPosition > top;
-        }
-      });
+      const sectionOnScreen =
+        // First check that the scroll position is between the top and bottom of elements
+        // It will work most of the time
+        (Object.keys(sectionsPositions) as (keyof typeof sectionsPositions)[]).find(sectionId => {
+          const positions = sectionsPositions[sectionId];
+          if (positions) {
+            const { top, bottom } = positions;
+            if (top === undefined || bottom === undefined) return false;
+            return scrollPosition < bottom && scrollPosition > top;
+          }
+        }) ||
+        // If nothing has been found previously there might still be something on the screen but the scroll position is not above the bottom of it
+        // This happens at the end of the screen for example, therefore we just try to find the first element whose top is above scroll position (ie starts to appear on screen)
+        (Object.keys(sectionsPositions) as (keyof typeof sectionsPositions)[]).find(sectionId => {
+          const positions = sectionsPositions[sectionId];
+          if (positions) {
+            const { top, bottom } = positions;
+            if (top === undefined || bottom === undefined) return false;
+            return scrollPosition < top;
+          }
+        });
 
       if (sectionOnScreen !== visibleSection) {
         setVisibleSection(sectionOnScreen ?? null);
