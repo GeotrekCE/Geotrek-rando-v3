74dc99c7412b19a3612eec27b052d1cb378dddbf
Delphine Bugner <delphineb@bam.tech>
2021-02-02 10:27:29 +0100

    feat (RemoteIconInformation) allow png icons

--- frontend/src/components/Information/RemoteIconInformation/index.tsx
+++ frontend/src/components/Information/RemoteIconInformation/index.tsx
@@ -13,7 +13,11 @@ export interface Props {
  * Wraps BaseInformation and gives it an icon coming from a remote source (url)
  */
 export const RemoteIconInformation: React.FC<Props> = ({ iconUri, ...otherProps }) => {
-  return (
-    <Information icon={<SVG src={iconUri} className="w-6 h-full fill-current" />} {...otherProps} />
-  );
+  let icon;
+  if (RegExp(/(.*).svg/).test(iconUri)) {
+    icon = <SVG src={iconUri} className="w-6 h-full fill-current" />;
+  } else {
+    icon = <img src={iconUri} className="object-cover object-center w-6 h-6" />;
+  }
+  return <Information icon={icon} {...otherProps} />;
 };
