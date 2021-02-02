0ce6044705df2263c57ee2dd16b70d070599dc1d
Delphine Bugner <delphineb@bam.tech>
2021-02-02 16:59:15 +0100

    ui (DetailsSource) no background for logo

--- frontend/src/components/pages/details/components/DetailsSource/DetailsSource.tsx
+++ frontend/src/components/pages/details/components/DetailsSource/DetailsSource.tsx
@@ -14,24 +14,25 @@ export const DetailsSource: React.FC<DetailsSourceProps> = ({ name, pictogramUri
         <div
           className="w-30 h-20 desktop:mr-6
           grid place-items-center
-          bg-primary1 border-solid border border-primary1
           rounded-2xl overflow-hidden"
         >
           <SourceIcon pictogramUri={pictogramUri} />
         </div>
       )}
       <div className="mt-2 desktop:my-auto flex flex-col">
-        <span className="text-Mobile-C1 desktop:text-H4 mb-1 font-bold">{name}</span>
-        <a
-          href={website}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-primary1 underline
-          text-P2 desktop:text-P1
+        <span className="text-Mobile-C1 desktop:text-H4 font-bold">{name}</span>
+        {website !== null && (
+          <a
+            href={website}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary1 underline
+          text-P2 desktop:text-P1 mt-1
           hover:text-primary1-light"
-        >
-          <span>{website}</span>
-        </a>
+          >
+            <span>{website}</span>
+          </a>
+        )}
       </div>
     </div>
   );
@@ -40,11 +41,13 @@ export const DetailsSource: React.FC<DetailsSourceProps> = ({ name, pictogramUri
 const SourceIcon: React.FC<{ pictogramUri: string }> = ({ pictogramUri }) => {
   if (RegExp(/(.*).svg/).test(pictogramUri)) {
     return (
-      <SVG
-        src={pictogramUri}
-        className="fill-current h-full w-full p-1 text-white"
-        preProcessor={fillSvgWithColor(colorPalette.white)}
-      />
+      <div className="bg-primary1 h-full w-full">
+        <SVG
+          src={pictogramUri}
+          className="fill-current h-full w-full p-1 text-white"
+          preProcessor={fillSvgWithColor(colorPalette.white)}
+        />
+      </div>
     );
   }
   return <img className="object-center object-contain h-20" src={pictogramUri} />;
