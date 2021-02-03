742f2e5a2c363219b8697de2a30edd91c527d282
Louis Kraemer <louisk@bam.tech>
2021-02-03 11:33:45 +0100

    :sparkles: (home) prefetch activity search filter

--- frontend/src/components/ActivitySearchFilter/useActivitySearchFilter.ts
+++ frontend/src/components/ActivitySearchFilter/useActivitySearchFilter.ts
@@ -5,7 +5,7 @@ import { useQuery } from 'react-query';
 import { getActivities } from '../../modules/activities/connector';
 
 export const useActivitySearchFilter = () => {
-  const { data: activities } = useQuery<ActivityChoices, Error>('activities', getActivities);
+  const { data: activities } = useQuery<ActivityChoices, Error>('homeActivities', getActivities);
 
   const [expandedState, setExpandedState] = useState<'EXPANDED' | 'COLLAPSED'>('COLLAPSED');
 
