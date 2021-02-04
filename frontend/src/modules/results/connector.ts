cba35068749ba03f331990ce4c75f712c129e092
Delphine Bugner <delphineb@bam.tech>
2021-02-04 17:05:26 +0100

    rebase (main) suite

--- frontend/src/modules/results/connector.ts
+++ frontend/src/modules/results/connector.ts
@@ -4,9 +4,9 @@ import { getThemes } from 'modules/filters/theme/connector';
 import { QueryFilterState } from 'components/pages/search/utils';
 import { getApiCallsConfig } from 'modules/utils/api.config';
 
-import { adaptTrekResults } from './adapter';
-import { fetchTrekResults } from './api';
-import { TrekResults } from './interface';
+import { adaptTrekResultList, adaptTrekResults } from './adapter';
+import { fetchTrekResult, fetchTrekResults } from './api';
+import { TrekResult, TrekResults } from './interface';
 import { formatFiltersToUrlParams } from './utils';
 
 export const getTrekResults = async (
@@ -26,4 +26,19 @@ export const getTrekResults = async (
   ]);
 
   return adaptTrekResults({ rawTrekResults, difficulties, themes, activities });
+};
+
+export const getTrekResultsById = async (trekIds: number[]): Promise<TrekResult[]> => {
+  if (trekIds === null || trekIds.length === 0) {
+    return [];
+  }
+  const [difficulties, themes, activities] = await Promise.all([
+    getDifficulties(),
+    getThemes(),
+    getActivities(),
+  ]);
+  const rawTrekResults = await Promise.all(
+    trekIds.map(trekId => fetchTrekResult({ language: 'fr' }, trekId)),
+  );
+  return adaptTrekResultList({ resultsList: rawTrekResults, difficulties, themes, activities });
 };
