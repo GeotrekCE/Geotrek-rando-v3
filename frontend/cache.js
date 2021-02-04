a8d4147bb3322ac0f1f743a6fc328fdfc6dc89cd
Maxime Sraïki <maxime.sraiki@gmail.com>
2021-02-04 14:33:37 +0100

    feat(offline): add search pages to its own cached directory

--- frontend/cache.js
+++ frontend/cache.js
@@ -98,6 +98,19 @@ module.exports = [
     },
   },
   {
+    urlPattern: /\/search.*$/i,
+    handler: 'NetworkFirst',
+    method: 'GET',
+    options: {
+      cacheName: 'search-pages',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      },
+      networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+    },
+  },
+  {
     urlPattern: /.*/i,
     handler: 'NetworkFirst',
     options: {
