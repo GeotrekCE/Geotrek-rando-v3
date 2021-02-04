0eebe20458c146b2276a4156a6e6a2741a0dd03f
Loïc Chau <loicch@bam.tech>
2021-02-04 17:31:38 +0100

    fix(mapConfig): Typescript can't infer a tuple type from a json

--- frontend/src/components/Map/config.ts
+++ frontend/src/components/Map/config.ts
@@ -2,4 +2,4 @@ import { MapConfig } from './interface';
 
 import mapConfig from '../../../config/map.json';
 
-export const getMapConfig = (): MapConfig => mapConfig;
+export const getMapConfig = (): MapConfig => mapConfig as { searchMapCenter: [number, number] };
