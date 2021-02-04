f87648276fadb8d0de41aebab1b6da4147d94230
Maxime Sraïki <maxime.sraiki@gmail.com>
2021-02-04 14:32:50 +0100

    prune(offline): remove unused api pattern to avoid confusion with cors api

--- frontend/cache.js
+++ frontend/cache.js
@@ -98,33 +98,6 @@ module.exports = [
     },
   },
   {
-    urlPattern: /\/api\/.*$/i,
-    handler: 'NetworkFirst',
-    method: 'GET',
-    options: {
-      cacheName: 'get-apis',
-      expiration: {
-        maxEntries: 16,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
-      },
-      networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-    },
-  },
-  // The API call to geotrek api are all read only but are made through POST
-  {
-    urlPattern: /\/api\/.*$/i,
-    handler: 'NetworkFirst',
-    method: 'POST',
-    options: {
-      cacheName: 'post-apis',
-      expiration: {
-        maxEntries: 16,
-        maxAgeSeconds: 24 * 60 * 60, // 24 hours
-      },
-      networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-    },
-  },
-  {
     urlPattern: /.*/i,
     handler: 'NetworkFirst',
     options: {
