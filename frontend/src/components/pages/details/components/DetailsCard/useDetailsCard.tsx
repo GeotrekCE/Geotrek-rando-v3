7e3aee2ee0392124e110988459ccd14aeaefc373
Delphine Bugner <delphineb@bam.tech>
2021-02-04 15:44:40 +0100

    refacto (useDetailsCard) add constant for default height

--- frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
+++ frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 
+const DETAILS_CARD_DEFAULT_HEIGHT = 200;
+
 export const useDetailsCard = () => {
   const [truncateState, setTruncateState] = useState<'TRUNCATE' | 'FULL'>('TRUNCATE');
   const detailsCardRef = useRef<HTMLDivElement>(null);
-  const [heightState, setHeightState] = useState(200);
+  const [heightState, setHeightState] = useState(DETAILS_CARD_DEFAULT_HEIGHT);
   const toggleTruncateState = () =>
     setTruncateState(currentTruncateState =>
       currentTruncateState === 'TRUNCATE' ? 'FULL' : 'TRUNCATE',
     );
   useEffect(() => {
     if (truncateState === 'TRUNCATE') {
-      setHeightState(200);
+      setHeightState(DETAILS_CARD_DEFAULT_HEIGHT);
     } else {
       const newHeight = detailsCardRef.current?.getBoundingClientRect().height;
       if (newHeight !== undefined) {
