4fc0d161d0b03c49490574ad89dc1bd36b0c2487
Maxime Sraïki <maxime.sraiki@gmail.com>
2021-02-04 14:31:50 +0100

    feat(offline): extend to 32 cached details pages

--- frontend/cache.js
+++ frontend/cache.js
@@ -91,7 +91,7 @@ module.exports = [
     options: {
       cacheName: 'details-pages',
       expiration: {
-        maxEntries: 16,
+        maxEntries: 32,
         maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
       networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
