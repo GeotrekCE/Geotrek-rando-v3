4030404423384e339b7252940561776074f12784
Delphine Bugner <delphineb@bam.tech>
2021-02-04 17:04:45 +0100

    rebase (main)

--- frontend/src/modules/results/adapter.ts
+++ frontend/src/modules/results/adapter.ts
@@ -3,7 +3,6 @@ import { DifficultyChoices } from 'modules/filters/difficulties/interface';
 import { Choices } from 'modules/filters/interface';
 import { getThumbnail } from 'modules/utils/adapter';
 import { formatHours } from 'modules/utils/time';
-import { APIResponseForList } from 'services/api/interface';
 import { RawTrekResult, RawTrekResults, TrekResult, TrekResults } from './interface';
 import { extractNextPageId, formatDistance } from './utils';
 
@@ -29,42 +28,58 @@ const isRawTrekResultComplete = (
   rawTrekResult.reservation_system !== undefined &&
   rawTrekResult.themes !== undefined;
 
+export const adaptTrekResultList = ({
+  resultsList,
+  difficulties,
+  themes,
+  activities,
+}: {
+  resultsList: RawTrekResult[];
+  difficulties: DifficultyChoices;
+  themes: Choices;
+  activities: ActivityChoices;
+}): TrekResult[] =>
+  resultsList.filter(isRawTrekResultComplete).map(rawResult => ({
+    id: rawResult.id,
+    activityIcon: 'TODO',
+    place: rawResult.departure,
+    title: rawResult.name,
+    tags: rawResult.themes.map(themeId => themes[themeId].label),
+    thumbnailUri: getThumbnail(rawResult.attachments) ?? fallbackImgUri,
+    practice: activities[rawResult.practice],
+    informations: {
+      duration: rawResult.duration !== null ? formatHours(rawResult.duration) : null,
+      distance: `${formatDistance(rawResult.length_2d)}`,
+      elevation: `+${rawResult.ascent}${dataUnits.distance}`,
+      difficulty:
+        rawResult.difficulty !== null
+          ? {
+              label: difficulties[rawResult.difficulty].label,
+              pictogramUri: difficulties[rawResult.difficulty].pictogramUri,
+            }
+          : null,
+      reservationSystem: rawResult.reservation_system,
+    },
+  }));
+
 export const adaptTrekResults = ({
   rawTrekResults,
   difficulties,
   themes,
   activities,
 }: {
-  rawTrekResults: APIResponseForList<Partial<RawTrekResult>>;
+  rawTrekResults: RawTrekResults;
   difficulties: DifficultyChoices;
   themes: Choices;
   activities: ActivityChoices;
 }): TrekResults => {
   const resultsList = rawTrekResults.results;
-  const adaptedResultsList: TrekResult[] = resultsList
-    .filter(isRawTrekResultComplete)
-    .map(rawResult => ({
-      id: rawResult.id,
-      activityIcon: 'TODO',
-      place: rawResult.departure,
-      title: rawResult.name,
-      tags: rawResult.themes.map(themeId => themes[themeId].label),
-      thumbnailUri: getThumbnail(rawResult.attachments) ?? fallbackImgUri,
-      practice: activities[rawResult.practice],
-      informations: {
-        duration: rawResult.duration !== null ? formatHours(rawResult.duration) : null,
-        distance: `${formatDistance(rawResult.length_2d)}`,
-        elevation: `+${rawResult.ascent}${dataUnits.distance}`,
-        difficulty:
-          rawResult.difficulty !== null
-            ? {
-                label: difficulties[rawResult.difficulty].label,
-                pictogramUri: difficulties[rawResult.difficulty].pictogramUri,
-              }
-            : null,
-        reservationSystem: rawResult.reservation_system,
-      },
-    }));
+  const adaptedResultsList: TrekResult[] = adaptTrekResultList({
+    resultsList,
+    difficulties,
+    themes,
+    activities,
+  });
 
   return {
     resultsNumber: rawTrekResults.count || 0,
