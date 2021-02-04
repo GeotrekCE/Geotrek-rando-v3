130a13487f9a8a35dec5854c95923ef2917e40f5
Loïc Chau <loicch@bam.tech>
2021-02-04 10:33:30 +0100

    chore(PointsReferenceMarker): Create marker

---
+++ frontend/src/components/Map/Markers/PointsReferenceMarker.tsx
@@ -0,0 +1,35 @@
+import { DivIcon } from 'leaflet';
+import { renderToStaticMarkup } from 'react-dom/server';
+import styled from 'styled-components';
+import { colorPalette } from 'stylesheet';
+
+const markerSize = 26;
+
+// The color is hardcoded because points reference are always red like this
+const Round = styled.div`
+  border-radius: 50%;
+  background-color: #e83737;
+  display: grid;
+  place-items: center;
+  color: ${colorPalette.white};
+
+  height: ${markerSize}px;
+  width: ${markerSize}px;
+`;
+
+const PointsReference: React.FC<{ number: number }> = ({ number }) => {
+  return (
+    <Round>
+      <span>{number}</span>
+    </Round>
+  );
+};
+
+export const PointsReferenceMarker = (id: number): DivIcon =>
+  new DivIcon({
+    iconSize: [markerSize, markerSize],
+    // point of the icon which will correspond to marker's location
+    iconAnchor: [markerSize / 2, markerSize / 2], // horizontal middle of the icon and bottom of it,
+    html: renderToStaticMarkup(<PointsReference number={id} />),
+    className: 'bg-none border-none',
+  });
