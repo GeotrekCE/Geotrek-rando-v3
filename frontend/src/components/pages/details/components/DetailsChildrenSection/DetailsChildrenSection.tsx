104aca42535591afb793136090b58732fcd54293
Delphine Bugner <delphineb@bam.tech>
2021-02-05 09:26:39 +0100

    feat (DetailsChildrenSection) create component

---
+++ frontend/src/components/pages/details/components/DetailsChildrenSection/DetailsChildrenSection.tsx
@@ -0,0 +1,71 @@
+import { Separator } from 'components/Separator';
+import { TrekResult } from 'modules/results/interface';
+import { ResultCard } from 'components/pages/search/components/ResultCard';
+import styled from 'styled-components';
+import { sizes } from 'stylesheet';
+import { Step } from '../DetailsDescription';
+import { marginDetailsChild } from '../../Details';
+
+interface DetailsChildrenSectionProps {
+  trekChildren: TrekResult[];
+  title: string;
+}
+
+export const DetailsChildrenSection: React.FC<DetailsChildrenSectionProps> = ({
+  trekChildren,
+  title,
+}) => {
+  return (
+    <div className="mt-6 desktop:mt-12">
+      <div className={`text-Mobile-H1 desktop:text-H2 font-bold ${marginDetailsChild}`}>
+        {title}
+      </div>
+      <ScrollContainer
+        className="flex desktop:flex-col
+        desktop:pl-18 desktop:pr-9 desktop:mr-9 desktop:pt-10
+        mt-4 mb-4 desktop:mb-0 px-4 pb-2
+        items-stretch
+        overflow-scroll flex-nowrap max-h-screen
+        "
+      >
+        {trekChildren.map((trekChild, i) => (
+          <div key={i}>
+            <div className="hidden desktop:block">
+              <Step number={i + 1} />
+            </div>
+            <div
+              className={`mr-3 desktop:ml-5.5 desktop:mr-0 desktop:pl-12 relative ${
+                i < trekChildren.length - 1
+                  ? 'desktop:border-solid desktop:border-primary1 desktop:border-l-3'
+                  : ''
+              }`}
+            >
+              <div className="relative desktop:-top-20">
+                <ResultCard
+                  id={trekChild.id}
+                  place={trekChild.place}
+                  title={trekChild.title}
+                  tags={trekChild.tags}
+                  thumbnailUri={trekChild.thumbnailUri}
+                  badgeIconUri={trekChild.practice.pictogram}
+                  informations={trekChild.informations}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </ScrollContainer>
+      <div className={marginDetailsChild}>
+        <Separator />
+      </div>
+    </div>
+  );
+};
+
+const offsetTopForTitle = 130;
+
+const ScrollContainer = styled.div`
+  max-height: calc(
+    100vh - ${sizes.desktopHeader + sizes.detailsHeaderDesktop + offsetTopForTitle}px
+  );
+`;
