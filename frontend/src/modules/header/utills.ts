efc7bffe505412df91f18ab2c7cf7fd0fe425dcc
Delphine Bugner <delphineb@bam.tech>
2021-02-10 17:48:25 +0100

    feat (header/utils) add function generateFlatPageUrl

--- frontend/src/modules/header/utills.ts
+++ frontend/src/modules/header/utills.ts
@@ -1,3 +1,4 @@
+import { routes } from 'services/routes';
 import headerConfig from '../../../config/header.json';
 import structureHeaderConfig from '../../../customization/config/header.json';
 import { HeaderConfig } from './interface';
@@ -6,3 +7,8 @@ export const getHeaderConfig = (): HeaderConfig => ({
   ...headerConfig,
   ...structureHeaderConfig,
 });
+
+export const generateFlatPageUrl = (id: number, title: string): string => {
+  const titleWithNoSpace = title.replace(/ /g, '-');
+  return `${routes.FLAT_PAGE}/${id}-${encodeURI(titleWithNoSpace)}`;
+};
