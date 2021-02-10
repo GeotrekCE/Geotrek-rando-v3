3a7c688012d87c773318aaf2d421b77243a72718
Delphine Bugner <delphineb@bam.tech>
2021-02-10 16:32:07 +0100

    clean (RemoteIconInformation) revert - no need for bakground

--- frontend/src/components/Information/RemoteIconInformation/index.tsx
+++ frontend/src/components/Information/RemoteIconInformation/index.tsx
@@ -7,29 +7,15 @@ export interface Props {
   iconUri: string;
   children: React.ReactNode;
   className?: string;
-  backgroundColor?: string;
 }
 
 /**
  * Wraps BaseInformation and gives it an icon coming from a remote source (url)
- * backgroundColor must be a tailwind color : primary1, primary2, etc. No hexa code.
  */
-export const RemoteIconInformation: React.FC<Props> = ({
-  iconUri,
-  backgroundColor,
-  ...otherProps
-}) => {
+export const RemoteIconInformation: React.FC<Props> = ({ iconUri, ...otherProps }) => {
   let icon;
   if (RegExp(/(.*).svg/).test(iconUri)) {
-    if (backgroundColor !== undefined) {
-      icon = (
-        <div className={`w-7 h-7 bg-${backgroundColor} rounded-full grid place-items-center`}>
-          <SVG src={iconUri} className="w-6 h-6" />
-        </div>
-      );
-    } else {
-      icon = <SVG src={iconUri} className="w-6 h-full fill-current" />;
-    }
+    icon = <SVG src={iconUri} className="w-6 h-full fill-current" />;
   } else {
     icon = <img src={iconUri} className="object-cover object-center w-6 h-6" />;
   }
