39e92037b149f3db65b1f4e676c18d00c8ccd375
Louis Kraemer <louisk@bam.tech>
2021-02-11 14:49:29 +0100

    :sparkles: (details) prefetch itinerane

--- frontend/src/pages/[detailsId].tsx
+++ frontend/src/pages/[detailsId].tsx
@@ -2,18 +2,22 @@ import { useRouter } from 'next/router';
 import { DetailsUI } from 'components/pages/details';
 import { QueryClient } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
-import { getDetails } from 'modules/details/connector';
+import { getDetails, getTrekChildren } from 'modules/details/connector';
 import { isUrlString } from 'modules/utils/string';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const getServerSideProps = async (context: {
-  query: { detailsId: string | string[] | undefined };
+  query: { detailsId: string | string[] | undefined; parentId: string | string[] | undefined };
 }) => {
   const id = isUrlString(context.query.detailsId) ? context.query.detailsId.split('-')[1] : '';
+  const parentIdString = isUrlString(context.query.parentId) ? context.query.parentId : '';
 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery(`details-${id}`, () => getDetails(id));
+  await queryClient.prefetchQuery(`trekFamily-${parentIdString}`, () =>
+    getTrekChildren(parentIdString),
+  );
 
   return {
     props: {
