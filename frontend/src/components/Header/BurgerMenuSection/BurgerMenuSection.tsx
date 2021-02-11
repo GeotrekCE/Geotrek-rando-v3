edd787ad5126364e5ae43716030c59369fd34ba9
Delphine Bugner <delphineb@bam.tech>
2021-02-11 15:58:31 +0100

    fix ui (BurgerMenuSection) subsections font was too small

--- frontend/src/components/Header/BurgerMenuSection/BurgerMenuSection.tsx
+++ frontend/src/components/Header/BurgerMenuSection/BurgerMenuSection.tsx
@@ -40,11 +40,11 @@ export const BurgerMenuSection: React.FC<Props> = ({ title, items }) => {
         <AccordionItemPanel className={openState === 'OPENED' ? classNameBorder : ''}>
           {items.map((item, i) =>
             isItemString(item) ? (
-              <p className="text-Mobile-C3 py-2" key={i}>
+              <p className="text-Mobile-C2 py-2" key={i}>
                 {item.toUpperCase()}
               </p>
             ) : (
-              <p className="text-Mobile-C3 py-2" key={i}>
+              <p className="text-Mobile-C2 py-2" key={i}>
                 <a href={item.url}>{item.title}</a>
               </p>
             ),
