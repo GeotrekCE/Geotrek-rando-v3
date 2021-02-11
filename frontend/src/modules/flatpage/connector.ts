869777b5aa002ecda805ccabc479bd56342da35c
Delphine Bugner <delphineb@bam.tech>
2021-02-11 11:30:22 +0100

    feat (modules/flatPage) add getFlatPageDetails connector

--- frontend/src/modules/flatpage/connector.ts
+++ frontend/src/modules/flatpage/connector.ts
@@ -1,8 +1,26 @@
 import { MenuItem } from 'modules/header/interface';
-import { adaptFlatPages } from './adapter';
-import { fetchFlatPages } from './api';
+import { getSources } from 'modules/source/connector';
+import { adaptFlatPageDetails, adaptFlatPages } from './adapter';
+import { fetchFlatPageDetails, fetchFlatPages } from './api';
+import { FlatPageDetails } from './interface';
 
 export const getFlatPages = async (): Promise<MenuItem[]> => {
   const rawFlatPages = await fetchFlatPages({ language: 'fr' });
   return adaptFlatPages(rawFlatPages.results);
+};
+
+export const getFlatPageDetails = async (id: string): Promise<FlatPageDetails> => {
+  try {
+    const [rawFlatPageDetails, sourceDictionnary] = await Promise.all([
+      fetchFlatPageDetails({ language: 'fr' }, id),
+      getSources(),
+    ]);
+    return adaptFlatPageDetails({
+      rawFlatPageDetails,
+      sourceDictionnary,
+    });
+  } catch (e) {
+    console.error('Error in flatpage connector', e);
+    throw e;
+  }
 };
--- frontend/src/modules/flatpage/interface.ts
+++ frontend/src/modules/flatpage/interface.ts
@@ -1,6 +1,22 @@
+import { Source } from 'modules/source/interface';
+
 export interface RawFlatPage {
   id: number;
   title: string;
   external_url: string | null;
   order: number | null;
+}
+
+export interface RawFlatPageDetails {
+  id: number;
+  title: string;
+  content: string;
+  source: number[];
+}
+
+export interface FlatPageDetails {
+  id: number;
+  title: string;
+  content: string;
+  sources: Source[];
 }
