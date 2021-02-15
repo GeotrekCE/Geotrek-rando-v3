159e7fdc0bb29b7346d4b8af6287be18c6786eb5
Louis Kraemer <louisk@bam.tech>
2021-02-15 14:16:21 +0100

    :recycle: (mapResults) use new way to fetch

--- frontend/src/modules/mapResults/connector.ts
+++ frontend/src/modules/mapResults/connector.ts
@@ -1,6 +1,10 @@
 import { QueryFilterState } from 'components/pages/search/utils';
 import { getActivities } from 'modules/activities/connector';
-import { formatTrekFiltersToUrlParams } from 'modules/results/utils';
+import { CATEGORY_ID, PRACTICE_ID } from 'modules/filters/constant';
+import {
+  formatTouristicContentFiltersToUrlParams,
+  formatTrekFiltersToUrlParams,
+} from 'modules/results/utils';
 import { getApiCallsConfig } from 'modules/utils/api.config';
 
 import { generatePageNumbersArray } from 'modules/utils/connector';
@@ -8,28 +12,53 @@ import { adaptMapResults } from './adapter';
 import { fetchMapResults } from './api';
 import { MapResults } from './interface';
 
+const emptyResultPromise = Promise.resolve({
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
+});
+
 export const getMapResults = async (filtersState: QueryFilterState[]): Promise<MapResults> => {
-  const resultsNumber = getApiCallsConfig().searchResultsPageSize;
+  try {
+    const practiceFilter = filtersState.find(({ id }) => id === PRACTICE_ID);
+    const isPracticeSelected = practiceFilter ? practiceFilter.selectedOptions.length > 0 : false;
+    const serviceFilter = filtersState.find(({ id }) => id === CATEGORY_ID);
+    const isServiceSelected = serviceFilter ? serviceFilter.selectedOptions.length > 0 : false;
 
-  const formattedFiltersToUrlParams = formatTrekFiltersToUrlParams(filtersState);
-  const rawMapResults = await fetchMapResults({
-    language: 'fr',
-    page_size: resultsNumber,
-    ...formattedFiltersToUrlParams,
-  });
+    const shouldFetchTreks = !isServiceSelected || isPracticeSelected;
+    const shouldFetchTouristicContents = !isPracticeSelected || isServiceSelected;
 
-  const mapResults = await Promise.all(
-    generatePageNumbersArray(resultsNumber, rawMapResults.count).map(pageNumber =>
-      fetchMapResults({
+    const trekFilters = formatTrekFiltersToUrlParams(filtersState);
+    const touristicContentFilter = formatTouristicContentFiltersToUrlParams(filtersState);
+
+    const resultsNumber = getApiCallsConfig().searchResultsPageSize;
+
+    const mapResults: MapResults = [];
+
+    if (shouldFetchTreks) {
+      const rawMapResults = await fetchMapResults({
         language: 'fr',
         page_size: resultsNumber,
-        page: pageNumber,
-        ...formattedFiltersToUrlParams,
-      }),
-    ),
-  );
+        ...trekFilters,
+      });
+      const mapTrekResults = await Promise.all(
+        generatePageNumbersArray(resultsNumber, rawMapResults.count).map(pageNumber =>
+          fetchMapResults({
+            language: 'fr',
+            page_size: resultsNumber,
+            page: pageNumber,
+            ...trekFilters,
+          }),
+        ),
+      );
+      const activities = await getActivities();
+      mapResults.push(...adaptMapResults({ mapResults: mapTrekResults, activities }));
+    }
 
-  const activities = await getActivities();
-
-  return adaptMapResults({ mapResults, activities });
+    return mapResults;
+  } catch (error) {
+    console.error('Error in connector / mapResults', error);
+    throw error;
+  }
 };
