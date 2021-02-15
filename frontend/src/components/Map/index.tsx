3dec91c6bd2daadf2b7b0840fb1332f52345eb91
Delphine Bugner <delphineb@bam.tech>
2021-02-15 16:44:15 +0100

    feat (Map) export dynamic TouristicContentMap

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import { PropsType as SearchMapProps } from './SearchMap';
 import { PropsType as DetailsMapProps } from './DetailsMap/DetailsMap';
+import { PropsType as TouristicContentMapProps } from './TouristicContentMap/TouristicContentMap';
 
 export const SearchMapDynamicComponent: React.FC<SearchMapProps> = props => {
   const SearchMap = dynamic(() => import('./SearchMap'), {
@@ -14,4 +15,11 @@ export const DetailsMapDynamicComponent: React.FC<DetailsMapProps> = props => {
     ssr: false,
   });
   return <DetailsMap {...props} />;
+};
+
+export const TouristicContentMapDynamicComponent: React.FC<TouristicContentMapProps> = props => {
+  const TouristicContentMap = dynamic(() => import('./TouristicContentMap/TouristicContentMap'), {
+    ssr: false,
+  });
+  return <TouristicContentMap {...props} />;
 };
