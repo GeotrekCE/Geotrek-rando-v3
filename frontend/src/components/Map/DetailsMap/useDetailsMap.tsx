5c63eda45317d9a81a5e11854270c91d1c8d69fc
Delphine Bugner <delphineb@bam.tech>
2021-02-16 15:50:25 +0100

    feat (DetailsMap) add trekChild visibility state

--- frontend/src/components/Map/DetailsMap/useDetailsMap.tsx
+++ frontend/src/components/Map/DetailsMap/useDetailsMap.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 export type Visibility = 'DISPLAYED' | 'HIDDEN' | null;
 
 export const useDetailsMap = () => {
+  const [trekChildrenMobileVisibility, setTrekChildrenVisibility] = useState<Visibility>('HIDDEN');
   const [poiMobileVisibility, setPoiVisibility] = useState<Visibility>('HIDDEN');
   const [referencePointsMobileVisibility, setReferencePointsVisibility] = useState<Visibility>(
     'HIDDEN',
@@ -10,6 +11,11 @@ export const useDetailsMap = () => {
   const [touristicContentMobileVisibility, setTouristicContentVisibility] = useState<Visibility>(
     'HIDDEN',
   );
+  const toggleTrekChildrenVisibility = () =>
+    setTrekChildrenVisibility(currentVisibility =>
+      currentVisibility === 'DISPLAYED' ? 'HIDDEN' : 'DISPLAYED',
+    );
+
   const togglePoiVisibility = () =>
     setPoiVisibility(currentVisibility =>
       currentVisibility === 'DISPLAYED' ? 'HIDDEN' : 'DISPLAYED',
@@ -26,6 +32,8 @@ export const useDetailsMap = () => {
     );
 
   return {
+    trekChildrenMobileVisibility,
+    toggleTrekChildrenVisibility,
     poiMobileVisibility,
     togglePoiVisibility,
     referencePointsMobileVisibility,
