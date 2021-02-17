0a5ecfe8ad4cf2f97d4e7a1b875f76d34bb89861
Delphine Bugner <delphineb@bam.tech>
2021-02-17 16:16:35 +0100

    feat (Root) useLanguageContext to set intl provider

--- frontend/src/components/pages/_app/Root.tsx
+++ frontend/src/components/pages/_app/Root.tsx
@@ -7,6 +7,7 @@ import { flattenMessages } from 'services/i18n/intl';
 import enMessages from 'translations/en.json';
 import frMessages from 'translations/fr.json';
 import customFrMessages from 'customization/translations/fr.json';
+import { useLanguageContext } from 'services/languageContext';
 import CSSResets from './CSSResets';
 import { ErrorBoundary } from './ErrorBoundary';
 
@@ -20,38 +21,44 @@ interface RootProps {
   errorEventId?: string;
 }
 
-export const Root: FunctionComponent<RootProps> = props => (
-  <ErrorBoundary
-    FallbackComponent={AppCrashFallback}
-    hasError={props.hasError}
-    eventId={props.errorEventId}
-  >
-    <IntlProvider locale="fr" messages={locales.fr}>
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
+export const Root: FunctionComponent<RootProps> = props => {
+  const { language } = useLanguageContext();
+  return (
+    <ErrorBoundary
+      FallbackComponent={AppCrashFallback}
+      hasError={props.hasError}
+      eventId={props.errorEventId}
+    >
+      {/*@ts-ignore-next-line we ignore because locales have to be given for the config languages */}
+      <IntlProvider locale={language} messages={locales[language] ?? locales.fr}>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
 
-        <meta name="application-name" content="Geotrek-rando" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Geotrek-rando" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+          <meta name="application-name" content="Geotrek-rando" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Geotrek-rando" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
-        <link rel="mask-icon" href="/images/logo.png" color="#5bbad5" />
-        <link rel="shortcut icon" href="/images/logo.png" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-        <meta
-          name="google-site-verification"
-          content="eKAyxwaXAobFWQcJen0mnZ8T3CpLoN45JysXeNkRf38"
-        />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
+          <link rel="mask-icon" href="/images/logo.png" color="#5bbad5" />
+          <link rel="shortcut icon" href="/images/logo.png" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <meta
+            name="google-site-verification"
+            content="eKAyxwaXAobFWQcJen0mnZ8T3CpLoN45JysXeNkRf38"
+          />
 
-        {/* <meta name="twitter:card" content="summary" />
+          {/* <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="localhost" />
         <meta name="twitter:title" content="Geotrek-rando" />
         <meta name="twitter:description" content="Geotrek rando" />
@@ -65,13 +72,14 @@ export const Root: FunctionComponent<RootProps> = props => (
         <meta property="og:url" content="localhost" />
         <meta property="og:image" content="/images/logo.png" /> */}
 
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
-      <CSSResets />
-      {props.children}
-    </IntlProvider>
-  </ErrorBoundary>
-);
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+        </Head>
+        <CSSResets />
+        {props.children}
+      </IntlProvider>
+    </ErrorBoundary>
+  );
+};
