dad320289de8a4b7f368c6b36904abe50344ec06
Delphine Bugner <delphineb@bam.tech>
2021-02-18 17:12:52 +0100

    feat (routeUtils) add functions for routing

---
+++ frontend/src/services/routeUtils.ts
@@ -0,0 +1,21 @@
+import { NextRouter } from 'next/router';
+import { routes } from './routes';
+
+export const switchToLanguage = async (router: NextRouter, newLanguage: string) => {
+  await router.push(
+    {
+      pathname: router.pathname,
+      query: { ...router.query },
+    },
+    router.pathname,
+    { locale: newLanguage },
+  );
+};
+
+export const isRessourceMissing = (error: Error | null): boolean =>
+  error !== null && error?.message === 'Failed to fetch';
+
+export const isInternalFlatPageUrl = (url: string): boolean => {
+  if (url === undefined) return false;
+  return url.indexOf(`${routes.FLAT_PAGE}/`) === 0;
+};
