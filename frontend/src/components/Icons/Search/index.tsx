3a46fb40b3c66a06d09715b96019ecd42193b7cf
Delphine Bugner <delphineb@bam.tech>
2021-02-18 10:07:20 +0100

    icon (Search) add icon

---
+++ frontend/src/components/Icons/Search/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GenericIconProps } from '../types';
+
+export const Search: React.FC<GenericIconProps> = ({
+  color = 'currentColor',
+  opacity,
+  className,
+  size,
+}) => {
+  return (
+    <svg
+      height={size}
+      width={size}
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      opacity={opacity}
+    >
+      <path d="M16 8.5a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z" stroke={color} strokeWidth={2} />
+      <path
+        stroke={color}
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M14.164 14.25l5.586 5.586"
+      />
+    </svg>
+  );
+};
