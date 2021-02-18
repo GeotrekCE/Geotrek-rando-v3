9ce449ee990821a7fbae5f825dd23d5ac76edf1e
Delphine Bugner <delphineb@bam.tech>
2021-02-18 17:16:44 +0100

    feat (BurgerMenu) manage language switch

--- frontend/src/components/Header/BurgerMenuSection/BurgerMenuSection.tsx
+++ frontend/src/components/Header/BurgerMenuSection/BurgerMenuSection.tsx
@@ -6,6 +6,8 @@ import {
   AccordionItemPanel,
 } from 'react-accessible-accordion';
 import { MenuItem } from 'modules/header/interface';
+import { Link } from 'components/Link';
+import { useRouter } from 'next/router';
 import { Plus } from '../../Icons/Plus';
 import { Minus } from '../../Icons/Minus';
 import { useBurgerMenuSection } from './useBurgerMenuSection';
@@ -13,12 +15,14 @@ import { useBurgerMenuSection } from './useBurgerMenuSection';
 export interface Props {
   title: string;
   items?: Array<string | MenuItem>;
+  languages?: string[];
   onClick?: () => void;
 }
 
 const isItemString = (item: MenuItem | string): item is string => typeof item === 'string';
 
 export const BurgerMenuSection: React.FC<Props> = ({ title, items, onClick }) => {
+  const router = useRouter();
   const classNameTitle = 'flex items-center pt-4 pb-4 font-bold outline-none cursor-pointer';
   const classNameBorder = 'border-b pb-2 border-solid border-greySoft';
   const openIcon = <Plus size={24} />;
@@ -27,7 +31,7 @@ export const BurgerMenuSection: React.FC<Props> = ({ title, items, onClick }) =>
   const updatePanelState = (openPanelIds: string[]) => {
     openPanelIds.length > 0 ? setOpenState('OPENED') : setOpenState('CLOSED');
   };
-  return items ? (
+  return items || languages ? (
     <Accordion allowZeroExpanded onChange={updatePanelState}>
       <AccordionItem>
         <AccordionItemHeading>
@@ -39,17 +43,26 @@ export const BurgerMenuSection: React.FC<Props> = ({ title, items, onClick }) =>
           </AccordionItemButton>
         </AccordionItemHeading>
         <AccordionItemPanel className={openState === 'OPENED' ? classNameBorder : ''}>
-          {items.map((item, i) =>
-            isItemString(item) ? (
-              <p className="text-Mobile-C2 py-2" key={i}>
-                {item.toUpperCase()}
-              </p>
-            ) : (
-              <p className="text-Mobile-C2 py-2" key={i}>
-                <a href={item.url}>{item.title}</a>
-              </p>
-            ),
-          )}
+          {items?.map((item, i) => (
+            <p key={i}>
+              <a className="text-Mobile-C2 py-2" href={item.url}>
+                {item.title}
+              </a>
+            </p>
+          ))}
+          {languages?.map(language => (
+            <Link
+              className="w-full"
+              key={language}
+              href={{
+                pathname: router.pathname,
+                query: { ...router.query },
+              }}
+              locale={language}
+            >
+              <p className="text-Mobile-C2 py-2 text-greyDarkColored">{language.toUpperCase()}</p>
+            </Link>
+          ))}
         </AccordionItemPanel>
       </AccordionItem>
     </Accordion>
