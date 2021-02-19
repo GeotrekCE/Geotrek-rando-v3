ff8c3c68cd2dd4ef85f3047ed78fd109038aabc2
Delphine Bugner <delphineb@bam.tech>
2021-02-19 19:13:07 +0100

    fix ui (HomeSection) add flex wrap & no overflow

--- frontend/src/components/pages/home/components/HomeSection/HomeSection.tsx
+++ frontend/src/components/pages/home/components/HomeSection/HomeSection.tsx
@@ -7,35 +7,34 @@ export interface HomeSectionProps {
   title: string;
   iconUrl: string;
   activitySuggestions?: ActivitySuggestion[];
+  className?: string;
 }
 
 export const HomeSection: React.FC<HomeSectionProps> = ({
   title,
   iconUrl,
   activitySuggestions,
+  className,
 }) => {
   return (
-    <div
-      className="
-        border-t border-solid border-greySoft
-        pt-3 desktop:pt-18
-        flex flex-col space-y-4 desktop:space-y-6"
-    >
-      <div className="flex items-center space-x-4">
+    <div className={`flex flex-col ${className ?? ''}`}>
+      <div className="flex border-t border-greySoft border-solid pt-3 desktop:pt-10 mb-3 desktop:mb-6">
         <SVG
           src={iconUrl}
           preProcessor={fillSvgWithColor(colorPalette.greyDarkColored)}
-          className="h-10"
+          className="h-10 mr-2 desktop:mr-3"
         />
         <span className="text-H4 desktop:text-H2 font-bold">{title}</span>
       </div>
-      <div className="flex space-x-6 overflow-x-scroll">
+      <div className="flex flex-wrap overflow-hidden mb-5 desktop:mb-15 justify-center">
         {activitySuggestions !== undefined &&
           activitySuggestions.map((activitySuggestion, i) => (
             <ActivitySuggestionCard
               key={i}
+              id={activitySuggestion.id}
               title={activitySuggestion.title}
               imgUrl={activitySuggestion.imgUrl}
+              className="m-1 desktop:m-2"
             />
           ))}
       </div>
