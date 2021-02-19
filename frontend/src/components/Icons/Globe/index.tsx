e5ef6f4ff902b5bdedd1f7bafb895f8f9cad1983
Delphine Bugner <delphineb@bam.tech>
2021-02-19 16:23:43 +0100

    asset (icons) add Globe & Instagram

---
+++ frontend/src/components/Icons/Globe/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GenericIconProps } from '../types';
+
+export const Globe: React.FC<GenericIconProps> = ({
+  color = 'currentColor',
+  opacity,
+  className,
+  size,
+}) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={2}
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      opacity={opacity}
+    >
+      <circle cx={12} cy={12} r={10} />
+      <path d="M2 12h20M12 2a15.3 15.3 0 014 10 15.3 15.3 0 01-4 10 15.3 15.3 0 01-4-10 15.3 15.3 0 014-10z" />
+    </svg>
+  );
+};
