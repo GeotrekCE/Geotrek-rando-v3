9df496547acb650499732aabd438120d802cdc81
Louis Kraemer <louisk@bam.tech>
2021-02-22 14:55:35 +0100

    :recycle: (concatMapResults) rename to concatTrekMapResults

--- frontend/src/modules/mapResults/utils.ts
+++ frontend/src/modules/mapResults/utils.ts
@@ -8,7 +8,7 @@ export const formatLocation = (rawLocation: number[] | null): { x: number; y: nu
         y: rawLocation[1],
       };
 
-export const concatMapResults = (rawMapResults: RawMapResults[]): RawMapResult[] =>
+export const concatTrekMapResults = (rawMapResults: RawMapResults[]): RawMapResult[] =>
   rawMapResults.reduce<RawMapResult[]>(
     (rawResults, currentResult) => [...rawResults, ...currentResult.results],
     [],
