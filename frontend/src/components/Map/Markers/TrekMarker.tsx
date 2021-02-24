e4fbe9447a08bcd51c62608dc78e3cff669d9f9b
Louis Kraemer <louisk@bam.tech>
2021-02-24 15:09:35 +0100

    :sparkles: (trekMarker) add optional zoom props

--- frontend/src/components/Map/Markers/TrekMarker.tsx
+++ frontend/src/components/Map/Markers/TrekMarker.tsx
@@ -1,8 +1,9 @@
+import { MapMarker } from 'components/Icons/MapMarker';
 import { DivIcon } from 'leaflet';
+import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import styled from 'styled-components';
-
-import { BaseMarker } from './BaseMarker';
+import { colorPalette } from 'stylesheet';
 
 const markerHeight = 44;
 const markerWidth = 36;
@@ -10,27 +11,34 @@ const markerWidth = 36;
 const markerHorizontalPadding = 6;
 const markerTopPadding = 7;
 
-const ActivityPictogram = styled.img`
-  width: ${markerWidth - 2 * markerHorizontalPadding}px;
-  height: auto;
-  left: ${markerHorizontalPadding}px;
-  top: ${markerTopPadding}px;
+const ActivityPictogram = styled.img<{ zoomRatio: number }>`
+  width: ${props => (markerWidth - 2 * markerHorizontalPadding) * props.zoomRatio}px;
+  top: ${props => markerTopPadding * props.zoomRatio}px;
 `;
 
-const ActivityMarker: React.FC<{ pictogramUrl: string }> = ({ pictogramUrl }) => {
+const ActivityMarker: React.FC<{ pictogramUrl: string; zoomRatio: number }> = ({
+  pictogramUrl,
+  zoomRatio,
+}) => {
   return (
-    <div className="relative">
-      <BaseMarker className="absolute" />
-      <ActivityPictogram className="absolute" src={pictogramUrl} />
+    <div className="relative flex justify-center">
+      <MapMarker color={colorPalette.primary1} size={markerWidth * zoomRatio} />
+      <ActivityPictogram
+        className="absolute z-leafletSvg"
+        src={pictogramUrl}
+        zoomRatio={zoomRatio}
+      />
     </div>
   );
 };
 
-export const TrekMarker = (pictogramUrl: string) =>
+export const TrekMarker = (pictogramUrl: string, zoomRatio = 1) =>
   new DivIcon({
-    iconSize: [markerHeight, markerWidth],
+    iconSize: [markerHeight * zoomRatio, markerWidth * zoomRatio],
     // point of the icon which will correspond to marker's location
-    iconAnchor: [markerWidth / 2, markerHeight], // horizontal middle of the icon and bottom of it
-    html: renderToStaticMarkup(<ActivityMarker pictogramUrl={pictogramUrl} />),
+    iconAnchor: [(markerWidth * zoomRatio) / 2, markerHeight * zoomRatio], // horizontal middle of the icon and bottom of it
+    html: renderToStaticMarkup(
+      <ActivityMarker pictogramUrl={pictogramUrl} zoomRatio={zoomRatio} />,
+    ),
     className: 'bg-none border-none',
   });
