9bc3b95afc1aeb156f6455d668e135370d9eed89
Thomas Pucci <thomas.lb.pucci@gmail.com>
2021-03-03 17:24:34 +0100

    feat: add analytics

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,5 +25,29 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8FSV2N4FXN"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-8FSV2N4FXN', {'debug_mode':true});`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
