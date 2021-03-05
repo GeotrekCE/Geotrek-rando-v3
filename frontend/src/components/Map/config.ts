c509cc0b8997321c38678e0d20d42d1e3aac9e91
Louis Kraemer <louisk@bam.tech>
2021-03-05 11:35:25 +0100

    feat(config): all json customizable

--- frontend/src/components/Map/config.ts
+++ frontend/src/components/Map/config.ts
@@ -1,5 +1,6 @@
 import { MapConfig } from './interface';
 
 import mapConfig from '../../../config/map.json';
+import mapConfigCustom from '../../../customization/config/map.json';
 
-export const getMapConfig = (): MapConfig => mapConfig;
+export const getMapConfig = (): MapConfig => ({ ...mapConfig, ...mapConfigCustom });
--- frontend/src/modules/home/utils.ts
+++ frontend/src/modules/home/utils.ts
@@ -1,5 +1,5 @@
 import { HomePageConfig } from './interface';
-import homePageConfig from '../../../config/homePage.json';
+import homePageConfig from '../../../config/home.json';
 import customHomePageConfig from '../../../customization/config/home.json';
 
 export const getHomePageConfig = (): HomePageConfig => ({
