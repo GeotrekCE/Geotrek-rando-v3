27a8c8543406e23d522d5c1db5f8fc7e74530f46
Delphine Bugner <delphineb@bam.tech>
2021-04-06 15:54:25 +0200

    :sparkles: (fetchCourseTypes) filter course types with portal

--- frontend/src/modules/filters/courseType/api.ts
+++ frontend/src/modules/filters/courseType/api.ts
@@ -1,9 +1,13 @@
+import { portalsFilter } from 'modules/utils/api.config';
 import { GeotrekAPI } from 'services/api/client';
 import { APIQuery, APIResponseForList } from 'services/api/interface';
 import { RawCourseType } from './interface';
 
 export const fetchCourseTypes = (query: APIQuery): Promise<APIResponseForList<RawCourseType>> =>
-  GeotrekAPI.url('/trek_route').query(query).get().json();
+  GeotrekAPI.url('/trek_route')
+    .query({ ...query, ...portalsFilter })
+    .get()
+    .json();
 
 export const fetchCourseType = (query: APIQuery, id: number): Promise<RawCourseType> =>
   GeotrekAPI.url(`/trek_route/${id}/`).query(query).get().json();
