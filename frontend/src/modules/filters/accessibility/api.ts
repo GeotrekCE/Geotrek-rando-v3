678b96e3ad6edc7aae987b384b16e0dfcc887c87
Delphine Bugner <delphineb@bam.tech>
2021-04-06 15:45:06 +0200

    :sparkles: (fetchAccessibilities) filter accessibilities with portal

--- frontend/src/modules/filters/accessibility/api.ts
+++ frontend/src/modules/filters/accessibility/api.ts
@@ -1,3 +1,4 @@
+import { portalsFilter } from 'modules/utils/api.config';
 import { GeotrekAPI } from 'services/api/client';
 import { APIQuery, APIResponseForList } from 'services/api/interface';
 import { RawAccessibilty } from './interface';
@@ -5,4 +6,7 @@ import { RawAccessibilty } from './interface';
 export const fetchAccessibilities = (
   query: APIQuery,
 ): Promise<APIResponseForList<RawAccessibilty>> =>
-  GeotrekAPI.url('/trek_accessibility').query(query).get().json();
+  GeotrekAPI.url('/trek_accessibility')
+    .query({ ...query, ...portalsFilter })
+    .get()
+    .json();
