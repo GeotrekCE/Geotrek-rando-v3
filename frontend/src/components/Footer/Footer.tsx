1af8d4f3efcf31af3b73969ef322d049ef3f9d69
Delphine Bugner <delphineb@bam.tech>
2021-04-08 15:00:57 +0200

    :sparkles: (Footer) & Footer components) add id

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -7,9 +7,10 @@ export const Footer: React.FC = () => {
   const { config } = useFooter();
   return (
     <div className="bg-black flex flex-col" id="footer">
-      <div className="mx-4 desktop:mx-10percent">
+      <div className="mx-4 desktop:mx-10percent" id="footer_content">
         {config.socialNetworks && <SocialNetworks socialNetworkList={config.socialNetworks} />}
         <div
+          id="footer_sections"
           className="
           flex flex-col desktop:flex-row justify-between
           pt-2.5 pb-10 desktop:pt-10 desktop:pb-18"
