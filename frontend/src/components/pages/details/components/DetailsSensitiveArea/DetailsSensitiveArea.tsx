ea8137e01ac05da7e2664862544f7a22e8aecbe7
Delphine Bugner <delphineb@bam.tech>
2021-04-13 18:08:54 +0200

    :bug: (DetailsSensitiveArea) fix margins between sections & title size

--- frontend/src/components/pages/details/components/DetailsSensitiveArea/DetailsSensitiveArea.tsx
+++ frontend/src/components/pages/details/components/DetailsSensitiveArea/DetailsSensitiveArea.tsx
@@ -17,15 +17,15 @@ export const DetailsSensitiveArea: React.FC<DetailsSensitiveAreaProps> = ({
 }) => {
   return (
     <div id="details_sensitiveArea" className={className}>
-      {name !== null && <span className="font-bold text-Mobile-C1 desktop:text-H4">{name}</span>}
+      {name !== null && <span className="font-bold text-H4 space-y-2">{name}</span>}
       {description !== null && <HtmlText>{parse(description)}</HtmlText>}
       {contact !== null && (
-        <>
+        <div className="mt-1 desktop:mt-2">
           <span className="font-bold">
             <FormattedMessage id={'details.sensitiveAreas.contact'} />
           </span>
           <HtmlText>{parse(contact)}</HtmlText>
-        </>
+        </div>
       )}
       {infoUrl !== null && (
         <div className="mt-1 desktop:mt-2">
