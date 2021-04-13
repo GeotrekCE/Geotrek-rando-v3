65cc30e67a3fa238877c9fad47184a022d260b4e
Delphine Bugner <delphineb@bam.tech>
2021-04-13 17:08:28 +0200

    :recycle: (adapt SensitiveArea) simplify string check

--- frontend/src/modules/sensitiveArea/adapter.ts
+++ frontend/src/modules/sensitiveArea/adapter.ts
@@ -1,10 +1,10 @@
 import { RawSensitiveArea, SensitiveArea } from './interface';
 
 const adaptSensitiveArea = (rawSensitiveArea: RawSensitiveArea): SensitiveArea => ({
-  name: rawSensitiveArea?.name.length > 0 ? rawSensitiveArea.name : null,
-  description: rawSensitiveArea.description?.length > 0 ? rawSensitiveArea.description : null,
-  contact: rawSensitiveArea.contact?.length > 0 ? rawSensitiveArea.contact : null,
-  infoUrl: rawSensitiveArea.info_url?.length > 0 ? rawSensitiveArea.info_url : null,
+  name: rawSensitiveArea.name ?? null,
+  description: rawSensitiveArea.description ?? null,
+  contact: rawSensitiveArea.contact ?? null,
+  infoUrl: rawSensitiveArea.info_url ?? null,
 });
 
 export const adaptSensitiveAreas = (rawSensitiveAreas: RawSensitiveArea[]): SensitiveArea[] =>
