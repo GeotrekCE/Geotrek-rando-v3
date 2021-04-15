4846ff91a37208bb38a316b98b8c9e8dc3ff49ab
Charlotte Isambert <charlotte.isambert@gmail.com>
2021-04-15 10:30:46 +0200

    (Search): add textFilter placeholder

--- frontend/src/components/pages/search/components/InputWithMagnifier/InputWithMagnifier.tsx
+++ frontend/src/components/pages/search/components/InputWithMagnifier/InputWithMagnifier.tsx
@@ -1,5 +1,6 @@
 import { Search } from 'components/Icons/Search';
 import React, { ChangeEvent, FunctionComponent, KeyboardEvent } from 'react';
+import { useIntl } from 'react-intl';
 import { colorPalette } from 'stylesheet';
 import CustomizedInput from './CustomizedInput.style';
 
@@ -14,6 +15,8 @@ const InputWithMagnifier: FunctionComponent<InputWithMagnifierProps> = ({
   value,
   onButtonClick,
 }) => {
+  const intl = useIntl();
+
   const onInputEnterPress = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       onButtonClick();
@@ -27,7 +30,7 @@ const InputWithMagnifier: FunctionComponent<InputWithMagnifierProps> = ({
         value={value !== null ? value : ''}
         type="text"
         onKeyPress={onInputEnterPress}
-        placeholder="Rechercher"
+        placeholder={intl.formatMessage({ id: 'search.textFilter' })}
       />
       <div
         className="w-10 h-10 desktop:h-12 desktop:w-12 bg-primary1 rounded-r-md
