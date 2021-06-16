e2cad418329d3d4b5e2735a73877601398d59ec5
Sylvain Chauffaille <contact@sylchauf.net>
2021-06-16 11:34:54 +0200

    Add purgeCSS + Remove unused npm package

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -14,5 +14,17 @@ module.exports = {
         },
       },
     ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: {
+          standard: [/^slick-/, /dropdown/, /^leaflet-/]
+        }
+      }
+    ],
   ],
 };
