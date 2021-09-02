27ea541010ca356ececdffb53291ea9b18505b27
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-09-02 11:16:10 +0200

    Add helper to parse and remove HTML entities/tag from string

--- frontend/src/modules/utils/string.ts
+++ frontend/src/modules/utils/string.ts
@@ -1,2 +1,9 @@
 export const isUrlString = (url: string | string[] | undefined): url is string =>
   url !== undefined && typeof url === 'string';
+
+export const cleanHTMLElementsFromString = (str: string | undefined = ''): string => {
+  const div = document.createElement('div');
+  div.innerHTML = str;
+  const content = div.textContent as string;
+  return content.trim();
+};
