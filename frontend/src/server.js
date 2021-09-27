48685b0052e085577cf98bf86bb02ae1886a5ef0
Sylvain Chauffaille <contact@sylchauf.net>
2021-09-27 09:10:23 +0200

    Fix Dockerfile

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -2,7 +2,7 @@ const compression = require('compression');
 const express = require('express');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 80;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
