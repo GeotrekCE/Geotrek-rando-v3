acd0c59a15b9866ee956cb05c650ab15ad01bdd3
Sylvain Chauffaille <contact@sylchauf.net>
2021-09-30 16:41:35 +0200

    Fix manifest.json syntax

--- frontend/src/pages/manifest.json.tsx
+++ frontend/src/pages/manifest.json.tsx
@@ -10,20 +10,20 @@ class ManifestJson extends React.Component {
     const appName = String(global.applicationName);
 
     const content = `{
-  name: '${appName}',
-  theme_color: '#ffffff',
-  background_color: '#ffffff',
-  display: 'standalone',
-  orientation: 'portrait',
-  icons: [
-    { src: '/medias/maskable-icon.png', sizes: '200x200', purpose: 'maskable', type: 'image/png' },
-    { src: '/medias/apple-icon.png', sizes: '144x144', type: 'image/png' },
-    { src: '/medias/android-icon.png', sizes: '144x144', type: 'image/png' },
-    { src: '/medias/apple-splashscreen.png', sizes: '512x512', type: 'image/png' },
-    { src: '/medias/android-splashscreen.png', sizes: '512x512', type: 'image/png' },
+  "name": "${appName}",
+  "theme_color": "#ffffff",
+  "background_color": "#ffffff",
+  "display": "standalone",
+  "orientation": "portrait",
+  "icons": [
+    { "src": "/medias/maskable-icon.png", "sizes": "200x200", "purpose": "maskable", "type": "image/png" },
+    { "src": "/medias/apple-icon.png", "sizes": "144x144", "type": "image/png" },
+    { "src": "/medias/android-icon.png", "sizes": "144x144", "type": "image/png" },
+    { "src": "/medias/apple-splashscreen.png", "sizes": "512x512", "type": "image/png" },
+    { "src": "/medias/android-splashscreen.png", "sizes": "512x512", "type": "image/png" }
   ],
-  scope: '.',
-  start_url: './',
+  "scope": ".",
+  "start_url": "./"
 }`;
 
     res.setHeader('Content-Type', 'text/plain');
