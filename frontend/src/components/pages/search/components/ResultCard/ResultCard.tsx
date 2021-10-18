8a9b43ed9c7dbd62a40b9bd29e8a396c664ce75d
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-10-18 17:04:39 +0200

    Display type of use and labels information values separated by commas

--- frontend/src/components/pages/search/components/ResultCard/ResultCard.tsx
+++ frontend/src/components/pages/search/components/ResultCard/ResultCard.tsx
@@ -139,9 +139,7 @@ export const ResultCard: React.FC<TrekProps | TouristicContentProps> = props => 
                     values.length > 0 && (
                       <div key={label} className="text-greyDarkColored">
                         <span className="font-bold">{`${label} : `}</span>
-                        {values.map(value => (
-                          <span key={value}>{value}</span>
-                        ))}
+                        {values.join(', ')}
                       </div>
                     ),
                 )}
