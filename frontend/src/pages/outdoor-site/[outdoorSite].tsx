d26412e65e219cb3282c6b5f597fb7688e66f1d3
Julien Vallini <vallinij@gmail.com>
2022-02-01 17:25:20 +0100

    fix: use nextjs redirect object instead of ctx.res.writehead - fixes nextjs prefetch mechanism

--- frontend/src/pages/outdoor-site/[outdoorSite].tsx
+++ frontend/src/pages/outdoor-site/[outdoorSite].tsx
@@ -11,6 +11,7 @@ import Custom404 from '../404';
 
 export const getServerSideProps = async (context: {
   locale: string;
+  resolvedUrl: string;
   query: { outdoorSite: string };
   res: any;
   req: any;
@@ -26,7 +27,11 @@ export const getServerSideProps = async (context: {
 
     await queryClient.prefetchQuery(`outdoorSiteDetails-${id}-${context.locale}`, () => details);
 
-    redirectIfWrongUrl(id, details.name, context, routes.OUTDOOR_SITE);
+    const redirect = redirectIfWrongUrl(id, details.name, context, routes.OUTDOOR_SITE);
+    if (redirect)
+      return {
+        redirect,
+      };
 
     return { props: {} };
   } catch (error) {
