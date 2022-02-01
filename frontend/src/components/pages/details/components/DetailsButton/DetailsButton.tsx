4bec084cc0e1c366ce0056032aeb9397cc776c13
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-02-01 10:03:39 +0100

    Use button tag in DetailsButton component if there are no url

--- frontend/src/components/pages/details/components/DetailsButton/DetailsButton.tsx
+++ frontend/src/components/pages/details/components/DetailsButton/DetailsButton.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 
 interface DetailsButtonProps {
-  url: string;
+  url?: string;
   onClick?: (event: React.MouseEvent) => void;
   children: JSX.Element;
 }
 
 export const DetailsButton: React.FC<DetailsButtonProps> = ({ url, onClick, children }) => {
+  const className =
+    'h-12 w-12 grid place-items-center rounded-full shadow-lg text-primary1 bg-white cursor-pointer hover:text-primary1-light transition-all';
+
+  if (url === undefined) {
+    return (
+      <button type="button" onClick={onClick} className={className}>
+        {children}
+      </button>
+    );
+  }
   return (
-    <a
-      href={url}
-      onClick={onClick}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="h-12 w-12
-        grid place-items-center
-        rounded-full shadow-lg
-        text-primary1 bg-white
-        cursor-pointer
-        hover:text-primary1-light transition-all"
-    >
+    <a href={url} onClick={onClick} target="_blank" rel="noopener noreferrer" className={className}>
       {children}
     </a>
   );
