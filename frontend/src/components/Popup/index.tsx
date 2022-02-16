41ca907fbfb279879ddf040c154b26b457cffaa9
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-02-16 18:24:41 +0100

    Improve Popup component: styles and add click outside to close feature

--- frontend/src/components/Popup/index.tsx
+++ frontend/src/components/Popup/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { FormattedMessage } from 'react-intl';
 import { Cross } from 'components/Icons/Cross';
+import styled from 'styled-components';
 
 interface PopupProps {
   title?: string;
@@ -24,14 +25,13 @@ const PopupContent: React.FC<PopupProps> = ({ children, onClose, title }) => {
   }, [onClose]);
 
   return (
-    <div
+    <Overlay
       className="fixed inset-0 overflow-y-auto overscroll-contain"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
-      style={{ zIndex: 901 }}
     >
-      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 overscroll-contain">
+      <div className="flex items-center justify-center min-h-screen text-center sm:block sm:p-0 overscroll-contain">
         <div
           className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
@@ -41,6 +41,7 @@ const PopupContent: React.FC<PopupProps> = ({ children, onClose, title }) => {
           &#8203;
         </span>
 
+        {onClose !== undefined && <ClickOutside onClick={onClose} />}
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div className="flex flex-col bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="flex justify-between mb-4">
@@ -58,7 +59,7 @@ const PopupContent: React.FC<PopupProps> = ({ children, onClose, title }) => {
           </div>
         </div>
       </div>
-    </div>
+    </Overlay>
   );
 };
 
@@ -77,5 +78,17 @@ const Popup: React.FC<PopupProps> = props => {
 
   return createPortal(<PopupContent {...props} />, container);
 };
+
+export const Overlay = styled.div`
+  z-index: 901;
+`;
+
+export const ClickOutside = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
 
 export default Popup;
