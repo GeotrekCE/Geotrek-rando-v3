8dffffb5ee66915fb43b5fb105d0f2bbad08828f
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-02-25 12:02:25 +0100

    Display correctly CardIcon in case of there are no icon

--- frontend/src/components/CardIcon/CardIcon.tsx
+++ frontend/src/components/CardIcon/CardIcon.tsx
@@ -2,6 +2,14 @@ import { colorPalette, fillSvgWithColor } from 'stylesheet';
 import SVG from 'react-inlinesvg';
 import styled from 'styled-components';
 
+interface IconProps {
+  iconUri: string;
+  color?: string;
+}
+interface Props extends IconProps {
+  iconName: string;
+}
+
 const Wrapper = styled.div<{ color?: string }>`
   z-index: 100;
   background: ${props => props.color};
@@ -37,28 +45,39 @@ const StyledSVG = styled(SVG)`
 const Img = styled.img`
   height: 28px;
   width: 28px;
-  border-radius: 50%;
 `;
 
-export const CardIcon: React.FC<{ iconUri: string; iconName: string; color?: string }> = ({
-  iconUri,
-  iconName,
-  color,
-}) => {
+const NoImg = styled.span<{ color?: string }>`
+  display: block;
+  height: 28px;
+  width: 28px;
+  border-radius: 50%;
+  background: ${props => props.color};
+`;
+
+const Icon: React.FC<IconProps> = ({ iconUri, color }) => {
+  if (!iconUri) {
+    return <NoImg color={color} />;
+  }
+  if (RegExp(/(.*).svg/).test(iconUri)) {
+    return (
+      <StyledSVG
+        src={iconUri}
+        className="fill-current p-1"
+        preProcessor={fillSvgWithColor(colorPalette.white)}
+      />
+    );
+  }
+  return <Img src={iconUri} alt="" />;
+};
+
+export const CardIcon: React.FC<Props> = ({ iconUri, iconName, color }) => {
   return (
     <Wrapper
       className="absolute top-4 left-4 h-8 flex items-center w-auto rounded-full shadow-sm text-white border-2 border-white border-solid"
       color={color}
     >
-      {RegExp(/(.*).svg/).test(iconUri) ? (
-        <StyledSVG
-          src={iconUri}
-          className="fill-current p-1"
-          preProcessor={fillSvgWithColor(colorPalette.white)}
-        />
-      ) : (
-        <Img src={iconUri} alt="" />
-      )}
+      <Icon color={color} iconUri={iconUri} />
       <Label>
         <div>{iconName}</div>
       </Label>
