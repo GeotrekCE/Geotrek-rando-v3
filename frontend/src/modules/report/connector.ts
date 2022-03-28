4da7d8234460b42bb3decac62d0dbac07a76012a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-03-28 14:17:47 +0200

    Improve error reports messages

--- frontend/src/modules/report/connector.ts
+++ frontend/src/modules/report/connector.ts
@@ -1,10 +1,10 @@
 import { postReport } from './api';
 
 export const createReport = async (lang: string, params: FormData) => {
-  if (params.get('email') === '') throw new Error('error.missing');
-  if (params.get('problem_magnitude') === '') throw new Error('error.missing');
-  if (params.get('activity') === '') throw new Error('error.missing');
-  if (params.get('category') === '') throw new Error('error.missing');
+  if (params.get('email') === '') throw new Error('error.missing.email');
+  if (params.get('problem_magnitude') === '') throw new Error('error.missing.magnitude');
+  if (params.get('activity') === '') throw new Error('error.missing.activity');
+  if (params.get('category') === '') throw new Error('error.missing.category');
 
   return postReport(lang, params);
 };
