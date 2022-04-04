179e9af22957137e7b943a4ed101fd93ef4db534
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-04-04 16:58:06 +0200

    DRY getConfig helper

--- frontend/src/services/getConfig.js
+++ frontend/src/services/getConfig.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 
-const getConfig = (file, parse = true) => {
-  // Default configuration
-  const defaultPath = `./config/${file}`;
-  let defaultConfig = parse ? {} : '';
-  if (fs.existsSync(defaultPath)) {
-    const content = fs.readFileSync(defaultPath).toString();
-    defaultConfig = parse ? JSON.parse(content) : content;
-  }
-
-  // Override configuration
-  const path = `./customization/config/${file}`;
-  let overrideConfig = parse ? {} : '';
+const getContent = (path, parse) => {
   if (fs.existsSync(path)) {
     const content = fs.readFileSync(path).toString();
-    overrideConfig = parse ? JSON.parse(content) : content;
+    return parse ? JSON.parse(content) : content;
   }
+  parse ? {} : '';
+}
+
+const getConfig = (file, parse = true) => {
+  // Default configuration
+  const defaultConfig = getContent(`./config/${file}`, parse);
+
+  // Override configuration
+  const overrideConfig = getContent(`./customization/config/${file}`, parse);
 
   const merge = (elem1, elem2) => {
     if (Array.isArray(elem1)) return [...elem2, ...elem1];
