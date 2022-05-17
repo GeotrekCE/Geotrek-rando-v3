d822bda898a148214aceeff90b5dc0e26289bc53
Julien-Gr4z <julien_graziani@natural-solutions.eu>
2022-05-17 14:08:22 +0200

    feat: add icon backpack to gear recommandation

--- frontend/src/components/pages/details/components/DetailsGear/DetailsGear.tsx
+++ frontend/src/components/pages/details/components/DetailsGear/DetailsGear.tsx
@@ -1,5 +1,6 @@
 import parse from 'html-react-parser';
 import { HtmlText } from 'components/pages/details/utils';
+import { BackPack } from 'components/Icons/BackPack';
 
 interface DetailsGearProps {
   className?: string;
@@ -14,6 +15,9 @@ export const DetailsGear: React.FC<DetailsGearProps> = ({ className, text }) => 
       rounded-2xl border-2 border-solid border-greyDarkColored
       ${className ?? ''}`}
     >
+      <div className="mr-2 desktop:mr-4 flex-shrink-0 w-6 h-6 desktop:h-12 desktop:w-12 self-start">
+        <BackPack />
+      </div>
       <div className="text-greyDarkColored desktop:font-bold text-Mobile-C2 desktop:text-P1 my-auto">
         <HtmlText>{parse(text)}</HtmlText>
       </div>
