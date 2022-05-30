36f5cb755a87f2579825c655bdc992901631e4f0
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-05-30 17:16:11 +0200

    Allow user to override manifest.json props

--- frontend/src/pages/manifest.json.tsx
+++ frontend/src/pages/manifest.json.tsx
@@ -7,7 +7,7 @@ const ManifestJson = (): null => {
 
 ManifestJson.getInitialProps = (props: NextPageContext) => {
   const {
-    publicRuntimeConfig: { global },
+    publicRuntimeConfig: { global, manifest = {} },
   } = getNextConfig();
 
   const { res } = props;
@@ -17,25 +17,34 @@ ManifestJson.getInitialProps = (props: NextPageContext) => {
   }
 
   const name = String(global.applicationName);
+  const defaultManifest = {
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '/medias/maskable-icon.png',
+        sizes: '200x200',
+        purpose: 'maskable',
+        type: 'image/png',
+      },
+      { src: '/medias/apple-icon.png', sizes: '144x144', type: 'image/png' },
+      { src: '/medias/android-icon.png', sizes: '144x144', type: 'image/png' },
+      { src: '/medias/apple-splashscreen.png', sizes: '512x512', type: 'image/png' },
+      { src: '/medias/android-splashscreen.png', sizes: '512x512', type: 'image/png' },
+    ],
+    scope: '.',
+    start_url: './',
+  };
 
-  const content = `{
-  "name": "${name}",
-  "theme_color": "#ffffff",
-  "background_color": "#ffffff",
-  "display": "standalone",
-  "orientation": "portrait",
-  "icons": [
-    { "src": "/medias/maskable-icon.png", "sizes": "200x200", "purpose": "maskable", "type": "image/png" },
-    { "src": "/medias/apple-icon.png", "sizes": "144x144", "type": "image/png" },
-    { "src": "/medias/android-icon.png", "sizes": "144x144", "type": "image/png" },
-    { "src": "/medias/apple-splashscreen.png", "sizes": "512x512", "type": "image/png" },
-    { "src": "/medias/android-splashscreen.png", "sizes": "512x512", "type": "image/png" }
-  ],
-  "scope": ".",
-  "start_url": "./"
-}`;
+  const content = {
+    name,
+    ...defaultManifest,
+    ...manifest,
+  };
   res.setHeader('Content-Type', 'text/plain');
-  res.write(content);
+  res.write(JSON.stringify(content, null, 2));
   res.end();
 };
 
