fcf13169f3ed768191d05401611b60ac0b00a60c
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-06-21 17:36:23 +0200

    Allow the display of different suggestions groups depending on the lang

--- frontend/src/components/pages/home/useHome.ts
+++ frontend/src/components/pages/home/useHome.ts
@@ -2,20 +2,30 @@ import { flatten } from 'lodash';
 import { getActivitySuggestions } from 'modules/activitySuggestions/connector';
 import { ActivitySuggestion } from 'modules/activitySuggestions/interface';
 import { getDefaultLanguage } from 'modules/header/utills';
-import { getHomePageConfig } from 'modules/home/utils';
+import { HomePageConfig } from 'modules/home/interface';
+import { adaptSuggestions, getHomePageConfig } from 'modules/home/utils';
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
+interface UseHome {
+  config: HomePageConfig;
+  suggestions: ActivitySuggestion[];
+}
 
-export const useHome = () => {
+export const useHome = (): UseHome => {
   const homePageConfig = getHomePageConfig();
-
-  const activitySuggestionIds = flatten(homePageConfig.suggestions.map(s => s.ids));
-
   const language = useRouter().locale ?? getDefaultLanguage();
-  const { data: suggestions } = useQuery<ActivitySuggestion[], Error>(
+  const suggestions = adaptSuggestions(homePageConfig.suggestions, language);
+
+  if (suggestions === null) {
+    return { config: homePageConfig, suggestions: [] };
+  }
+
+  const activitySuggestionIds = flatten(suggestions.map(s => s.ids));
+
+  const { data = [] } = useQuery<ActivitySuggestion[], Error>(
     ['activitySuggestions', activitySuggestionIds.join('-'), language],
-    () => getActivitySuggestions(homePageConfig.suggestions, language),
+    () => getActivitySuggestions(suggestions, language),
   );
 
-  return { config: homePageConfig, suggestions: suggestions as ActivitySuggestion[] };
+  return { config: homePageConfig, suggestions: data };
 };
