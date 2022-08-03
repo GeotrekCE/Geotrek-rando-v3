3de862f2258cf5ce6b81be52eb1f528b68046935
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-08-03 14:59:57 +0200

    Refacto useShowOnScrollPosition hook

--- frontend/src/hooks/useShowOnScrollPosition.ts
+++ frontend/src/hooks/useShowOnScrollPosition.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export type Display = 'DISPLAYED' | 'HIDDEN';
 
@@ -6,31 +6,34 @@ export type Display = 'DISPLAYED' | 'HIDDEN';
  * Returns "DISPLAYED" if the scroll position is above minScrollPosition, "HIDDEN" otherwise
  */
 export const useShowOnScrollPosition = (minScrollPosition: number): Display => {
-  let previousPosition = 0;
+  const previousPosition = useRef(0);
   // Necessary to avoid errors with Nextjs
   if (typeof window !== 'undefined') {
-    previousPosition = window.scrollY;
+    previousPosition.current = window.scrollY;
   }
   const [displayState, setDisplayState] = useState<Display>(
-    previousPosition > minScrollPosition ? 'DISPLAYED' : 'HIDDEN',
+    previousPosition.current > minScrollPosition ? 'DISPLAYED' : 'HIDDEN',
   );
 
-  const handleScrolling = () => {
+  const handleScrolling = useCallback(() => {
     const currentPosition = window.scrollY;
-    if (currentPosition > minScrollPosition && previousPosition <= minScrollPosition) {
+    if (currentPosition > minScrollPosition && previousPosition.current <= minScrollPosition) {
       setDisplayState('DISPLAYED');
-    } else if (currentPosition <= minScrollPosition && previousPosition > minScrollPosition) {
+    } else if (
+      currentPosition <= minScrollPosition &&
+      previousPosition.current > minScrollPosition
+    ) {
       setDisplayState('HIDDEN');
     }
-    previousPosition = currentPosition;
-  };
+    previousPosition.current = currentPosition;
+  }, [minScrollPosition]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScrolling);
     return () => {
       window.removeEventListener('scroll', handleScrolling);
     };
-  }, []);
+  }, [handleScrolling]);
 
   return displayState;
 };
