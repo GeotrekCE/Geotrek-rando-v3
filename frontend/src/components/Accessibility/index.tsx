4f710c5d5126cac2b41b0734a0eeaf16360c6c6a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-08-16 18:27:25 +0200

    Refacto shouldPictureRowBeDisplayed helper without lodash sum helper

--- frontend/src/components/Accessibility/index.tsx
+++ frontend/src/components/Accessibility/index.tsx
@@ -4,7 +4,6 @@ import { Modal } from 'components/Modal';
 import { CardSingleImage } from 'components/pages/details/components/DetailsCard';
 import { HtmlText } from 'components/pages/details/utils';
 import parse from 'html-react-parser';
-import { sum } from 'lodash';
 import { AccessibilityAttachment, Details } from 'modules/details/interface';
 import { getGlobalConfig } from 'modules/utils/api.config';
 import { FormattedMessage } from 'react-intl';
@@ -16,16 +15,14 @@ const Accessibility = ({ details, language }: { details: Details; language: stri
   const accessibilityCodeNumber = getGlobalConfig().accessibilityCodeNumber;
 
   const shouldPictureRowBeDisplayed = details.attachmentsAccessibility
-    ? sum(
-        ['slope', 'width', 'signage']
-          .filter(k => (details as any)[`accessibility_${k}`])
-          .map(k => {
-            const attachments = details.attachmentsAccessibility.filter(
-              a => a.info_accessibility === k,
-            );
-            return attachments.length;
-          }),
-      ) > 0
+    ? ['slope', 'width', 'signage']
+        .filter(k => (details as any)[`accessibility_${k}`])
+        .flatMap(k => {
+          const attachments = details.attachmentsAccessibility.filter(
+            a => a.info_accessibility === k,
+          );
+          return attachments.length;
+        }).length > 0
     : false;
 
   return (
