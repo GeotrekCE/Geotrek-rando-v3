a55d3b037b6dc108eff9e3d13bda213603c91d85
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-09-14 16:03:27 +0200

    Change configuration of sentry

---
+++ frontend/src/sentry.client.config.js
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+const sentryOptions = {
+  dsn: SENTRY_DSN,
+  release: process.env.VERSION,
+  environment: process.env.ENVIRONMENT,
+  maxBreadcrumbs: 50,
+  attachStacktrace: true,
+};
+
+// Development & tests setup
+if (process.env.NODE_ENV !== 'production') {
+  // Don't actually send the errors to Sentry
+  sentryOptions.beforeSend = () => null;
+}
+
+Sentry.init(sentryOptions);
