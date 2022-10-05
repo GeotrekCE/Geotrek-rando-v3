5242b3df7128d876fefc406c7cbc2ad6f5cee7c9
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-10-05 13:57:49 +0200

    Display Catalan flag for Catalan version

--- frontend/src/services/i18n/intl.ts
+++ frontend/src/services/i18n/intl.ts
@@ -23,4 +23,20 @@ export const flattenMessages = (
     return messages;
   }, {});
 
+export const getCountryCodeFromLanguage = (language: string) => {
+  const [lng, region] = language.toUpperCase().split('-');
+  if (lng === 'EN') {
+    // Canada / USA / etc
+    if (region) {
+      return region;
+    }
+    return 'GB';
+  }
+  // Catalan does not have an official country code
+  if (lng === 'CA') {
+    return 'ES-CT';
+  }
+  return lng;
+};
+
 export default flattenMessages;
