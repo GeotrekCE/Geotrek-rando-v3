c4320ff811edea0b9ce0564f64280c95bb377e80
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-10-25 12:15:16 +0200

    Add the ability to secure form with captcha

--- frontend/src/modules/report/connector.ts
+++ frontend/src/modules/report/connector.ts
@@ -1,10 +1,15 @@
+import { getGlobalConfig } from 'modules/utils/api.config';
 import { postReport } from './api';
 
 export const createReport = async (lang: string, params: FormData) => {
+  const { hCaptchaKey } = getGlobalConfig();
+
   if (params.get('email') === '') throw new Error('error.missing.email');
   if (params.get('problem_magnitude') === '') throw new Error('error.missing.magnitude');
   if (params.get('activity') === '') throw new Error('error.missing.activity');
   if (params.get('category') === '') throw new Error('error.missing.category');
+  if (hCaptchaKey !== null && params.get('g-recaptcha-response') === '')
+    throw new Error('error.missing.captcha');
 
   return postReport(lang, params);
 };
