42b1497837de82b5f2b11fc62825ac76b2e1b110
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-10-26 18:09:01 +0200

    Rewrite Loader component

--- frontend/src/components/Loader/Loader.style.ts
+++ frontend/src/components/Loader/Loader.style.ts
@@ -1,97 +1,76 @@
 import styled, { keyframes } from 'styled-components';
-import { colorPalette } from 'stylesheet';
-
-const loaderAnimation = keyframes`
-  0%, 20%, 80%, 100% {
-    transform: scale(1);
-  }
-  50% {
-    transform: scale(1.5);
-  }
-`;
-
-export const LoaderWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 // Loader from https://loading.io/css/
-export const LoaderContainer = styled.div`
-  display: inline-block;
-  position: relative;
-  width: 64px;
-  height: 64px;
+const loaderAnimation = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 `;
-
 export const Dot = styled.div`
-  position: absolute;
-  width: 5px;
-  height: 5px;
-  background: ${colorPalette.primary3};
-  border-radius: 50%;
+  transform-origin: 30px 30px;
   animation: ${loaderAnimation} 1.2s linear infinite;
 
+  &::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 6px;
+    left: 28px;
+    width: 5px;
+    height: 12px;
+    border-radius: 20%;
+    background: currentColor;
+  }
+
   :nth-child(1) {
-    animation-delay: 0s;
-    top: 29px;
-    left: 53px;
+    transform: rotate(0deg);
+    animation-delay: -1.1s;
   }
   :nth-child(2) {
-    animation-delay: -0.1s;
-    top: 18px;
-    left: 50px;
+    transform: rotate(30deg);
+    animation-delay: -1s;
   }
   :nth-child(3) {
-    animation-delay: -0.2s;
-    top: 9px;
-    left: 41px;
+    transform: rotate(60deg);
+    animation-delay: -0.9s;
   }
   :nth-child(4) {
-    animation-delay: -0.3s;
-    top: 6px;
-    left: 29px;
+    transform: rotate(90deg);
+    animation-delay: -0.8s;
   }
   :nth-child(5) {
-    animation-delay: -0.4s;
-    top: 9px;
-    left: 18px;
+    transform: rotate(120deg);
+    animation-delay: -0.7s;
   }
   :nth-child(6) {
-    animation-delay: -0.5s;
-    top: 18px;
-    left: 9px;
+    transform: rotate(150deg);
+    animation-delay: -0.6s;
   }
   :nth-child(7) {
-    animation-delay: -0.6s;
-    top: 29px;
-    left: 6px;
+    transform: rotate(180deg);
+    animation-delay: -0.5s;
   }
   :nth-child(8) {
-    animation-delay: -0.7s;
-    top: 41px;
-    left: 9px;
+    transform: rotate(210deg);
+    animation-delay: -0.4s;
   }
   :nth-child(9) {
-    animation-delay: -0.8s;
-    top: 50px;
-    left: 18px;
+    transform: rotate(240deg);
+    animation-delay: -0.3s;
   }
   :nth-child(10) {
-    animation-delay: -0.9s;
-    top: 53px;
-    left: 29px;
+    transform: rotate(270deg);
+    animation-delay: -0.2s;
   }
   :nth-child(11) {
-    animation-delay: -1s;
-    top: 50px;
-    left: 41px;
+    transform: rotate(300deg);
+    animation-delay: -0.1s;
   }
   :nth-child(12) {
-    animation-delay: -1.1s;
-    top: 41px;
-    left: 50px;
+    transform: rotate(330deg);
+    animation-delay: 0s;
   }
 `;
