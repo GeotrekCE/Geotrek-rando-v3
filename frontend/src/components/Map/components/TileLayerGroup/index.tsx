99f038b70dbe80317286830afc4cdebde73aca23
Florian Sommariva <florian.sommariva@makina-corpus.com>
2022-12-01 14:33:15 +0100

    Fix map zoom range in offline mode

--- frontend/src/components/Map/components/TileLayerGroup/index.tsx
+++ frontend/src/components/Map/components/TileLayerGroup/index.tsx
@@ -1,4 +1,5 @@
 import { TileLayer } from 'components/Map/interface';
+import { getMapConfig } from 'components/Map/config';
 import TileLayerExtended from '../TileLayerExtended';
 
 interface TileLayerGroupProps {
@@ -9,9 +10,13 @@ const TileLayerGroup: React.FC<TileLayerGroupProps> = ({ layers }) => {
   if (layers.length === 0) {
     return null;
   }
+  const { maximumZoomLevel, zoomAvailableOffline = [0, maximumZoomLevel] } = getMapConfig();
 
   const groupRangeZoom = layers.reduce(
-    (list, { options: { minZoom = 0, maxZoom = 20 } = {} }) => {
+    (list, { options: { minZoom = 0, maxZoom = maximumZoomLevel } = {} }) => {
+      if (navigator.onLine === false) {
+        return [Math.min(...zoomAvailableOffline), Math.max(...zoomAvailableOffline)];
+      }
       return [Math.min(minZoom, list[0]), Math.max(maxZoom, list[1])];
     },
     [Infinity, -Infinity],
