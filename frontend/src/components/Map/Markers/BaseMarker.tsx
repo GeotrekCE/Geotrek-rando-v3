ee9dc2f523a76bfb247ef3473f4c3763481209f6
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-01-26 15:58:47 +0100

    Remove useless code

--- frontend/src/components/Map/Markers/BaseMarker.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Empty marker background with no picto in it
- */
-export const BaseMarker: React.FC<{ className: string }> = ({ className }) => {
-  return <img src="/icons/active-map-marker.svg" className={className} alt="" />;
-};
