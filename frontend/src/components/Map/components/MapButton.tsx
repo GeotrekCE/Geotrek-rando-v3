69d368c11b8170c3d19734c7008462a019feec84
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-02-10 11:58:14 +0100

    A11Y: Add a label to close full screen mode

--- frontend/src/components/Map/components/MapButton.tsx
+++ frontend/src/components/Map/components/MapButton.tsx
@@ -5,11 +5,11 @@ interface Props {
   className?: string;
 }
 
-export const MapButton: React.FC<Props> = ({ icon, onClick, className = '', children }) => (
+export const MapButton: React.FC<Props> = ({ icon, className = '', children, ...props }) => (
   <button
     type="button"
-    onClick={onClick}
     className={`rounded-full p-2 absolute top-8 left-8 shadow-md bg-white z-mapButton flex justify-center items-center text-greyDarkColored ${className}`}
+    {...props}
   >
     {icon} {children !== undefined && <span className="ml-2">{children}</span>}
   </button>
