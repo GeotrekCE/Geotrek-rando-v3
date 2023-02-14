f0df58ae0d6d3557edfd5a74314689749619c199
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-02-14 15:46:45 +0100

    Define precisely title and h1 tags for search page

--- frontend/src/components/pages/search/components/SearchResultsMeta/__test__/SearchResultsMeta.test.tsx
+++ frontend/src/components/pages/search/components/SearchResultsMeta/__test__/SearchResultsMeta.test.tsx
@@ -2,19 +2,7 @@ import { render } from 'services/testing/reactTestingLibraryWrapper';
 
 import { SearchResultsMeta } from '../SearchResultsMeta';
 
-describe('AAU, I can see informations on my search', () => {
-  it('with 0 results', () => {
-    const component = render(<SearchResultsMeta resultsNumber={0} />);
-    expect(component).toMatchSnapshot();
-  });
-
-  it('with 1 result', () => {
-    const component = render(<SearchResultsMeta resultsNumber={1} />);
-    expect(component).toMatchSnapshot();
-  });
-
-  it('with many results', () => {
-    const component = render(<SearchResultsMeta resultsNumber={123} />);
-    expect(component).toMatchSnapshot();
-  });
+it('AAU, I can see informations on my search', () => {
+  const component = render(<SearchResultsMeta textContent="XX results" />);
+  expect(component).toMatchSnapshot();
 });
