70af26d7570361bdbf6dd43d8ef807b527249073
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-03-03 10:18:28 +0100

    Homogenize Suggestions homepage for all content types

--- frontend/src/components/pages/home/components/HomeSection/HomeSection.tsx
+++ frontend/src/components/pages/home/components/HomeSection/HomeSection.tsx
@@ -1,10 +1,6 @@
-import {
-  generateOutdoorSiteUrl,
-  generateTouristicContentUrl,
-  generateTouristicEventUrl,
-} from 'components/pages/details/utils';
+import { generateDetailsUrlFromType } from 'components/pages/details/utils';
 import { ResultCard } from 'components/pages/search/components/ResultCard';
-import { generateResultDetailsUrl, getHoverId } from 'components/pages/search/utils';
+import { getHoverId } from 'components/pages/search/utils';
 import { ActivitySuggestion } from 'modules/activitySuggestions/interface';
 import SVG from 'react-inlinesvg';
 import styled from 'styled-components';
@@ -14,10 +10,9 @@ export interface HomeSectionProps {
   title: string;
   iconUrl: string;
   results: ActivitySuggestion['results'];
-  type: string;
 }
 
-export const HomeSection: React.FC<HomeSectionProps> = ({ title, iconUrl, results, type }) => {
+export const HomeSection: React.FC<HomeSectionProps> = ({ title, iconUrl, results }) => {
   return (
     <div id={'home_section'} className={`flex flex-col`}>
       <h2
@@ -31,107 +26,34 @@ export const HomeSection: React.FC<HomeSectionProps> = ({ title, iconUrl, result
         />
         <span className="mt-1 desktop:mt-0 text-H2 desktop:text-H2 font-bold">{title}</span>
       </h2>
-      <ScrollContainer>
-        {results !== undefined &&
-          // eslint-disable-next-line
-          results.map((e: any) => {
-            if (type === 'trek')
-              return (
-                <ResultCard
-                  asColumn
-                  type={e.type}
-                  key={e.name}
-                  id={e.id}
-                  hoverId={getHoverId(e)}
-                  place={e.place}
-                  title={e.name}
-                  tags={e.themes}
-                  attachments={e.attachments.slice(0, 1)}
-                  badgeIconUri={e.category?.pictogramUri}
-                  badgeName={e.category?.label}
-                  informations={e.informations}
-                  redirectionUrl={generateResultDetailsUrl(e.id, e.name)}
-                  className="my-4 desktop:my-6 desktop:mx-1" // Height is not limited to let the card grow with long text & informations. Most photos are not vertical, and does not have to be restrained.
-                />
-              );
-            else if (type === 'service')
-              return (
-                <ResultCard
-                  asColumn
-                  type={e.type}
-                  key={e.name}
-                  id={e.id}
-                  hoverId={getHoverId(e)}
-                  place={e.place}
-                  title={e.name}
-                  tags={e.themes}
-                  attachments={e.attachments.slice(0, 1)}
-                  badgeIconUri={e.category.pictogramUri}
-                  badgeName={e.category?.label}
-                  informations={e.types}
-                  redirectionUrl={generateTouristicContentUrl(e.id, e.name)}
-                  className="my-4 desktop:my-6 desktop:mx-1" // Height is limited in desktop to restrain vertical images ; not limiting with short text & informations
-                />
-              );
-            else if (type === 'outdoor')
-              return (
-                <ResultCard
-                  asColumn
-                  type={e.type}
-                  key={e.name}
-                  id={e.id}
-                  hoverId={getHoverId(e)}
-                  place={e.place}
-                  title={e.name}
-                  tags={e.themes}
-                  attachments={e.attachments.slice(0, 1)}
-                  badgeIconUri={e.category?.pictogramUri}
-                  badgeName={e.category?.label}
-                  informations={[]}
-                  redirectionUrl={generateOutdoorSiteUrl(e.id, e.name)}
-                  className="my-4 desktop:my-6 desktop:mx-1" // Height is limited in desktop to restrain vertical images ; not limiting with short text & informations
-                />
-              );
-            else if (type === 'events')
-              return (
-                <ResultCard
-                  asColumn
-                  type={e.type}
-                  key={e.name}
-                  id={e.id}
-                  hoverId={getHoverId(e)}
-                  place={e.place}
-                  title={e.name}
-                  tags={e.themes}
-                  attachments={e.attachments.slice(0, 1)}
-                  badgeIconUri={e.category?.pictogramUri}
-                  badgeName={e.category?.label}
-                  informations={e.informations}
-                  redirectionUrl={generateTouristicEventUrl(e.id, e.name)}
-                  className="my-4 desktop:my-6 desktop:mx-1" // Height is limited in desktop to restrain vertical images ; not limiting with short text & informations
-                />
-              );
-          })}
+      <ScrollContainer className="flex p-5 overflow-scroll desktop:overflow-auto desktop:mx-10percent desktop:px-0 desktop:grid desktop:grid-cols-3 desktop:gap-3">
+        {results.map(e => {
+          return (
+            <ResultCard
+              asColumn
+              type={e.type}
+              key={e.name}
+              id={e.id}
+              hoverId={getHoverId(e)}
+              place={e.place}
+              title={e.name}
+              tags={e.tags}
+              attachments={e.attachments.slice(0, 1)}
+              badgeIconUri={e.category?.pictogramUri}
+              badgeName={e.category?.label}
+              informations={e.informations}
+              redirectionUrl={generateDetailsUrlFromType(e.type, e.id, e.name)}
+              className="my-4 desktop:my-6 desktop:mx-1" // Height is not limited to let the card grow with long text & informations. Most photos are not vertical, and does not have to be restrained.
+            />
+          );
+        })}
       </ScrollContainer>
     </div>
   );
 };
 
 const ScrollContainer = styled.div`
-  display: flex;
-  padding: 20px;
-
-  @media (min-width: 1024px) {
-    margin-left: 10%;
-    margin-right: 10%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
-  }
-
   @media (max-width: 1024px) {
-    overflow: scroll;
-
     & > * {
       flex: auto;
       max-width: 300px;
