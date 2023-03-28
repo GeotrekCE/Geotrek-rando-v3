72b66ad1cda45efb4c071a84599a124663507686
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-03-28 17:49:40 +0200

    Clean useless files

--- frontend/src/components/Input/__tests__/Input.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'services/testing/reactTestingLibraryWrapper';
-
-import Input from '../';
-
-test('AAU, I can see an Input', () => {
-  render(<Input hasError={false} />);
-});
