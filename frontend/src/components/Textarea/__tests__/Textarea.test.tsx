d779a126d1a84f863f5549e34be54e496cb1c4a8
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-03-28 17:49:40 +0200

    Clean useless files

--- frontend/src/components/Textarea/__tests__/Textarea.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'services/testing/reactTestingLibraryWrapper';
-
-import Textarea from '../';
-
-test('AAU, I can see a Textarea', () => {
-  render(<Textarea hasError={false} />);
-});
