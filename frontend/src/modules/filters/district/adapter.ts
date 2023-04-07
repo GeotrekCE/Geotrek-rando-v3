7986724067e248355a74f5586ca046675efaf5e5
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-04-07 15:00:03 +0200

    Handle cities and districts values as string like others filters values

--- frontend/src/modules/filters/district/adapter.ts
+++ frontend/src/modules/filters/district/adapter.ts
@@ -5,7 +5,7 @@ import { RawDistrict } from './interface';
 export const adaptDistrictFilter = (rawDistricts: RawDistrict[]): FilterWithoutType => ({
   id: DISTRICT_ID,
   options: rawDistricts.map(rawDistrict => ({
-    value: rawDistrict.id,
+    value: `${rawDistrict.id}`,
     label: rawDistrict.name,
   })),
 });
