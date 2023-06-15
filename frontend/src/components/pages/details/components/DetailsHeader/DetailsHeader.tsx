a7f1284a43257353045e72f3e6d3e97484d9267f
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-06-15 15:08:36 +0200

    Set anchors to navigate between sections of details pages

--- frontend/src/components/pages/details/components/DetailsHeader/DetailsHeader.tsx
+++ frontend/src/components/pages/details/components/DetailsHeader/DetailsHeader.tsx
@@ -1,7 +1,8 @@
 import { DetailsDownloadIcons } from 'components/pages/details/components/DetailsDownloadIcons';
 import React, { MutableRefObject } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { sizes } from 'stylesheet';
+import { cn } from 'services/utils/cn';
+import useHasMounted from 'hooks/useHasMounted';
 import { Details } from '../../../../../modules/details/interface';
 import { OutdoorCourseDetails } from '../../../../../modules/outdoorCourse/interface';
 import { OutdoorSiteDetails } from '../../../../../modules/outdoorSite/interface';
@@ -15,55 +16,43 @@ interface DetailsHeaderProps {
   type: 'TREK' | 'OUTDOOR_SITE' | 'OUTDOOR_COURSE' | 'TOURISTIC_EVENT';
 }
 
-const scrollTo = (element: HTMLDivElement | undefined | null) => {
-  if (element !== null && element !== undefined) {
-    // offsetTop : offset to the closest relative parent
-    const adjustedPosition =
-      element.offsetTop +
-      sizes.coverDetailsDesktop +
-      sizes.desktopHeader -
-      sizes.detailsHeaderDesktop -
-      sizes.topIconsDetailsDesktop;
-    window.scrollTo({ top: adjustedPosition, behavior: 'smooth' });
-  }
-};
-
 export const DetailsHeader: React.FC<DetailsHeaderProps> = ({
   sectionsReferences,
   details,
   type,
 }) => {
   const { detailsHeaderSection, currentSectionId } = useDetailsHeader(sectionsReferences);
+  const isMounted = useHasMounted();
   return (
-    <div
+    <nav
       id="details_headerDesktop"
       className="hidden desktop:flex
       sticky top-desktopHeader z-subHeader
-      shadow-md bg-white"
-      style={{ height: 55 }}
+      shadow-md bg-white h-14"
+      role="navigation"
     >
-      <div id="details_headerDesktop_inlineMenu" className="flex flex-1 pb-2.5 pt-4 ml-3">
+      <ul
+        id="details_headerDesktop_inlineMenu"
+        className="flex flex-1 pb-2.5 pt-4 ml-3 text-center"
+      >
         {(Object.keys(detailsHeaderSection) as Array<keyof DetailsHeaderSection>)
+          // Report anchor is in <DetailsDownloadIcons /> below
           .filter(sectionId => sectionId !== 'report')
           .map(sectionId => (
-            <div
-              onClick={() => scrollTo(detailsHeaderSection[sectionId])}
-              key={sectionId}
-              className="text-center"
-            >
-              <span
-                className={`hover:text-primary1 mx-5
-                pb-1 border-b-2 hover:border-primary1 border-transparent border-solid
-                cursor-pointer transition-all duration-300 ${
-                  currentSectionId === sectionId ? 'text-primary1 border-primary1' : ''
-                }`}
+            <li key={sectionId}>
+              <a
+                className={cn(
+                  'mx-5 pb-1 border-b-2 border-transparent border-solid transition-all duration-300  hover:text-primary1 hover:border-primary1  focus:text-primary1 focus:border-primary1',
+                  currentSectionId === sectionId && isMounted && 'text-primary1 border-primary1',
+                )}
+                href={`#details_${sectionId}`}
               >
                 <FormattedMessage id={`details.${sectionId}`} />
-              </span>
-            </div>
+              </a>
+            </li>
           ))}
-      </div>
+      </ul>
       <DetailsDownloadIcons details={details} hideReport={type !== 'TREK'} />
-    </div>
+    </nav>
   );
 };
