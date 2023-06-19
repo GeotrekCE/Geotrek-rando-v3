9a1691f739b01c84dade07e7bb7b244228c8f691
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-06-19 11:14:03 +0200

    Rework detailsCard

--- frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
+++ frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
+import debounce from 'debounce';
+import useIsomorphicLayoutEffect from 'hooks/useIsomorphicLayoutEffect';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const DETAILS_CARD_DEFAULT_HEIGHT = 200;
 
 export const useDetailsCard = () => {
-  const [truncateState, setTruncateState] = useState<'TRUNCATE' | 'FULL'>('TRUNCATE');
   const detailsCardRef = useRef<HTMLDivElement>(null);
   const [heightState, setHeightState] = useState(DETAILS_CARD_DEFAULT_HEIGHT);
+  const [truncateState, setTruncateState] = useState<'NONE' | 'TRUNCATE' | 'FULL'>(
+    () => 'TRUNCATE',
+  );
+
   const toggleTruncateState = () =>
     setTruncateState(currentTruncateState =>
       currentTruncateState === 'TRUNCATE' ? 'FULL' : 'TRUNCATE',
@@ -19,6 +24,58 @@ export const useDetailsCard = () => {
         setHeightState(newHeight);
       }
     }
-  }, [truncateState]);
+  }, [truncateState, setHeightState]);
+
+  useEffect(() => {
+    if (
+      detailsCardRef.current &&
+      detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2')?.offsetHeight ===
+        detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2')?.scrollHeight
+    ) {
+      setTruncateState('NONE');
+    }
+  }, []);
+
+  const handleResize = useCallback(
+    debounce(
+      () => {
+        setTruncateState(prevState => {
+          if (detailsCardRef.current === null) {
+            return prevState;
+          }
+          if (
+            prevState === 'TRUNCATE' &&
+            detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2')?.offsetHeight ===
+              detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2')?.scrollHeight
+          ) {
+            return 'NONE';
+          } else if (
+            prevState === 'FULL' &&
+            heightState >= detailsCardRef.current?.getBoundingClientRect().height
+          ) {
+            return 'NONE';
+          } else if (
+            prevState === 'NONE' &&
+            heightState < detailsCardRef.current?.getBoundingClientRect().height
+          ) {
+            setHeightState(DETAILS_CARD_DEFAULT_HEIGHT);
+            return 'TRUNCATE';
+          }
+          return prevState;
+        });
+      },
+      1000,
+      false,
+    ),
+    [setTruncateState, setHeightState, detailsCardRef],
+  );
+
+  useIsomorphicLayoutEffect(() => {
+    global.addEventListener('resize', handleResize);
+    return () => {
+      global.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return { truncateState, toggleTruncateState, heightState, detailsCardRef };
 };
