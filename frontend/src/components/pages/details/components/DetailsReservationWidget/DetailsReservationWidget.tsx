466a0b19fdae968d03adfc0e0033a5a6331c621d
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-07-04 15:26:08 +0200

    Synchronously load scripts for the Reservations widget

--- frontend/src/components/pages/details/components/DetailsReservationWidget/DetailsReservationWidget.tsx
+++ frontend/src/components/pages/details/components/DetailsReservationWidget/DetailsReservationWidget.tsx
@@ -1,5 +1,5 @@
 import { Reservation } from 'modules/details/interface';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Loader from 'components/Loader';
 import Script from 'next/script';
 import Head from 'next/head';
@@ -17,18 +17,7 @@ interface DetailsReservationWidgetProps {
   reservation: Reservation;
   language: string;
 }
-const waitForGlobal = async (key: string) => {
-  return new Promise(resolve => {
-    if (key in window) {
-      resolve(true);
-    } else {
-      // eslint-disable-next-line
-      setTimeout(() => {
-        return waitForGlobal(key);
-      }, 100);
-    }
-  });
-};
+
 export const DetailsReservationWidget: React.FC<DetailsReservationWidgetProps> = ({
   id,
   reservation: { partner, project },
@@ -36,6 +25,7 @@ export const DetailsReservationWidget: React.FC<DetailsReservationWidgetProps> =
 }) => {
   const { asPath } = useRouter();
   const isMounted = useHasMounted();
+  const [stepsLoadedScript, setStepsLoadedScript] = useState<number>(0);
 
   const onLoad = useCallback(() => {
     const layer = {
@@ -45,20 +35,8 @@ export const DetailsReservationWidget: React.FC<DetailsReservationWidgetProps> =
       routeId: id,
       routePage: asPath,
     };
-    (core => {
-      void waitForGlobal('eitinerance')
-        .then(() => waitForGlobal('AllianceReseaux'))
-        .then(() => {
-          const spaClient = core.pages?.getSinglePageApplicationClient({ layer });
-          window.AllianceReseaux.jQuery(function () {
-            if (spaClient !== undefined) {
-              spaClient.executePage();
-            } else {
-              onLoad();
-            }
-          });
-        });
-    })(window?.eitinerance?.core);
+    const spaClient = window?.eitinerance?.core?.pages?.getSinglePageApplicationClient({ layer });
+    spaClient?.executePage();
   }, [asPath, id, language, partner]);
 
   useEffect(() => {
@@ -88,13 +66,22 @@ export const DetailsReservationWidget: React.FC<DetailsReservationWidgetProps> =
         {/* Hide modal due to user inactivity */}
         <style>{`.osi-modal { display: none !important }`}</style>
       </Head>
-      <Script src="https://gadget.open-system.fr/widgets-libs/rel/noyau-2.0.min.js" />
-      <Script src="https://eitinerancecdn.open-system.fr/widgets-eiti/core/core.min.js" />
       <Script
-        src={`https://eitinerancecdn.open-system.fr/widgets-eiti/${project}/${project}.min.js`}
-        onLoad={() => onLoad()}
-        strategy="lazyOnload"
+        src="https://gadget.open-system.fr/widgets-libs/rel/noyau-2.0.min.js"
+        onLoad={() => setStepsLoadedScript(1)}
       />
+      {stepsLoadedScript > 0 && (
+        <Script
+          src="https://eitinerancecdn.open-system.fr/widgets-eiti/core/core.min.js"
+          onLoad={() => setStepsLoadedScript(2)}
+        />
+      )}
+      {stepsLoadedScript > 1 && (
+        <Script
+          src={`https://eitinerancecdn.open-system.fr/widgets-eiti/${project}/${project}.min.js`}
+          onLoad={onLoad}
+        />
+      )}
       <div className="OsItinerance OsItPartner CssCustom">
         <div id="eiti-partner">
           <Loader />
