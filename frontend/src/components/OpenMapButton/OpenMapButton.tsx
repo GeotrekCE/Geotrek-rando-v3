a6487edf237709cfdeaee0ce29f581a9a4464e95
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-09-05 17:00:57 +0200

    Handle OpenMapbutton component with viewpoint

--- frontend/src/components/OpenMapButton/OpenMapButton.tsx
+++ frontend/src/components/OpenMapButton/OpenMapButton.tsx
@@ -1,26 +1,39 @@
+import { useCallback } from 'react';
 import { Map } from 'components/Icons/Map';
 import { FormattedMessage } from 'react-intl';
 import { useHideOnScrollDown } from 'hooks/useHideOnScrollDown';
-import { Button } from 'components/Button';
 import { cn } from 'services/utils/cn';
 
 export const OpenMapButton: React.FC<
-  React.ButtonHTMLAttributes<HTMLButtonElement> & { displayMap: () => void }
-> = ({ displayMap, ...nativeButtonProps }) => {
+  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    displayMap: () => void;
+    setMapId?: (key: string) => void;
+  }
+> = ({ displayMap, setMapId, ...nativeButtonProps }) => {
   const buttonDisplayState = useHideOnScrollDown();
 
+  const handleClick = useCallback(() => {
+    displayMap();
+    setMapId?.('default');
+  }, [displayMap, setMapId]);
+
   return (
-    <Button
+    <button
       id="openMapButton"
+      type="button"
       className={cn(
-        'fixed z-floatingButton flex gap-1 desktop:hidden border-0 rounded-full shadow-md left-1/2 -translate-x-1/2 transition-all',
-        buttonDisplayState === 'HIDDEN' ? '-bottom-25' : 'bottom-6',
+        `flex desktop:hidden 
+        fixed z-floatingButton bottom-6 left-1/2 -translate-x-1/2 
+        py-3 px-4 
+        shadow-sm rounded-full 
+        text-primary1 bg-white hover:bg-primary2 focus:bg-primary2 transition-all`,
+        buttonDisplayState === 'HIDDEN' && '-bottom-25',
       )}
       {...nativeButtonProps}
-      onClick={displayMap}
+      onClick={handleClick}
     >
       <FormattedMessage id="search.seeMap" />
-      <Map size={24} />
-    </Button>
+      <Map size={24} className="ml-1" />
+    </button>
   );
 };
