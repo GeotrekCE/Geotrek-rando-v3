c4ceff0cc5583b9afc216db62292e11400c99f90
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-09-06 15:50:41 +0200

    Use tailwind instead of styled component for ControlSection component

--- frontend/src/components/Map/components/ControlSection/ControlPanel/Line.tsx
+++ frontend/src/components/Map/components/ControlSection/ControlPanel/Line.tsx
@@ -1,37 +1,26 @@
-import styled from 'styled-components';
-import { colorPalette } from 'stylesheet';
 import { useIntl } from 'react-intl';
+import { cn } from 'services/utils/cn';
 import Check from './Check';
 
-const Wrapper = styled.button<{ active: boolean }>`
-  color: ${props => (props.active ? colorPalette.primary1 : colorPalette.greyDarkColored)};
-  display: flex;
-  align-items: center;
-  font-weight: bold;
-  text-align: left;
-`;
-const IconWrapper = styled.div`
-  & svg {
-    height: 25px;
-    width: 25px;
-    margin-right: 10px;
-  }
-`;
-
-const Text = styled.div`
-  flex: auto;
-`;
-
-export const Line: React.FC<{ Icon: any; active: boolean; toggle: () => void; transKey: string }> =
-  ({ Icon, active, toggle, transKey }) => {
-    const intl = useIntl();
-    return (
-      <Wrapper active={active} onClick={toggle} type="button">
-        <IconWrapper>
-          <Icon />
-        </IconWrapper>
-        <Text>{intl.formatMessage({ id: transKey })}</Text>
-        {<Check visibility={active ? 'auto' : 'hidden'} />}
-      </Wrapper>
-    );
-  };
+export const Line: React.FC<{
+  icon: any;
+  active: boolean;
+  toggle: () => void;
+  transKey: string;
+}> = ({ icon: Icon, active, toggle, transKey }) => {
+  const intl = useIntl();
+  return (
+    <button
+      className={cn(
+        'flex items-center text-left font-bold',
+        active ? 'text-primary1' : 'text-greyDarkColored',
+      )}
+      onClick={toggle}
+      type="button"
+    >
+      <Icon className="w-6 h-6 mr-3" />
+      <span className="flex-auto">{intl.formatMessage({ id: transKey })}</span>
+      <Check visibility={active ? 'auto' : 'hidden'} />
+    </button>
+  );
+};
