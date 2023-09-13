814f42c9b1d5bad2b6745a3b976e0e8ade24ccb3
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-09-13 11:36:16 +0200

    Get viewpoint for POI module

--- frontend/src/modules/poi/adapter.ts
+++ frontend/src/modules/poi/adapter.ts
@@ -1,5 +1,6 @@
 import { PoiTypeDictionnary } from 'modules/poiType/interface';
 import { getAttachments, getThumbnails } from 'modules/utils/adapter';
+import { adaptViewPoints } from 'modules/viewPoint/adapter';
 import { Poi, RawPoi } from './interface';
 
 export const adaptPoi = ({
@@ -8,17 +9,25 @@ export const adaptPoi = ({
 }: {
   rawPoisResults: RawPoi[];
   poiTypes: PoiTypeDictionnary;
-}): Poi[] =>
-  rawPoisResults.map(rawPoi => ({
-    id: `${rawPoi.id}`,
-    name: rawPoi.name,
-    description: rawPoi.description,
-    thumbnails: getThumbnails(rawPoi.attachments),
-    attachments: getAttachments(rawPoi.attachments),
-    type: poiTypes[rawPoi.type],
-    geometry: {
-      x: rawPoi.geometry.coordinates[0],
-      y: rawPoi.geometry.coordinates[1],
-      z: rawPoi.geometry.coordinates[2],
-    },
-  }));
+}): Promise<Poi[]> =>
+  Promise.all(
+    rawPoisResults.map(async rawPoi => {
+      const viewPoints =
+        rawPoi.view_points?.length > 0 ? await adaptViewPoints(rawPoi.view_points) : [];
+
+      return {
+        id: `${rawPoi.id}`,
+        name: rawPoi.name,
+        description: rawPoi.description,
+        thumbnails: getThumbnails(rawPoi.attachments),
+        attachments: getAttachments(rawPoi.attachments),
+        type: poiTypes[rawPoi.type],
+        geometry: {
+          x: rawPoi.geometry.coordinates[0],
+          y: rawPoi.geometry.coordinates[1],
+          z: rawPoi.geometry.coordinates[2],
+        },
+        viewPoints,
+      };
+    }),
+  );
--- frontend/src/modules/poi/api.ts
+++ frontend/src/modules/poi/api.ts
@@ -3,7 +3,7 @@ import { APIQuery, APIResponseForList } from 'services/api/interface';
 import { RawPoi } from './interface';
 
 const fieldsParams = {
-  fields: 'id,name,description,attachments,type,geometry',
+  fields: 'id,name,description,attachments,type,geometry,view_points',
 };
 
 export const fetchPois = (query: APIQuery): Promise<APIResponseForList<RawPoi>> =>
