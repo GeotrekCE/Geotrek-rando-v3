0c7453149bb66ccf506e588037ee574457b7e47c
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-09-27 16:12:57 +0200

    Allow Popup map component to display explicit content

--- frontend/src/components/Map/components/Popup/index.tsx
+++ frontend/src/components/Map/components/Popup/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { routes } from 'services/routes';
 import styled, { css } from 'styled-components';
 import { Popup as LeafletPopup, Tooltip as LeafletTooltip } from 'react-leaflet';
@@ -10,32 +10,45 @@ import { Button } from 'components/Button';
 import { generateResultDetailsUrl } from 'components/pages/search/utils';
 
 import Link from 'components/Link';
+import { PopupResult } from 'modules/trekResult/interface';
 import { usePopupResult } from '../../hooks/usePopupResult';
 
 interface Props {
-  id: number;
+  id: number | string;
   parentId?: number;
   handleOpen?: () => void;
   handleClose?: () => void;
-  type: 'TREK' | 'TOURISTIC_CONTENT' | 'OUTDOOR_SITE' | 'TOURISTIC_EVENT';
+  type: 'TREK' | 'TOURISTIC_CONTENT' | 'OUTDOOR_SITE' | 'TOURISTIC_EVENT' | null;
+  content?: PopupResult;
 }
 
 interface PropsPC {
   showButton: boolean;
-  id: number;
-  type: 'TREK' | 'TOURISTIC_CONTENT' | 'OUTDOOR_SITE' | 'TOURISTIC_EVENT';
+  id: number | string;
+  type: 'TREK' | 'TOURISTIC_CONTENT' | 'OUTDOOR_SITE' | 'TOURISTIC_EVENT' | null;
   parentId?: number;
+  content?: PopupResult;
 }
 
-const getRoute = (type: string) => {
+const getRoute = (type: PropsPC['type']) => {
   if (type === 'TOURISTIC_CONTENT') return routes.TOURISTIC_CONTENT;
   if (type === 'OUTDOOR_SITE') return routes.OUTDOOR_SITE;
   if (type === 'TOURISTIC_EVENT') return routes.TOURISTIC_EVENT;
+  if (type === 'TREK') return routes.TREK;
   return routes.TREK;
 };
 
-const PopupContent: React.FC<PropsPC> = ({ showButton, id, type, parentId }) => {
-  const { isLoading, trekPopupResult } = usePopupResult(id.toString(), true, type);
+const PopupContent: React.FC<PropsPC> = ({ showButton, id, type, parentId, content }) => {
+  let isLoading = false;
+  let trekPopupResult = null;
+
+  if (!content && type) {
+    const popupResult = usePopupResult(id.toString(), true, type);
+    isLoading = popupResult.isLoading;
+    trekPopupResult = popupResult.trekPopupResult;
+  } else {
+    trekPopupResult = content;
+  }
 
   return (
     <Loader className="absolute inset-0" loaded={!isLoading}>
@@ -43,22 +56,38 @@ const PopupContent: React.FC<PropsPC> = ({ showButton, id, type, parentId }) => 
         <div className="flex flex-col">
           <CoverImage src={trekPopupResult.imgUrl} />
           <div className="p-4">
-            <span className="text-P2 mb-1 text-greyDarkColored hidden desktop:inline">
-              {trekPopupResult.place}
-            </span>
-            <h3 className="text-Mobile-C1 text-primary1 font-bold desktop:text-H4 line-clamp-1">
+            {trekPopupResult.place && (
+              <span className="text-P2 mb-1 text-greyDarkColored hidden desktop:inline">
+                {trekPopupResult.place}
+              </span>
+            )}
+            <h3 className="text-Mobile-C1 text-primary1 font-bold desktop:text-H4 line-clamp-2">
               {trekPopupResult.title}
             </h3>
             {showButton && (
-              <Link
-                href={generateResultDetailsUrl(id, trekPopupResult.title, getRoute(type), parentId)}
-              >
-                <Button className="mt-4 w-full">
-                  <span className="w-full text-center">
-                    <FormattedMessage id="search.map.seeResult" />
-                  </span>
-                </Button>
-              </Link>
+              <>
+                {trekPopupResult?.button?.onClick ? (
+                  <Button className="mt-4 w-full" {...trekPopupResult.button}>
+                    <span className="w-full">
+                      <FormattedMessage id={trekPopupResult.button.label} />
+                    </span>
+                  </Button>
+                ) : (
+                  <Link
+                    className="mt-4 flex gap-1 items-center py-2 px-4 h-12 border border-solid border-primary1 rounded-lg text-sm text-primary1 bg-white font-semibold transition transition-color hover:bg-primary2 focus:bg-primary2"
+                    href={generateResultDetailsUrl(
+                      id,
+                      trekPopupResult.title,
+                      getRoute(type ?? null),
+                      parentId,
+                    )}
+                  >
+                    <span className="text-center w-full">
+                      <FormattedMessage id="search.map.seeResult" />
+                    </span>
+                  </Link>
+                )}
+              </>
             )}
           </div>
         </div>
@@ -67,29 +96,36 @@ const PopupContent: React.FC<PropsPC> = ({ showButton, id, type, parentId }) => 
   );
 };
 
-export const Popup: React.FC<Props> = ({ id, parentId, handleOpen, handleClose, type }) => {
+export const Popup: React.FC<Props> = ({
+  id,
+  parentId,
+  handleOpen,
+  handleClose,
+  type,
+  content,
+}) => {
   const [hideTooltip, setHideTooltip] = useState<boolean>(false);
 
   return (
     <>
       {!hideTooltip && (
         <StyledTooltip>
-          <PopupContent type={type} id={id} showButton={false} />
+          <PopupContent type={type} id={id} showButton={false} content={content} />
         </StyledTooltip>
       )}
       <StyledPopup
         closeButton={false}
         onOpen={() => {
           setHideTooltip(true);
-          handleOpen && handleOpen();
+          handleOpen?.();
         }}
         onClose={() => {
           setHideTooltip(false);
-          handleClose && handleClose();
+          handleClose?.();
         }}
         offset={[0, -12]}
       >
-        <PopupContent type={type} id={id} showButton={true} parentId={parentId} />
+        <PopupContent type={type} id={id} showButton={true} parentId={parentId} content={content} />
       </StyledPopup>
     </>
   );
