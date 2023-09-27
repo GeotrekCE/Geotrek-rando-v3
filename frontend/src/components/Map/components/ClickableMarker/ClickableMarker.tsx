3f7a143949c883f82390e63b0f23542e51dc41b1
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-09-27 16:16:53 +0200

    Create ClickableMarker component

---
+++ frontend/src/components/Map/components/ClickableMarker/ClickableMarker.tsx
@@ -0,0 +1,36 @@
+import { HoverableMarker } from 'components/Map/components/HoverableMarker';
+import { Popup } from 'components/Map/components/Popup';
+import { PopupResult } from 'modules/trekResult/interface';
+
+interface ClickableMarkerProps {
+  points: {
+    location: { x: number; y: number };
+    pictogramUri: string;
+    id: string;
+    content: PopupResult;
+    type?: 'TREK' | 'TOURISTIC_CONTENT' | 'OUTDOOR_SITE' | 'TOURISTIC_EVENT' | null;
+  }[];
+}
+
+export const ClickableMarker = ({ points, ...props }: ClickableMarkerProps) => {
+  if (!points?.length) {
+    return null;
+  }
+
+  return (
+    <>
+      {points.map(point => (
+        <HoverableMarker
+          key={point.id}
+          id={point.id}
+          position={[point.location.y, point.location.x]}
+          pictogramUri={point.pictogramUri}
+        >
+          <Popup id={point.id} type={point.type ?? null} content={point.content} {...props} />
+        </HoverableMarker>
+      ))}
+    </>
+  );
+};
+
+export default ClickableMarker;
