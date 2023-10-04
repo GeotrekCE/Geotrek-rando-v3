5a7887c9c7b97fed67fe35389cdee900906e87bb
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-10-04 13:29:14 +0200

    Call scripts template according to GPDR

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,22 +1,29 @@
 import { Header } from 'components/Header';
 import ConditionallyRender from 'components/ConditionallyRender';
 import Loader from 'components/Loader';
+import Head from 'next/head';
 import { useNavigationLoader } from './useRedirection';
+import { useExternalsScripts } from './useExternalScripts';
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { isNavigationLoading } = useNavigationLoader();
+  const { scriptsHeader, scriptsFooter } = useExternalsScripts(true);
 
   return (
-    <div className="flex flex-col min-h-full">
-      <Header />
-      <main className="relative grow">
-        <ConditionallyRender client>
-          <Loader loaded={!isNavigationLoading} className="z-loader absolute inset-0">
-            {children}
-          </Loader>
-        </ConditionallyRender>
-        <ConditionallyRender server>{children}</ConditionallyRender>
-      </main>
-    </div>
+    <>
+      <Head>{scriptsHeader && <>{scriptsHeader}</>}</Head>
+      <div className="flex flex-col min-h-full">
+        <Header />
+        <main className="relative grow">
+          <ConditionallyRender client>
+            <Loader loaded={!isNavigationLoading} className="z-loader absolute inset-0">
+              {children}
+            </Loader>
+          </ConditionallyRender>
+          <ConditionallyRender server>{children}</ConditionallyRender>
+        </main>
+      </div>
+      {scriptsFooter && <>{scriptsFooter}</>}
+    </>
   );
 };
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,6 +8,7 @@ import '../styles/global.css';
 import '../public/fonts.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'orejime/dist/orejime.css';
 import '../public/style.css';
 
 import { ListAndMapProvider } from 'modules/map/ListAndMapContext';
