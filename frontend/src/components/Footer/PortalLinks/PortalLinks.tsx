a9c007701dece15e4645705b8d1027644368ca89
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-10-05 17:33:40 +0200

    Rewrite Portalinks component

--- frontend/src/components/Footer/PortalLinks/PortalLinks.tsx
+++ frontend/src/components/Footer/PortalLinks/PortalLinks.tsx
@@ -3,99 +3,94 @@ import { Plus } from 'components/Icons/Plus';
 import { Minus } from 'components/Icons/Minus';
 import { FormattedMessage } from 'react-intl';
 import useHasMounted from 'hooks/useHasMounted';
+import { cn } from 'services/utils/cn';
+import { useId } from 'react';
 import { usePortalLinks } from './usePortalLinks';
 import { PortalLinkStatic } from '../interface';
 import { isLinkInternal, linkWithoutHost } from '../utils';
 
 interface PortalLinksContentProps {
+  id: string;
   className?: string;
   links: PortalLinkStatic[];
 }
 
-export interface PortalLinksProps extends PortalLinksContentProps {
+export interface PortalLinksProps {
   name: string;
+  links: PortalLinkStatic[];
 }
 
 export const PortalLinks: React.FC<PortalLinksProps> = ({ name, links }) => {
   const { openState, updatePanelState } = usePortalLinks();
+  const contentId = useId();
 
   return (
-    <>
-      <div className="hidden desktop:flex flex-col text-greySoft ml-3 items-end">
+    <div className="text-greySoft desktop:ml-3">
+      <h3 className="hidden desktop:block">
         <PortalLinksTitle name={name} />
-        <PortalLinksDesktopContent links={links} />
-      </div>
-      <div className="desktop:hidden text-greySoft">
-        <div
-          className={`
-          flex justify-between items-center
-          py-2.5 outline-none
-          ${openState === 'CLOSED' ? 'border-b border-solid border-greySoft' : ''}`}
-          onClick={updatePanelState}
-        >
-          <PortalLinksTitle name={name} />
-          {openState === 'OPENED' ? (
-            <Minus size={24} className="shrink-0" />
-          ) : (
-            <Plus size={24} className="shrink-0" />
-          )}
-        </div>
-        <PortalLinksMobileContent
-          links={links}
-          className={
-            openState === 'CLOSED' ? 'hidden' : 'block border-b border-solid border-greySoft'
-          }
-        />
-      </div>
-    </>
+      </h3>
+      <button
+        className={cn(
+          `
+        flex justify-between items-center w-full
+        py-2.5 outline-none
+        desktop:hidden
+        text-left`,
+          openState === 'CLOSED' && 'border-b border-solid border-greySoft',
+        )}
+        onClick={updatePanelState}
+        type="button"
+        aria-expanded={openState === 'OPENED' ? 'true' : 'false'}
+        aria-controls={contentId}
+      >
+        <PortalLinksTitle name={name} />
+        {openState === 'OPENED' ? (
+          <Minus size={24} className="shrink-0" />
+        ) : (
+          <Plus size={24} className="shrink-0" />
+        )}
+      </button>
+      <PortalLinksContent
+        id={contentId}
+        links={links}
+        className={
+          openState === 'CLOSED'
+            ? 'hidden desktop:block'
+            : 'block border-b border-solid border-greySoft desktop:border-0'
+        }
+      />
+    </div>
   );
 };
 
 const PortalLinksTitle: React.FC<{ name: string }> = ({ name }) => (
-  <p
-    className="
-        text-Mobile-C1 desktop:text-H3
+  <span
+    className={cn(
+      `
+        block text-Mobile-C1 desktop:text-H3
         font-bold cursor-pointer w-full
-        desktop:mb-3.5 desktop:text-right"
+        desktop:mb-3.5 desktop:text-right`,
+    )}
   >
     <FormattedMessage id={name} />
-  </p>
+  </span>
 );
 
-const PortalLinksDesktopContent: React.FC<PortalLinksContentProps> = ({
-  className = '',
-  links,
-}) => {
+const PortalLinksContent: React.FC<PortalLinksContentProps> = ({ id, className = '', links }) => {
   return (
-    <div className={`flex flex-col pb-4 ${className}`}>
+    <ul id={id} className={cn('columns-2 desktop:columns-auto flex-col w-full pb-4', className)}>
       {links.map((l, i) => (
-        <PortalLinkRendered link={l} key={i} />
+        <li key={i} className="desktop:text-right">
+          <PortalLinkRendered link={l} />
+        </li>
       ))}
-    </div>
-  );
-};
-
-const PortalLinksMobileContent: React.FC<PortalLinksContentProps> = ({ className = '', links }) => {
-  const nbOfLinksPerColumn = Math.ceil(links.length / 2);
-  return (
-    <div className={`flex pb-4 ${className}`}>
-      <div className="flex flex-col flex-1">
-        {links.slice(0, nbOfLinksPerColumn).map((l, i) => (
-          <PortalLinkRendered link={l} key={i} />
-        ))}
-      </div>
-      <div className="flex flex-col flex-1">
-        {links.slice(nbOfLinksPerColumn).map((l, i) => (
-          <PortalLinkRendered link={l} key={i} />
-        ))}
-      </div>
-    </div>
+    </ul>
   );
 };
 
 const PortalLinkRendered: React.FC<{ link: PortalLinkStatic }> = ({ link }) => {
   const className =
-    'text-greySoft text-Mobile-C3 desktop:text-P1 cursor-pointer hover:text-white transition-all desktop:text-right';
+    'text-greySoft text-Mobile-C3 desktop:text-P1 hover:text-white focus:text-white transition-all';
   const isWindow = useHasMounted(typeof window !== 'undefined');
   if (isWindow) {
     return isLinkInternal(link.url) ? (
