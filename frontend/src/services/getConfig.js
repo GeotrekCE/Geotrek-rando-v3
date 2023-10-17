e34706f394d3d68781524a4843e962d979fedb5c
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-10-17 17:03:26 +0200

    Get details template HTML available to publicRuntimeConfig

--- frontend/src/services/getConfig.js
+++ frontend/src/services/getConfig.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const deepmerge = require('deepmerge');
 const { getLocales } = require('./getLocales');
 
+function getFiles(dir, files = []) {
+  const fileList = fs.readdirSync(dir);
+  for (const file of fileList) {
+    const name = `${dir}/${file}`;
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files);
+    } else {
+      files.push(name);
+    }
+  }
+  return files;
+}
+
 const getContent = (path, parse) => {
   if (fs.existsSync(path)) {
     const content = fs.readFileSync(path).toString();
@@ -20,28 +33,22 @@ const getConfig = (file, parse = true, deepMerge = false) => {
   const merge = (elem1, elem2) => {
     if (Array.isArray(elem1)) {
       return [...elem2, ...elem1];
-    } 
-    if (deepMerge) {
-      return deepmerge.all([elem2 ?? {}, elem1 ?? {}])
     }
-    else {
-      return { ...elem1, ...elem2 }
-    };
+    if (deepMerge) {
+      return deepmerge.all([elem2 ?? {}, elem1 ?? {}]);
+    } else {
+      return { ...elem1, ...elem2 };
+    }
   };
 
-  return parse
-    ? merge(defaultConfig, overrideConfig)
-    : overrideConfig || defaultConfig;
+  return parse ? merge(defaultConfig, overrideConfig) : overrideConfig || defaultConfig;
 };
 
 const getTemplates = (file, languages) => {
   const [path] = file.split('.html');
   return languages.reduce(
     (list, language) => {
-      list[language] = getContent(
-        `./customization/config/${path}-${language}.html`,
-        false,
-      );
+      list[language] = getContent(`./customization/config/${path}-${language}.html`, false);
       return list;
     },
     { default: getContent(`./customization/config/${file}`, false) },
@@ -55,7 +62,7 @@ const configDetails = getConfig('details.json', true, true);
 const filterAndOrderSectionsDetails = sections =>
   sections
     .filter(({ name }, index, array) => array.findIndex(item => item.name === name) === index)
-    .sort((a, b) => (a.order ?? Infinity) - (b.order ?? Infinity))
+    .sort((a, b) => (a.order ?? Infinity) - (b.order ?? Infinity));
 
 const details = {
   ...configDetails,
@@ -65,35 +72,26 @@ const details = {
     touristicContent: filterAndOrderSectionsDetails(configDetails.sections.touristicContent),
     touristicEvent: filterAndOrderSectionsDetails(configDetails.sections.touristicEvent),
     outdoorSite: filterAndOrderSectionsDetails(configDetails.sections.outdoorSite),
-    outdoorCourse: filterAndOrderSectionsDetails(configDetails.sections.outdoorCourse)
-  }
-}
+    outdoorCourse: filterAndOrderSectionsDetails(configDetails.sections.outdoorCourse),
+  },
+};
+
+const detailsFiles = getFiles('./customization/html/details');
+const detailsSectionHtml = detailsFiles
+  .map(item => item.replace('./customization', '../'))
+  .reduce((list, file) => {
+    const [nameFile] = file.split('/').pop().split('.');
+    return { ...list, [nameFile]: getTemplates(file, headers.menu.supportedLanguages) };
+  }, {});
 
 const getAllConfigs = {
-  homeBottomHtml: getTemplates(
-    '../html/homeBottom.html',
-    headers.menu.supportedLanguages,
-  ),
-  homeTopHtml: getTemplates(
-    '../html/homeTop.html',
-    headers.menu.supportedLanguages,
-  ),
-  headerTopHtml: getTemplates(
-    '../html/headerTop.html',
-    headers.menu.supportedLanguages,
-  ),
-  headerBottomHtml: getTemplates(
-    '../html/headerBottom.html',
-    headers.menu.supportedLanguages,
-  ),
-  footerTopHtml: getTemplates(
-    '../html/footerTop.html',
-    headers.menu.supportedLanguages,
-  ),
-  footerBottomHtml: getTemplates(
-    '../html/footerBottom.html',
-    headers.menu.supportedLanguages,
-  ),
+  homeBottomHtml: getTemplates('../html/homeBottom.html', headers.menu.supportedLanguages),
+  homeTopHtml: getTemplates('../html/homeTop.html', headers.menu.supportedLanguages),
+  headerTopHtml: getTemplates('../html/headerTop.html', headers.menu.supportedLanguages),
+  headerBottomHtml: getTemplates('../html/headerBottom.html', headers.menu.supportedLanguages),
+  footerTopHtml: getTemplates('../html/footerTop.html', headers.menu.supportedLanguages),
+  footerBottomHtml: getTemplates('../html/footerBottom.html', headers.menu.supportedLanguages),
+  detailsSectionHtml,
   scriptsHeaderHtml: getConfig('../html/scriptsHeader.html', false),
   scriptsFooterHtml: getConfig('../html/scriptsFooter.html', false),
   style: getConfig('../theme/style.css', false),
