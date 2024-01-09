8b79a0a386168afccbec59e36b5511ee9d998a59
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-01-09 17:12:09 +0100

    Possibility to add negativeElevation to resultCard

--- frontend/src/modules/results/api.ts
+++ frontend/src/modules/results/api.ts
@@ -5,7 +5,7 @@ import { RawTrekResult } from './interface';
 
 const fieldsParams = {
   fields:
-    'id,departure,name,themes,duration,length_2d,ascent,difficulty,reservation_system,attachments,practice,departure_city',
+    'id,departure,name,themes,duration,length_2d,ascent,descent,difficulty,reservation_system,attachments,practice,departure_city',
 };
 
 export const fetchTrekResults = (
