9ad24388b88e79b9a450774b58e416e352bfa448
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-02-29 16:48:45 +0100

    Ignore Promise-returning function provided to attribute where a void return was expected for ErroFallback

--- frontend/src/components/pages/search/components/ErrorFallback/ErrorFallback.tsx
+++ frontend/src/components/pages/search/components/ErrorFallback/ErrorFallback.tsx
@@ -13,6 +13,7 @@ export const ErrorFallback: React.FC<Props> = ({ refetch }) => {
       <h1 className="text-H2 text-center">
         <FormattedMessage id="search.anErrorOccured" />
       </h1>
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
       <button type="button" className="flex mt-4 flex-col items-center" onClick={refetch}>
         <Reload size={48} />
         <span>
