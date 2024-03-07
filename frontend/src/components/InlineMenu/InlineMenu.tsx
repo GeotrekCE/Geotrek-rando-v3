4568da98ba914748bcaaafccc99ff7de7c06d6f6
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-07 12:19:09 +0100

    Move props/logics related to menu from Header to InlineMenu

--- frontend/src/components/InlineMenu/InlineMenu.tsx
+++ frontend/src/components/InlineMenu/InlineMenu.tsx
@@ -3,7 +3,7 @@ import ReactCountryFlag from 'react-country-flag';
 import { useIntl } from 'react-intl';
 import { Link } from 'components/Link';
 import NextLink from 'next/link';
-import { MenuItem } from 'modules/header/interface';
+import { MenuConfig, MenuItem } from 'modules/header/interface';
 import { ChevronDown } from 'components/Icons/ChevronDown';
 import { useRouter } from 'next/router';
 import { getDefaultLanguage } from 'modules/header/utills';
@@ -11,27 +11,25 @@ import { getCountryCodeFromLanguage } from 'services/i18n/intl';
 
 export interface InlineMenuProps {
   className?: string;
-  shouldDisplayFavorites: boolean;
-  sections?: MenuItem[];
-  subSections?: MenuItem[];
-  supportedLanguages: string[];
+  menuItems: MenuItem[];
+  config: MenuConfig;
 }
 
 const InlineMenu: React.FC<InlineMenuProps> = ({
   className,
-  sections,
-  subSections,
-  shouldDisplayFavorites,
-  supportedLanguages,
+  menuItems,
+  config: { primaryItemsNumber, shouldDisplayFavorite, supportedLanguages}
 }) => {
   const intl = useIntl();
   const router = useRouter();
   const language = router.locale ?? getDefaultLanguage();
 
+  const sections = menuItems.slice(0, primaryItemsNumber);
+  const subSections = menuItems.slice(primaryItemsNumber);
+
   return (
     <div className={className} id="header_inlineMenu">
-      {sections &&
-        sections.map((menuItem, i) => (
+      {sections.map((menuItem, i) => (
           <Section
             name={menuItem.title}
             key={i}
@@ -40,7 +38,7 @@ const InlineMenu: React.FC<InlineMenuProps> = ({
             openInAnotherTab={menuItem.openInAnotherTab}
           />
         ))}
-      {subSections && subSections.length > 0 && (
+      {subSections.length > 0 && (
         <details className="flex-row">
           <summary className={controlClassName}>
             {intl.formatMessage({
@@ -72,7 +70,7 @@ const InlineMenu: React.FC<InlineMenuProps> = ({
         </details>
       )}
 
-      {shouldDisplayFavorites && (
+      {shouldDisplayFavorite && (
         <div className="flex items-center text-white">
           <Heart size={16} className="mr-2" />
           <Section name={intl.formatMessage({ id: 'header.favorites' })} />
