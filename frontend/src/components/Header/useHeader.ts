09b1c7217c2fdcc5c19864b2c581813ff94d4972
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-11 16:50:15 +0100

    UseHeader uses new MenuItems module

--- frontend/src/components/Header/useHeader.ts
+++ frontend/src/components/Header/useHeader.ts
@@ -1,15 +1,22 @@
-import { getFlatPages } from 'modules/flatpage/connector';
-import { MenuItem } from 'modules/header/interface';
+import { MenuItem } from 'modules/menuItems/interface';
 import { getDefaultLanguage, getHeaderConfig } from 'modules/header/utills';
 import { useRouter } from 'next/router';
 import { useIntl } from 'react-intl';
 import { useQuery } from '@tanstack/react-query';
+import { ONE_DAY } from 'services/constants/staleTime';
+import { geMenuItems } from 'modules/menuItems/connector';
 
 export const useHeader = () => {
   const config = getHeaderConfig();
   const language = useRouter().locale ?? getDefaultLanguage();
-  const { data } = useQuery<MenuItem[], Error>(['header', language], () => getFlatPages(language));
+  const { data: menuItems } = useQuery<MenuItem[], Error>(
+    ['header', language],
+    () => geMenuItems(language),
+    {
+      staleTime: ONE_DAY,
+    },
+  );
   const intl = useIntl();
 
-  return { config, intl, menuItems: data };
+  return { config, intl, menuItems };
 };
