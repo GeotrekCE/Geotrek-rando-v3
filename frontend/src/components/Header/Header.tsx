dfb557acaa64aedee4c7b8d732a954503ab73970
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-19 15:51:26 +0100

    Use Image component for the main logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { FormattedMessage } from 'react-intl';
 import { routes } from 'services/routes';
+import Image from 'next/image';
 
 import { Link } from 'components/Link';
 import { Display } from 'hooks/useHideOnScrollDown';
@@ -41,11 +42,14 @@ export const Header: React.FC = () => {
         <div className="h-11 desktop:h-desktopHeader flex flex-row items-center sticky z-header px-3 shadow-sm shrink-0 transition-all duration-300 delay-100">
           <Link href={routes.HOME} className="flex items-center">
             <div className="shrink-0" id="header_logo">
-              <img
+              <Image
                 id="header_logoImg"
-                className="h-9 desktop:h-18 mr-3 rounded-md"
+                className="h-9 w-auto desktop:h-18 mr-3"
                 alt=""
                 src={config.logo}
+                height={36}
+                width={36}
+                priority
               />
             </div>
             <p
