c77e3063b4f5009a80cb7e61d4c980ce85a9f5e0
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-19 15:46:02 +0100

    Better type definition for GenericIconProps

--- frontend/src/components/Icons/types.ts
+++ frontend/src/components/Icons/types.ts
@@ -1,8 +1,8 @@
-export interface GenericIconProps {
+import { SVGProps } from 'react';
+
+export interface GenericIconProps extends SVGProps<SVGSVGElement> {
   /** Default value is "currentColor", which allows you
    * to color your icon using the CSS `color` property. It is the recommended way. */
   color?: string;
-  opacity?: number;
   size?: number;
-  className?: string;
 }
