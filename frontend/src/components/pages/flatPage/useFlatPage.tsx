a90a6d83a9b33690c8d18b78d8856fef653e2f34
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-19 17:40:01 +0100

    Use sources from commonDictionnaries for flatpages

--- frontend/src/components/pages/flatPage/useFlatPage.tsx
+++ frontend/src/components/pages/flatPage/useFlatPage.tsx
@@ -5,16 +5,20 @@ import { isUrlString } from 'modules/utils/string';
 import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
 import { ONE_DAY } from 'services/constants/staleTime';
+import { useQueryCommonDictionaries } from 'modules/dictionaries/api';
 
 export const useFlatPage = (flatPageUrl: string | undefined) => {
   const language = useRouter().locale ?? getDefaultLanguage();
   const path = isUrlString(flatPageUrl) ? decodeURI(flatPageUrl) : '';
   const id = isUrlString(flatPageUrl) ? flatPageUrl.split('-')[0] : '';
+
+  const commonDictionaries = useQueryCommonDictionaries(language);
+
   const { data, refetch, isLoading, error } = useQuery<FlatPageDetails, Error>(
     ['flatPageDetails', id, language],
-    () => getFlatPageDetails(id, language),
+    () => getFlatPageDetails(id, language, commonDictionaries),
     {
-      enabled: isUrlString(flatPageUrl),
+      enabled: isUrlString(flatPageUrl) && commonDictionaries !== undefined,
       staleTime: ONE_DAY,
     },
   );
