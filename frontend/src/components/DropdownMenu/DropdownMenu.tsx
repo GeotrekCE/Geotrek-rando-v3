3942b46af328dead8edf6884cb0df26935871499
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-19 10:39:04 +0100

    Improve DropDownMenu component to use it with mouveover/out

--- frontend/src/components/DropdownMenu/DropdownMenu.tsx
+++ frontend/src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,4 +1,5 @@
-import { SyntheticEvent, useId, useState } from 'react';
+import Link from 'next/link';
+import { FocusEventHandler, SyntheticEvent, useId, useState } from 'react';
 import { cn } from 'services/utils/cn';
 
 interface DropdownMenuProps {
@@ -11,6 +12,7 @@ interface DropdownMenuProps {
   href?: string;
   activeID?: string | null;
   setActiveID?: (id: string | null) => void;
+  asHover?: boolean;
 }
 
 export const DropdownMenu: React.FC<DropdownMenuProps> = ({
@@ -22,11 +24,29 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = ({
   expandedClassName,
   activeID,
   setActiveID,
+  asHover = false,
+  href = null,
   ...props
 }) => {
-  const TriggerTag = props.href ? 'a' : 'button';
   const id = useId();
   const [expanded, setExpanded] = useState(false);
+  const handleMouseEnter = () => {
+    setExpanded(true);
+    setActiveID?.(id);
+  };
+
+  const handleMouseLeave = () => {
+    setExpanded(false);
+    setActiveID?.(null);
+  };
+
+  const handleFocusLeave: FocusEventHandler = event => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setExpanded(false);
+      setActiveID?.(null);
+    }
+  };
+
   const handleClick = (event: SyntheticEvent) => {
     event.preventDefault();
     setExpanded(prevOpen => !prevOpen);
@@ -35,20 +55,37 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = ({
 
   const open = activeID !== undefined ? activeID === id : expanded;
 
+  const triggerCommonProps = {
+    ...(asHover
+      ? {
+          onMouseEnter: handleMouseEnter,
+        }
+      : {
+          onClick: handleClick,
+        }),
+    'aria-expanded': open,
+    'aria-controls': id,
+    className: cn(className, open && expandedClassName),
+  };
+
   return (
-    <div className={wrapperClassName}>
-      <TriggerTag
-        aria-expanded={open ? 'true' : 'false'}
-        aria-controls={id}
-        onClick={handleClick}
-        {...(props.href && {
-          role: 'button',
-        })}
-        className={cn(className, open && expandedClassName)}
-        {...props}
-      >
-        {trigger}
-      </TriggerTag>
+    <div
+      className={wrapperClassName}
+      {...(asHover === true && {
+        onMouseLeave: handleMouseLeave,
+        onFocus: handleMouseEnter,
+        onBlur: handleFocusLeave,
+      })}
+    >
+      {href ? (
+        <Link role="button" href={href} {...triggerCommonProps} {...props}>
+          {trigger}
+        </Link>
+      ) : (
+        <button {...triggerCommonProps} {...props}>
+          {trigger}
+        </button>
+      )}
       <div id={id} className={contentClassName} hidden={!open}>
         {children}
       </div>
