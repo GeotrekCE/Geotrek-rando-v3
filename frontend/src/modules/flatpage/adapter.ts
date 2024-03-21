3a5d60b2a5fceb5d9ae1285f8f3293d8fea48ff6
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-21 10:19:18 +0100

    Improve flatPage module to get children pages

--- frontend/src/modules/flatpage/adapter.ts
+++ frontend/src/modules/flatpage/adapter.ts
@@ -25,9 +25,11 @@ export const adaptFlatPages = (rawFlatPages: RawFlatPage[]): MenuItem[] => {
 export const adaptFlatPageDetails = ({
   rawFlatPageDetails,
   sourceDictionnary,
+  rawFlatPageChildrenDetails,
 }: {
   rawFlatPageDetails: RawFlatPageDetails;
   sourceDictionnary: SourceDictionnary;
+  rawFlatPageChildrenDetails: RawFlatPageDetails[];
 }): FlatPageDetails => ({
   id: rawFlatPageDetails.id,
   title: rawFlatPageDetails.title,
@@ -37,4 +39,14 @@ export const adaptFlatPageDetails = ({
     rawFlatPageDetails.attachments.length > 0 && rawFlatPageDetails.attachments[0].type === 'image'
       ? rawFlatPageDetails.attachments[0].url
       : null,
+  children: rawFlatPageChildrenDetails.map(child => ({
+    id: child.id,
+    title: child.title,
+    content: child.content,
+    sources: child.source.map(sourceId => sourceDictionnary[sourceId]).filter(Boolean),
+    attachment:
+      child.attachments.length > 0 && child.attachments[0].type === 'image'
+        ? child.attachments[0].thumbnail
+        : null,
+  })),
 });
