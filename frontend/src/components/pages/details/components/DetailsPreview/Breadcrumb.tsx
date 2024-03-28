166d09f37be42d66fb9bf5aa794d14682281fffd
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-03-28 17:54:49 +0100

    Improve breadcrumb semantics

--- frontend/src/components/pages/details/components/DetailsPreview/Breadcrumb.tsx
+++ frontend/src/components/pages/details/components/DetailsPreview/Breadcrumb.tsx
@@ -1,27 +1,41 @@
 import Link from 'components/Link';
-import { Fragment } from 'react';
+import { useIntl } from 'react-intl';
+import { cn } from 'services/utils/cn';
 
-const Breadcrumb: React.FC<{ breadcrumb: { label: string; link?: string }[] }> = ({
-  breadcrumb,
-}) => {
+interface BreadcrumbProps {
+  breadcrumb: { label: string; link?: string }[];
+  className?: string;
+}
+
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumb, className }) => {
+  const intl = useIntl();
   return (
-    <div className="mt-2 mb-4 desktop:mt-8 desktop:mb-16 flex flex-wrap items-center text-Mobile-C2">
-      {breadcrumb.map(({ label, link }, index) => {
-        const separator = index === 0 ? '>>' : '>';
-        return (
-          <Fragment key={index}>
-            <div className="mx-2">{separator}</div>
-            {link !== undefined ? (
-              <Link href={link} className="text-sm hover:underline">
-                {label}
-              </Link>
-            ) : (
-              label
-            )}
-          </Fragment>
-        );
-      })}
-    </div>
+    <nav
+      className={cn(
+        'mt-2 mb-4 desktop:mt-8 desktop:mb-16 flex flex-wrap items-center text-Mobile-C2',
+        className,
+      )}
+      aria-label={intl.formatMessage({ id: 'breadcrumb.title' })}
+      role="navigation"
+    >
+      <ol className="flex gap-2">
+        {breadcrumb.map(({ label, link }, index) => {
+          const separator = index === 0 ? '>>' : '>';
+          return (
+            <li className="flex items-center gap-2" key={index}>
+              <span aria-hidden>{separator}</span>
+              {link ? (
+                <Link href={link} className="text-sm hover:underline focus:underline">
+                  {label}
+                </Link>
+              ) : (
+                <span>{label}</span>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 };
 
