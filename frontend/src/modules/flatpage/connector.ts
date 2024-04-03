62cb2662b030e66db9ea5b467e49fc2ab8da93a7
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-04-03 10:30:09 +0200

    Rename isLowerOrEqualCurrentAPIVersion helper

--- frontend/src/modules/flatpage/connector.ts
+++ frontend/src/modules/flatpage/connector.ts
@@ -1,7 +1,7 @@
 import { MenuItem } from 'modules/menuItems/interface';
 import { CommonDictionaries } from 'modules/dictionaries/interface';
 import { getAPIVersion } from 'modules/APIVersion/connector';
-import { isUpperOrEqualCurrentAPIVersion } from 'modules/APIVersion/utils';
+import { isLowerOrEqualCurrentAPIVersion } from 'modules/APIVersion/utils';
 import { adaptFlatPageDetails, adaptFlatPages } from './adapter';
 import { fetchChildrenFlatPageDetails, fetchFlatPageDetails, fetchFlatPages } from './api';
 import { FlatPageDetails, RawFlatPageDetails } from './interface';
@@ -27,11 +27,11 @@ export const getFlatPageDetails = async (
   }
   try {
     const currentAPIVersion = await getAPIVersion();
-    const isUpperOrEqualCurrentAPIVersion_2_104 = isUpperOrEqualCurrentAPIVersion(
+    const is2_104LowerOrEqualCurrentAPIVersion = isLowerOrEqualCurrentAPIVersion(
       '2.104.0',
       currentAPIVersion,
     );
-    rawFlatPageChildrenDetails = isUpperOrEqualCurrentAPIVersion_2_104
+    rawFlatPageChildrenDetails = is2_104LowerOrEqualCurrentAPIVersion
       ? (await fetchChildrenFlatPageDetails({ language }, id)).results
       : [];
   } catch (e) {
