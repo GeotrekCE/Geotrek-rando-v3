0f5892a1385dd661a6bbab0479357ced288ac92a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-04-09 16:08:45 +0200

    Do not call anonymous function in UseDetailCard usecallback

--- frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
+++ frontend/src/components/pages/details/components/DetailsCard/useDetailsCard.tsx
@@ -1,6 +1,6 @@
 import debounce from 'debounce';
 import useIsomorphicLayoutEffect from 'hooks/useIsomorphicLayoutEffect';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 
 const DETAILS_CARD_DEFAULT_HEIGHT = 220;
 
@@ -13,54 +13,51 @@ export const useDetailsCard = (hasMedia = false) => {
       currentTruncateState === 'TRUNCATE' ? 'FULL' : 'TRUNCATE',
     );
 
-  useEffect(() => {
-    const descriptionNode = detailsCardRef.current?.querySelector<HTMLElement>('.line-clamp-2');
-    if (descriptionNode?.offsetHeight === descriptionNode?.scrollHeight && !hasMedia) {
-      setTruncateState('NONE');
-    }
-  }, [hasMedia]);
+  const debouncedResize = useMemo(
+    () =>
+      debounce(
+        () => {
+          setTruncateState(prevState => {
+            if (detailsCardRef.current === null || hasMedia) {
+              return prevState;
+            }
+            const descriptionNode =
+              detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2');
 
-  const handleResize = useCallback(
-    debounce(
-      () => {
-        setTruncateState(prevState => {
-          if (detailsCardRef.current === null || hasMedia) {
+            if (
+              prevState === 'TRUNCATE' &&
+              (!descriptionNode || descriptionNode.offsetHeight <= descriptionNode.scrollHeight)
+            ) {
+              return 'NONE';
+            } else if (
+              prevState === 'FULL' &&
+              DETAILS_CARD_DEFAULT_HEIGHT >= detailsCardRef.current.getBoundingClientRect().height
+            ) {
+              return 'NONE';
+            } else if (
+              prevState !== 'TRUNCATE' &&
+              DETAILS_CARD_DEFAULT_HEIGHT < detailsCardRef.current.getBoundingClientRect().height
+            ) {
+              return 'TRUNCATE';
+            }
             return prevState;
-          }
-          const descriptionNode =
-            detailsCardRef.current.querySelector<HTMLElement>('.line-clamp-2');
-
-          if (
-            prevState === 'TRUNCATE' &&
-            (!descriptionNode || descriptionNode.offsetHeight <= descriptionNode.scrollHeight)
-          ) {
-            return 'NONE';
-          } else if (
-            prevState === 'FULL' &&
-            DETAILS_CARD_DEFAULT_HEIGHT >= detailsCardRef.current.getBoundingClientRect().height
-          ) {
-            return 'NONE';
-          } else if (
-            prevState !== 'TRUNCATE' &&
-            DETAILS_CARD_DEFAULT_HEIGHT < detailsCardRef.current.getBoundingClientRect().height
-          ) {
-            return 'TRUNCATE';
-          }
-          return prevState;
-        });
-      },
-      1000,
-      false,
-    ),
-    [setTruncateState, detailsCardRef],
+          });
+        },
+        1000,
+        false,
+      ),
+    [hasMedia],
   );
 
+  const handleResize = useCallback(debouncedResize, [debouncedResize]);
+
   useIsomorphicLayoutEffect(() => {
+    debouncedResize();
     global.addEventListener('resize', handleResize);
     return () => {
       global.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [debouncedResize]);
 
   return { truncateState, toggleTruncateState, detailsCardRef };
 };
