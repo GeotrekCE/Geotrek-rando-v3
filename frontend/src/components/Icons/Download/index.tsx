f0796dd08956ccc69d61d4f0f1f4748784c91398
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-04-18 12:23:35 +0200

    Allow Download icon to fwp props

--- frontend/src/components/Icons/Download/index.tsx
+++ frontend/src/components/Icons/Download/index.tsx
@@ -3,9 +3,8 @@ import { GenericIconProps } from '../types';
 
 export const Download: React.FC<GenericIconProps> = ({
   color = 'currentColor',
-  opacity,
-  className,
   size,
+  ...props
 }) => {
   return (
     <svg
@@ -14,8 +13,7 @@ export const Download: React.FC<GenericIconProps> = ({
       viewBox="0 0 30 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      opacity={opacity}
+      {...props}
     >
       <path
         d="M26.077 19.001v4.923a2.462 2.462 0 01-2.462 2.462H6.385a2.462 2.462 0 01-2.462-2.462v-4.923M8.846 12.847l6.153 6.154 6.154-6.154M15 19.001V4.231"
