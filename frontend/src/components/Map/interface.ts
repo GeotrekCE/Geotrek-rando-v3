d258fbc58f6be5ba3bced08f0a63dd82c7e00072
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-14 16:36:32 +0200

    Add a config to display or not secondary layers by default

--- frontend/src/components/Map/interface.ts
+++ frontend/src/components/Map/interface.ts
@@ -14,4 +14,5 @@ export interface MapConfig {
   mapOfflineLayer: TileLayer;
   zoomAvailableOffline?: number[];
   mobileMapPanelDefaultOpened: boolean;
+  displaySecondaryLayersByDefault: boolean;
 }
