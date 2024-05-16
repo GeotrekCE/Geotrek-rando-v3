9a89c5cebbba9b909b12199213697d1eed08f2d2
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-16 17:07:26 +0200

    Allow Cross and Bin icons to receive other props

--- frontend/src/components/Icons/Bin/index.tsx
+++ frontend/src/components/Icons/Bin/index.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
 import { GenericIconProps } from '../types';
 
-export const Bin: React.FC<GenericIconProps> = ({
-  color = 'currentColor',
-  opacity,
-  className,
-  size,
-}) => {
+export const Bin: React.FC<GenericIconProps> = ({ color = 'currentColor', size, ...props }) => {
   return (
     <svg
       width={size}
       height={size}
       viewBox="0 0 16 16"
-      className={className}
-      opacity={opacity}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
         d="M2 4.00033H3.33333H14"
