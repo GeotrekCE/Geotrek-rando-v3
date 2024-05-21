5bfd12cd585e3af4bb241312c2eeedfc85427755
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-21 10:59:24 +0200

    Create helper to get classNames related to activity type

--- frontend/src/components/pages/search/components/ResultCard/getActivityColor.ts
+++ frontend/src/components/pages/search/components/ResultCard/getActivityColor.ts
@@ -37,6 +37,87 @@ const getActivityColor = (type?: string | null): string => {
       : defaultColor) ?? defaultColor;
 
   return color;
+
+
+// TODO standardize `type` prop
+const getStandardizedType = (type: string | null) => {
+  switch (type) {
+    case 'TREK':
+    case 'PRACTICE':
+    case 'practices':
+      return 'TREK';
+
+    case 'OUTDOOR_SITE':
+    case 'OUTDOOR_PRACTICE':
+    case 'outdoorPractice':
+      return 'OUTDOOR_SITE';
+
+    case 'TOURISTIC_CONTENT':
+    case 'CATEGORY':
+    case 'categories':
+      return 'TOURISTIC_CONTENT';
+
+    case 'TOURISTIC_EVENT':
+    case 'TOURISTIC_EVENT_TYPE':
+    case 'event':
+      return 'TOURISTIC_EVENT';
+
+    default:
+      return 'default';
+  }
 };
 
+export const getActivityColorClassName = (
+  rawType: string | null,
+  {
+    withColor,
+    withColorHover,
+    withBackground,
+    withBackgroundHover,
+  }: {
+    withColor?: boolean;
+    withColorHover?: boolean;
+    withBackground?: boolean;
+    withBackgroundHover?: boolean;
+  },
+): string => {
+  const classes = [];
+  const type = getStandardizedType(rawType);
+  switch (type) {
+    case 'TREK':
+      if (withColor) classes.push('text-trek');
+      if (withBackground) classes.push('bg-trek');
+      if (withColorHover) classes.push('hover:text-trek focus:text-trek');
+      if (withBackgroundHover) classes.push('hover:bg-trek focus:bg-trek');
+      break;
+
+    case 'OUTDOOR_SITE':
+      if (withColor) classes.push('text-outdoor');
+      if (withBackground) classes.push('bg-outdoor');
+      if (withColorHover) classes.push('hover:text-outdoor focus:text-outdoor');
+      if (withBackgroundHover) classes.push('hover:bg-outdoor focus:bg-outdoor');
+      break;
+
+    case 'TOURISTIC_CONTENT':
+      if (withColor) classes.push('text-service');
+      if (withBackground) classes.push('bg-service');
+      if (withColorHover) classes.push('hover:text-service focus:text-service');
+      if (withBackgroundHover) classes.push('hover:bg-service focus:bg-service');
+      break;
+
+    case 'TOURISTIC_EVENT':
+      if (withColor) classes.push('text-events');
+      if (withBackground) classes.push('bg-events');
+      if (withColorHover) classes.push('hover:text-events focus:text-events');
+      if (withBackgroundHover) classes.push('hover:bg-events focus:bg-events');
+      break;
+
+    default:
+      if (withColor) classes.push('text-primary1');
+      if (withBackground) classes.push('bg-primary1');
+      if (withColorHover) classes.push('hover:text-primary1 focus:text-primary1');
+      if (withBackgroundHover) classes.push('hover:bg-primary1 focus:bg-primary1');
+  }
+  return classes.join(' ');
+};
 export default getActivityColor;
