2166b33d92ec44e1534c38633638cf1e2db1f216
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-21 11:03:06 +0200

    Rewrite getActivityColor to render custom properties

--- frontend/src/components/Map/components/HoverableMarker.tsx
+++ frontend/src/components/Map/components/HoverableMarker.tsx
@@ -2,7 +2,6 @@ import getActivityColor from 'components/pages/search/components/ResultCard/getA
 import { useListAndMapContext } from 'modules/map/ListAndMapContext';
 import React, { ReactNode, useMemo } from 'react';
 import { Marker } from 'react-leaflet';
-import { ContentType } from 'modules/interface';
 import { TrekChildMarker } from '../Markers/TrekChildMarker';
 import { TrekMarker } from '../Markers/TrekMarker';
 
@@ -18,7 +17,7 @@ interface BaseProps {
 
 interface TrekOrTouristicContentProps extends BaseProps {
   pictogramUri?: string;
-  type?: ContentType | null;
+  type?: string | null;
 }
 
 interface TrekChildProps extends BaseProps {
@@ -32,7 +31,7 @@ const isTrekChild = (trek: TrekOrTouristicContentProps | TrekChildProps): trek i
 export const HoverableMarker: React.FC<TrekOrTouristicContentProps | TrekChildProps> = props => {
   const { hoveredCardId } = useListAndMapContext();
   const isCorrespondingCardHovered = props.id === hoveredCardId;
-  const color = getActivityColor(props.type);
+  const color = getActivityColor(props.type ?? null);
 
   return useMemo(
     () => (
