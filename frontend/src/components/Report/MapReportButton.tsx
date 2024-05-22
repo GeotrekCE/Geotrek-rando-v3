c78a2c4095ecdb697fb05e5e362b8d66debd4649
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-22 16:02:52 +0200

    Use tailwind instead of styled-component for MapReportButton component

--- frontend/src/components/Report/MapReportButton.tsx
+++ frontend/src/components/Report/MapReportButton.tsx
@@ -1,37 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
-import { borderRadius, colorPalette, getSpacing, typography } from 'stylesheet';
 import { AlertCircle } from 'components/Icons/AlertCircle';
+import { cn } from 'services/utils/cn';
 
 export const MapReportButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
   children,
-  className = '',
+  className,
   ...props
 }) => {
   return (
-    <MapButton type="button" className={`m-auto flex gap-2  ${className}`} {...props}>
-      <AlertCircle size={24} className="ml-1" />
+    <button
+      type="button"
+      className={cn(
+        'm-auto flex gap-2 items-center p-3 shadow-sm text-primary1 bg-white hover:bg-primary2 focus:bg-primary2 transition rounded-2xl',
+        className,
+      )}
+      {...props}
+    >
+      <AlertCircle size={24} aria-hidden />
       {children}
-    </MapButton>
+    </button>
   );
 };
-
-const MapButton = styled.button`
-  padding: ${getSpacing(3)};
-
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.15);
-  border-radius: ${borderRadius.roundButton};
-
-  ${typography.main};
-  color: ${colorPalette.primary1};
-
-  background-color: ${colorPalette.white};
-
-  transition-property: background-color bottom;
-  transition-duration: 0.3s;
-  transition-timing-function: ease-in-out;
-
-  &:hover {
-    background-color: ${colorPalette.primary2};
-  }
-`;
