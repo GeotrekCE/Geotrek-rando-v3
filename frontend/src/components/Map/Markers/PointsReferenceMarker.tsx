16b6bf873d33dd012e01c7ce0b3bf1bce432eec0
Florian Sommariva <florian.sommariva@makina-corpus.com>
2024-05-23 15:52:18 +0200

    Use tailwind instead of styled-component for PointsReferenceMarker component

--- frontend/src/components/Map/Markers/PointsReferenceMarker.tsx
+++ frontend/src/components/Map/Markers/PointsReferenceMarker.tsx
@@ -1,34 +1,19 @@
 import { DivIcon } from 'leaflet';
 import { renderToStaticMarkup } from 'react-dom/server';
-import styled from 'styled-components';
-import { colorPalette } from 'stylesheet';
-
-const markerSize = 26;
-
-const Round = styled.div`
-  border-radius: 50%;
-  background-color: ${colorPalette.redMarker};
-  display: grid;
-  place-items: center;
-  color: ${colorPalette.white};
-
-  height: ${markerSize}px;
-  width: ${markerSize}px;
-`;
 
 const PointsReference: React.FC<{ number: number }> = ({ number }) => {
   return (
-    <Round>
+    <div className="size-7 rounded-full bg-redMarker grid place-items-center text-white">
       <span>{number}</span>
-    </Round>
+    </div>
   );
 };
 
 export const PointsReferenceMarker = (id: number): DivIcon =>
   new DivIcon({
-    iconSize: [markerSize, markerSize],
+    iconSize: [26, 26],
     // point of the icon which will correspond to marker's location
-    iconAnchor: [markerSize / 2, markerSize / 2], // horizontal middle of the icon and bottom of it,
+    iconAnchor: [13, 13], // horizontal middle of the icon and bottom of it,
     html: renderToStaticMarkup(<PointsReference number={id} />),
     className: 'bg-none border-none',
   });
