53a5eda2c96167e69197941c5947e89d9c871d1e
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-08-21 10:23:24 +0200

    Remove the locateControl call from useTileLayer hook

--- frontend/src/hooks/useTileLayer.ts
+++ frontend/src/hooks/useTileLayer.ts
@@ -1,46 +1,23 @@
-import L, { LatLngBoundsExpression } from 'leaflet';
+import { LatLngBoundsExpression } from 'leaflet';
 import { Map } from 'leaflet';
 import { useState } from 'react';
-import { useIntl } from 'react-intl';
 
-require('leaflet.locatecontrol');
-import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css';
 import injectOfflineMode from 'services/offline/injectOfflineMode';
-import { ViewPoint } from 'modules/viewPoint/interface';
 
 export const useTileLayer = (
   id?: number,
   center?: LatLngBoundsExpression | null,
-  mapToDisplay: ViewPoint | 'default' = 'default',
 ): {
   map: Map | null;
   setMapInstance: (newMap: Map) => void;
 } => {
   const [map, setMap] = useState<Map | null>(null);
 
-  const intl = useIntl();
-
   const setMapInstance = (newMap: Map) => {
     setMap(newMap);
 
     if (id !== undefined && center) {
       injectOfflineMode(newMap, id, center);
-    }
-
-    if (mapToDisplay === 'default') {
-      L.control
-        // @ts-expect-error no type available in this plugin
-        .locate({
-          locateOptions: {
-            enableHighAccuracy: true,
-          },
-          icon: 'gg-track',
-          strings: {
-            title: intl.formatMessage({ id: 'search.map.seeMe' }),
-          },
-          position: 'bottomright',
-        })
-        .addTo(newMap);
     }
   };
 
