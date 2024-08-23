ed983ca1d5e5534779cf8711cc1f714afe310bd1
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-08-23 15:33:07 +0200

    Allows to forward props to Map icon component

--- frontend/src/components/Icons/Map/index.tsx
+++ frontend/src/components/Icons/Map/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import { GenericIconProps } from '../types';
 
-export const Map: React.FC<GenericIconProps> = ({
-  color = 'currentColor',
-  opacity,
-  className,
-  size,
-}) => {
+export const Map: React.FC<GenericIconProps> = ({ color = 'currentColor', size, ...props }) => {
   return (
     <svg
       width={size}
@@ -14,8 +8,7 @@ export const Map: React.FC<GenericIconProps> = ({
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      opacity={opacity}
+      {...props}
     >
       <path
         clipRule="evenodd"
