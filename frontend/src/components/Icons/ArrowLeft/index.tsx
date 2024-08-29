9015611bc198a7c429ee5b6a95d3739f64b9f19d
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-08-29 17:02:09 +0200

    Allows to forward props to ArrowLeft icon component

--- frontend/src/components/Icons/ArrowLeft/index.tsx
+++ frontend/src/components/Icons/ArrowLeft/index.tsx
@@ -4,7 +4,7 @@ import { GenericIconProps } from '../types';
 export const ArrowLeft: React.FC<GenericIconProps> = ({
   color = 'currentColor',
   size,
-  className,
+  ...props
 }) => {
   return (
     <svg
@@ -13,7 +13,7 @@ export const ArrowLeft: React.FC<GenericIconProps> = ({
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
+      {...props}
     >
       <path
         d="M19 12H5"
