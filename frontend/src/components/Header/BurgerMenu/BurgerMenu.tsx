5e5b89a5c7a3677da0962bf9f4fa700645a2d94e
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-08-29 16:02:51 +0200

    Use Sheet to display MenuMobile

--- frontend/src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ frontend/src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,15 +1,13 @@
 import { useMemo, useState } from 'react';
-// @ts-expect-error Not official but useful to reduce bundle size
-import Slide from 'react-burger-menu/lib/menus/slide';
-import { MenuConfig } from 'modules/header/interface';
-import { MenuItem } from 'modules/menuItems/interface';
 import { useIntl } from 'react-intl';
 import NextLink from 'next/link';
+import { MenuConfig } from 'modules/header/interface';
+import { MenuItem } from 'modules/menuItems/interface';
 import { routes } from 'services/routes';
 import { cn } from 'services/utils/cn';
-import { BurgerMenuSection } from '../BurgerMenuSection/BurgerMenuSection';
-import { BurgerMenu as BmIcon } from '../../Icons/BurgerMenu';
-import { Cross } from '../../Icons/Cross';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from 'components/Sheet';
+import { BurgerMenuSection } from 'components/Header/BurgerMenuSection/BurgerMenuSection';
+import { BurgerMenu as BurgerMenuIcon } from 'components/Icons/BurgerMenu';
 
 interface Props {
   config: MenuConfig;
@@ -25,7 +23,7 @@ export const BurgerMenu: React.FC<Props> = ({
   displayState = 'DISPLAYED',
 }) => {
   const burgerButtonClassName = cn(
-    'fixed size-6 right-2.5 desktop:right-8 transition-all delay-100 duration-300',
+    'fixed size-6 right-2.5 desktop:right-8 transition-all delay-100 duration-300 text-white z-sliderMenu',
     displayState === 'HIDDEN' ? '-top-21.5' : 'top-2.5 desktop:top-8',
     className,
   );
@@ -63,77 +61,77 @@ export const BurgerMenu: React.FC<Props> = ({
   }, [intl, menuItems]);
 
   return (
-    <Slide
-      id="verticalMenu"
-      right
-      customBurgerIcon={<BmIcon className="text-white" />}
-      customCrossIcon={<Cross size={24} />}
-      burgerButtonClassName={burgerButtonClassName}
-      burgerBarClassName="bg-white"
-      menuClassName="bg-white p-4"
-      crossButtonClassName="left-4 mt-2"
-      crossClassName="bg-greyDarkColored"
-      overlayClassName="top-0"
-      className="top-0"
-      isOpen={isMenuOpen}
-      onStateChange={({ isOpen }: { isOpen: boolean }) => handleMenu(isOpen)}
-    >
-      <span
-        id="verticalMenu_title"
-        className="pb-4 font-bold text-center border-b border-solid border-greySoft outline-none"
-      >
-        {intl.formatMessage({ id: 'header.menu' })}
-      </span>
-      {menuSection.map((item, index) => {
-        if (!item.children?.length) {
-          if (item.url) {
+    <Sheet open={isMenuOpen} onOpenChange={handleMenu}>
+      <SheetTrigger className={burgerButtonClassName}>
+        <BurgerMenuIcon aria-hidden />
+        <span className="sr-only">{intl.formatMessage({ id: 'header.menu' })}</span>
+      </SheetTrigger>
+      <SheetContent className="z-sliderMenu">
+        <SheetHeader>
+          <SheetTitle
+            id="verticalMenu_title"
+            className="pb-4 font-bold text-center border-b border-solid border-greySoft outline-none"
+          >
+            {intl.formatMessage({ id: 'header.menu' })}
+          </SheetTitle>
+        </SheetHeader>
+        <div className="h-full overflow-auto p-6 -mt-6 -mx-6">
+          {menuSection.map((item, index) => {
+            if (!item.children?.length) {
+              if (item.url) {
+                return (
+                  <NextLink
+                    key={index}
+                    className={menuItemLinkClassNames}
+                    href={item.url}
+                    onClick={handleCloseMenu}
+                  >
+                    {item.title}
+                  </NextLink>
+                );
+              }
+              return null;
+            }
             return (
-              <NextLink
+              <BurgerMenuSection
                 key={index}
-                className={menuItemLinkClassNames}
-                href={item.url}
-                onClick={handleCloseMenu}
-              >
-                {item.title}
-              </NextLink>
+                title={item.title}
+                items={item.children}
+                handleCloseMenu={handleCloseMenu}
+              />
             );
-          }
-          return null;
-        }
-        return (
-          <BurgerMenuSection
-            key={index}
-            title={item.title}
-            items={item.children}
-            handleCloseMenu={handleCloseMenu}
-          />
-        );
-      })}
-      {config.shouldDisplayFavorite && (
-        <BurgerMenuSection
-          title={intl.formatMessage({ id: 'header.favorites' })}
-          handleCloseMenu={handleCloseMenu}
-        />
-      )}
-      {config.supportedLanguages.length > 1 && (
-        <BurgerMenuSection
-          title={intl.formatMessage({ id: 'header.language' })}
-          languages={config.supportedLanguages}
-          handleCloseMenu={handleCloseMenu}
-        />
-      )}
-      <NextLink className={menuItemLinkClassNames} href={routes.SEARCH} onClick={handleCloseMenu}>
-        {intl.formatMessage({ id: 'header.goToSearch' })}
-      </NextLink>
-      <NextLink
-        className={menuItemLinkClassNames}
-        href={routes.OFFLINE}
-        prefetch={false}
-        rel="nofollow"
-        onClick={handleCloseMenu}
-      >
-        {intl.formatMessage({ id: 'header.offline' })}
-      </NextLink>
-    </Slide>
+          })}
+          {config.shouldDisplayFavorite && (
+            <BurgerMenuSection
+              title={intl.formatMessage({ id: 'header.favorites' })}
+              handleCloseMenu={handleCloseMenu}
+            />
+          )}
+          {config.supportedLanguages.length > 1 && (
+            <BurgerMenuSection
+              title={intl.formatMessage({ id: 'header.language' })}
+              languages={config.supportedLanguages}
+              handleCloseMenu={handleCloseMenu}
+            />
+          )}
+          <NextLink
+            className={menuItemLinkClassNames}
+            href={routes.SEARCH}
+            onClick={handleCloseMenu}
+          >
+            {intl.formatMessage({ id: 'header.goToSearch' })}
+          </NextLink>
+          <NextLink
+            className={menuItemLinkClassNames}
+            href={routes.OFFLINE}
+            prefetch={false}
+            rel="nofollow"
+            onClick={handleCloseMenu}
+          >
+            {intl.formatMessage({ id: 'header.offline' })}
+          </NextLink>
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
