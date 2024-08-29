ca5b6b658007a9b7d73ae805caa9e3429f1e133f
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-08-29 16:00:57 +0200

    Allows to forward props to BurgerMenu icon component

--- frontend/src/components/Icons/BurgerMenu/index.tsx
+++ frontend/src/components/Icons/BurgerMenu/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import { GenericIconProps } from '../types';
 
 export const BurgerMenu: React.FC<GenericIconProps> = ({
   color = 'currentColor',
-  opacity,
-  className,
   size,
+  ...props
 }) => {
   return (
     <svg
@@ -14,8 +12,7 @@ export const BurgerMenu: React.FC<GenericIconProps> = ({
       viewBox="0 0 24 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      opacity={opacity}
+      {...props}
     >
       <path
         d="M3 12h18M3 6h18M3 18h18"
