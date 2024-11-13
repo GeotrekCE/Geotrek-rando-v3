af9e0fc7d47534bc93dafd91073a37cb27a1faa8
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-11-13 14:35:15 +0100

    Allows to forward props to AlertTriangle/Printer/ThreeDMap icons component

--- frontend/src/components/Icons/Printer/index.tsx
+++ frontend/src/components/Icons/Printer/index.tsx
@@ -3,9 +3,8 @@ import { GenericIconProps } from '../types';
 
 export const Printer: React.FC<GenericIconProps> = ({
   color = 'currentColor',
-  opacity,
-  className,
   size,
+  ...props
 }) => {
   return (
     <svg
@@ -14,8 +13,7 @@ export const Printer: React.FC<GenericIconProps> = ({
       viewBox="0 0 30 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      opacity={opacity}
+      {...props}
     >
       <path
         d="M7.5 11.25V2.5h15v8.75M7.5 22.5H5A2.5 2.5 0 012.5 20v-6.25a2.5 2.5 0 012.5-2.5h20a2.5 2.5 0 012.5 2.5V20a2.5 2.5 0 01-2.5 2.5h-2.5"
