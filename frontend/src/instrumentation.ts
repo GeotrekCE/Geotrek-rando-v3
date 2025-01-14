d2600aee87543bf46c769a730382f81ff8463670
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2025-01-14 12:38:08 +0100

    Do not try to import sentry server config if there is no SENTRY_DSN environement key

--- frontend/src/instrumentation.ts
+++ frontend/src/instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.SENTRY_DSN && process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
   }
 }
