f80fe9bb78ea8c6907df79d9daa5715e85270832
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2026-02-18 17:22:38 +0100

    Use portal param for filtering trek_network filter

--- frontend/src/modules/filters/networks/api.ts
+++ frontend/src/modules/filters/networks/api.ts
@@ -1,6 +1,7 @@
 import { GeotrekAPI } from 'services/api/client';
+import { portalsFilter } from 'modules/utils/api.config';
 import { APIQuery, APIResponseForList } from 'services/api/interface';
 import { RawNetworks } from './interface';
 
 export const fetchNetworks = (query: APIQuery): Promise<APIResponseForList<RawNetworks>> =>
-  GeotrekAPI.get('/trek_network/', { params: query }).then(r => r.data);
+  GeotrekAPI.get('/trek_network/', { params: { ...query, ...portalsFilter } }).then(r => r.data);
