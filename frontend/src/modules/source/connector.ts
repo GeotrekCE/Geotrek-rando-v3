7746fd4f3dd127fc77c2561257e4bb7a7f5108da
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2026-08-26 16:53:26 +0200

    Fetch all paginated sources

--- frontend/src/modules/source/connector.ts
+++ frontend/src/modules/source/connector.ts
@@ -1,8 +1,28 @@
+import { getGlobalConfig } from 'modules/utils/api.config';
+import { generatePageNumbersArray } from 'modules/utils/connector';
+import { concatResults } from 'modules/utils/adapter';
 import { adaptSources } from './adapter';
 import { fetchSources } from './api';
 import { SourceDictionnary } from './interface';
 
 export const getSources = async (language: string): Promise<SourceDictionnary> => {
-  const rawSources = await fetchSources({ language });
-  return adaptSources(rawSources.results);
+  const resultsNumber = getGlobalConfig().mapResultsPageSize;
+  const rawSources = await fetchSources({ language, page_size: resultsNumber });
+  if (rawSources.count < resultsNumber) {
+    return adaptSources(rawSources.results);
+  }
+  // Second call with loop to load all the necessary pages to reach the count
+  const rawSourcesOtherPages = await Promise.all(
+    generatePageNumbersArray(resultsNumber, rawSources.count)
+      .slice(1)
+      .map(pageNumber =>
+        fetchSources({
+          language,
+          page_size: resultsNumber,
+          page: pageNumber,
+        }),
+      ),
+  );
+
+  return adaptSources(concatResults([rawSources, ...rawSourcesOtherPages]));
 };
